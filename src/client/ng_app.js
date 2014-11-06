ac524de2d623390ebf0c3771a1476836370d0458
gu <guymguym@gmail.com>
2014-11-06 22:04:22 +0200

    missing file in last commit

---
+++ src/client/ng_app.js
@@ -0,0 +1,100 @@
+/* global angular, alertify */
+'use strict';
+
+var _ = require('lodash');
+var util = require('util');
+var moment = require('moment');
+var size_utils = require('../util/size_utils');
+var mgmt_api = require('../api/mgmt_api');
+var edge_node_api = require('../api/edge_node_api');
+var ObjectClient = require('../api/object_client');
+var file_reader_stream = require('filereader-stream');
+var mgmt_client = new mgmt_api.Client({
+    path: '/api/mgmt_api/',
+});
+var edge_node_client = new edge_node_api.Client({
+    path: '/api/edge_node_api/',
+});
+var object_client = new ObjectClient({
+    path: '/api/object_api/',
+});
+
+var ng_app = angular.module('ng_app', [
+    'ng_util',
+    'ngRoute',
+    'ngCookies',
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch',
+]);
+
+require('./ng_nodes');
+require('./ng_files');
+
+
+ng_app.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.when('/', {
+            templateUrl: 'status.html',
+        }).when('/nodes', {
+            templateUrl: 'nodes.html',
+        }).when('/files', {
+            templateUrl: 'files.html',
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }
+]);
+
+
+ng_app.controller('AppCtrl', [
+    '$scope', '$http', '$q', '$window', 'nbMgmt', 'nbNodes', 'nbFiles',
+    function($scope, $http, $q, $window, nbMgmt, nbNodes, nbFiles) {
+        $scope.nbMgmt = nbMgmt;
+        $scope.nbNodes = nbNodes;
+        $scope.nbFiles = nbFiles;
+
+        $scope.nav = {
+            root: '/app/'
+        };
+    }
+]);
+
+
+ng_app.controller('StatusCtrl', [
+    '$scope', '$http', '$q', '$window', '$timeout',
+    function($scope, $http, $q, $window, $timeout) {
+        $scope.nav.crumbs = [];
+        $scope.nbMgmt.refresh_status();
+        $scope.nbNodes.refresh_nodes();
+    }
+]);
+
+
+ng_app.factory('nbMgmt', [
+    '$q', '$timeout',
+    function($q, $timeout) {
+        var $scope = {};
+        $scope.refresh_status = refresh_status;
+        refresh_status();
+
+        function refresh_status() {
+            if ($scope.refreshing) {
+                return;
+            }
+            $scope.refreshing = true;
+            return $q.when(mgmt_client.system_stats()).then(
+                function(res) {
+                    console.log('STATS', res);
+                    $scope.stats = res;
+                    return $timeout(function() {
+                        $scope.refreshing = false;
+                    }, 500);
+                }
+            );
+        }
+
+        return $scope;
+    }
+]);

f8292047660ac25415c4b34183da3a9a290e2808
yaelal <stam@stams-MacBook-Air.local>
2015-02-25 16:11:05 +0200

    change port for signalling 2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-// SIGNALING AND ICE (on heroku: ws://noobaa-signaling.herokuapp.com)
-config.address = 'ws://127.0.0.1:4005';
+// SIGNALING AND ICE
+config.address = 'ws://127.0.0.1:5002'; // (on heroku: ws://noobaa-signaling.herokuapp.com)
 config.alive_delay = 10 * 1000;
 config.reconnect_delay = 5000;
 config.connection_data_stale = 1 * 60 * 1000;
@@ -11,11 +11,13 @@ config.chunk_size = 60 * 1000;
 config.doDedup = false;
 config.buildWorkerOn = false;
 
+// ACTION CONCURRENCY
 config.READ_CONCURRENCY = 32;
 config.WRITE_CONCURRENCY = 16;
 config.READ_RANGE_CONCURRENCY = 8;
 config.REPLICATE_CONCURRENCY = 32;
 
+// WEB SERVER
 config.web_address = 'http://127.0.0.1:5001';
 config.web_address_heroku = 'https://noobaa-core.herokuapp.com';
 

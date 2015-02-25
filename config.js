b46ffd231d4c0a7e3fff0d2cef5d3077231d8d45
yaelal <stam@stams-MacBook-Air.local>
2015-02-25 14:53:31 +0200

    cog signalling server port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 // SIGNALING AND ICE (on heroku: ws://noobaa-signaling.herokuapp.com)
-config.address = 'ws://127.0.0.1:5000';
+config.address = 'ws://127.0.0.1:4005';
 config.alive_delay = 10 * 1000;
 config.reconnect_delay = 5000;
 config.connection_data_stale = 1 * 60 * 1000;

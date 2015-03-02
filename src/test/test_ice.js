3c7f0a7a049e80dd281f569b01c237cc9e1a0ad7
yaelal <stam@stams-MacBook-Air.local>
2015-03-02 15:27:44 +0200

    add ws msg test

--- src/test/test_ice.js
+++ src/test/test_ice.js
@@ -227,9 +227,9 @@ describe('on ice message', function() {
 
         // chk onopen sends required message
         socket.ws.onopen();
-        assert.ok(socket.alive_interval);
-        assert.ok(socket.ws.msgsSent[0].indexOf('sigType') >= 0);
-        assert.equal(socket.idInServer,agentId);
+        assert.ok(socket.alive_interval, 'ws onopen issue - keep alive not set');
+        assert.ok(socket.ws.msgsSent[0].indexOf('sigType') >= 0, 'ws onopen issue - msg not sent');
+        assert.equal(socket.idInServer,agentId, 'ws onopen issue - id not set');
 
         // clear keep alive interval and stale conns one
         clearInterval(socket.alive_interval);
@@ -237,13 +237,13 @@ describe('on ice message', function() {
 
         // chk that when id msg received from server it is ignored for agent
         socket.ws.onmessage({sigType: 'id', id: 'ytytytytyt'});
-        assert.equal(socket.idInServer,agentId);
+        assert.equal(socket.idInServer,agentId, 'id sent from server should be ignored by agent');
 
         // test send message to peer
         ice_lib.sendWSMessage(socket, '6y6y6yy6', '7777', {df: 'hh', ba: '434'});
-        assert.ok(socket.ws.msgsSent[1].indexOf('sigType') >= 0);
-        assert.ok(socket.ws.msgsSent[1].indexOf('ice') >= 0);
-        assert.ok(socket.ws.msgsSent[1].indexOf('434') >= 0);
+        assert.ok(socket.ws.msgsSent[1].indexOf('sigType') >= 0, 'sendWSMessage issue - didnt send with sigType');
+        assert.ok(socket.ws.msgsSent[1].indexOf('ice') >= 0, 'sendWSMessage issue - didnt send with ice');
+        assert.ok(socket.ws.msgsSent[1].indexOf('434') >= 0, 'sendWSMessage issue - didnt send with body');
 
         // chk that when request of no specific type arrives it goes to the right method
         socket.handleRequestMethod = function(ws, message) {
@@ -251,7 +251,7 @@ describe('on ice message', function() {
             ws.msgsRec.push(message);
         };
         socket.ws.onmessage({sigType: 'blat', requestId: 'blat'});
-        assert.ok(socket.ws.msgsRec[0].sigType === 'blat');
+        assert.ok(socket.ws.msgsRec[0].sigType === 'blat', 'onmessage issue - didnt receive');
 
         // get ice connection to peer of which and ice connection already exists
         var peerId = '9i9i9i9';
@@ -262,18 +262,18 @@ describe('on ice message', function() {
             usedBy: {}
         };
         ice_lib.initiateIce(p2p_context, socket, peerId, true, reqId);
-        assert.ok(socket.icemap[reqId].peerId === peerId);
-        assert.ok(socket.icemap[reqId].isInitiator);
-        assert.ok(socket.icemap[reqId].requestId === reqId);
-        assert.ok(p2p_context.iceSockets[peerId].usedBy[reqId] === 1);
+        assert.ok(socket.icemap[reqId].peerId === peerId,'initiateIce issue - peer id not marked');
+        assert.ok(socket.icemap[reqId].isInitiator,'initiateIce issue - initiator not marked');
+        assert.ok(socket.icemap[reqId].requestId === reqId,'initiateIce issue - req id not marked');
+        assert.ok(p2p_context.iceSockets[peerId].usedBy[reqId] === 1,'initiateIce issue - used by not marked');
 
         // get ice connection to peer - new
         peerId = '9i9i888';
         reqId = '888';
         assert(!socket.icemap[reqId], 'request used before ?');
         ice_lib.initiateIce(p2p_context, socket, peerId, true, reqId);
-        assert.ok(socket.ws.msgsSent[2].indexOf('sigType') >= 0);
-        assert.ok(socket.ws.msgsSent[2].indexOf('accept') >= 0);
+        assert.ok(socket.ws.msgsSent[2].indexOf('sigType') >= 0,'cant find accept msg sig type');
+        assert.ok(socket.ws.msgsSent[2].indexOf('accept') >= 0,'cant find accept msg type');
         assert(socket.icemap[reqId].peerConn.offers === 1, 'ice offer not called during initiateIce');
 
         // candidate ws msg handling
@@ -294,6 +294,10 @@ describe('on ice message', function() {
         assert(socket.icemap[reqId].peerConn.remoteDesk === 2, 'ice handle answer not called');
         assert(global.RTCSessionDescription.calledTwice, 'global.RTCSessionDescription not called');
 
+        // test onopen of dataChannel
+        socket.icemap[reqId].dataChannel.onopen();
+        assert.ok(p2p_context.iceSockets[peerId].usedBy[reqId] === 1,'channel open issue - used by not marked');
+
         // test peer conn onicecandidate
         socket.icemap[reqId].peerConn.onicecandidate({candidate: {
                 sdpMLineIndex: 1,
@@ -301,21 +305,125 @@ describe('on ice message', function() {
                 candidate: 'fgfgfhf'
             }
         });
-        var intervalChk = setInterval(function() {
-            if (socket.ws.msgsSent.length >= 4) {
-                assert.ok(socket.ws.msgsSent[3].indexOf('sigType') >= 0);
-                assert.ok(socket.ws.msgsSent[3].indexOf('ice') >= 0);
-                clearInterval(intervalChk);
+        var int1 = setInterval(function() {
+            if (socket.ws.msgsSent[3]) {
+                assert.ok(socket.ws.msgsSent[3].indexOf('sigType') >= 0,'cant find onicecandidate msg sig type');
+                clearInterval(int1); int1 = null;
             }
         },1000);
-
-        socket.icemap[reqId].dataChannel.onopen();
-        assert.ok(p2p_context.iceSockets[peerId].usedBy[reqId] === 1);
+        int1.unref();
 
         // check close
         ice_lib.closeIce(socket, reqId, socket.icemap[reqId].dataChannel);
         assert.ok(socket.icemap[reqId].done, 'close ice didnt mark req as done is: '+socket.icemap[reqId].done);
         assert.ok(!p2p_context.iceSockets[peerId].usedBy[reqId], 'close ice didnt delete used by');
+        ice_lib.closeSignaling(socket);
+
+    });
+
+    it('test ws req', function(done) {
+
+        var int1, int2, int3;
+        var ice_lib;
+        var socket;
+
+        function clearIntervalsAndEnd() {
+            try {clearInterval(int1);} catch (err) {}
+            try {clearInterval(int2);} catch (err) {}
+            try {clearInterval(int3);} catch (err) {}
+
+            ice_lib.closeSignaling(socket);
+        }
+
+        try {
+            // create mock for web sockets
+            var wsMock = function(addr) {
+                this.ws = {
+                    msgsSent: [],
+                    msgsRec: []
+                };
+
+                this.ws.send = function(msg) {
+                    this.msgsSent.push(msg);
+                    //console.log('test ws req got MSG '+require('util').inspect(this.msgsSent));
+                };
+
+                console.log('creating dummy ws for addr '+addr);
+                return this.ws;
+            };
+
+            var rewire = require('rewire');
+            ice_lib = rewire('../util/ice_lib');
+            ice_lib.__set__({
+                'WebSocket': wsMock
+            });
+
+            var peerId = 'rgg4g5545hh45h5';
+
+            var p2p_context = {
+                iceSockets: {}
+            };
+
+            // open mock web socket to signaling server
+            socket = ice_lib.setup(function(p2p_context, channel, event) {
+                console.log('got event '+event.data+' from peer '+channel.peerId);
+            }, null, function(channel, message) {
+                console.log('got message '+message+' from peer '+channel.peerId);
+            });
+
+            // clear keep alive interval and stale conns one
+            clearInterval(socket.alive_interval); socket.alive_interval = null;
+            clearInterval(socket.stale_conn_interval); socket.stale_conn_interval = null;
+
+            // add socket to context
+            p2p_context.wsClientSocket = {ws_socket: socket, lastTimeUsed: new Date().getTime(), interval: {}};
+
+            // test send ws request
+            var requestId;
+
+            int1 = setInterval(function() {
+                if (socket.conn_defer) {
+                    socket.conn_defer.resolve();
+                    clearInterval(int1); int1 = null;
+                }
+            },1000);
+            int1.unref();
+
+            int2 = setInterval(function() {
+                if (socket.ws.msgsSent[0]) {
+                    assert.ok(socket.ws.msgsSent[0].indexOf('sigType') >= 0,'cant find sendWSRequest msg sig type');
+                    assert.ok(socket.ws.msgsSent[0].indexOf('replicate') >= 0,'cant find sendWSRequest msg type replicate');
+                    var msgSent = JSON.parse(socket.ws.msgsSent[0]);
+                    requestId = msgSent.requestId;
+                    clearInterval(int2); int2 = null;
+                }
+            },1000);
+            int2.unref();
+
+            int3 = setInterval(function() {
+                if (requestId && socket.action_defer[requestId]) {
+                    assert.ok(ice_lib.isRequestAlive(p2p_context, peerId, requestId),'request is not alive');
+                    socket.action_defer[requestId].resolve('{status: 200}');
+                    clearInterval(int3); int3 = null;
+                }
+            },1000);
+            int3.unref();
+
+            return Q.fcall(function() {
+                return ice_api.sendWSRequest(p2p_context, peerId, {method: 'GET', path: '/replicate/block/4t5t54t4', body: 'my body'});
+            }).then(function(res) {
+                assert.ok(res.indexOf('200') >= 0, 'sendWSRequest failed res is: '+res);
+                clearIntervalsAndEnd();
+            }).then(null, function(err) {
+                console.error('WS test ws req (sendWSRequest) threw an ex '+err);
+                clearIntervalsAndEnd();
+                assert.fail();
+            }).nodeify(done);
+        } catch (ex) {
+            console.error('WS test ws req threw an ex: '+ex);
+            clearIntervalsAndEnd();
+            assert.fail();
+        }
 
     });
 

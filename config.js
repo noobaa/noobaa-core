3c8d758fa29f3617aff29b49c268a29bdf92537b
yaelal <stam@stams-MacBook-Air.local>
2015-03-03 14:18:37 +0200

    cog timeout for response from 10 min to 2 min

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config.chunk_size = 60 * 1000;
 config.doDedup = false;
 config.buildWorkerOn = false;
 config.connection_default_timeout = 60 * 1000;
-config.get_response_default_timeout = 10 * 60 * 1000;
+config.get_response_default_timeout = 2 * 60 * 1000;
 
 config.use_ws_when_possible = true;
 config.use_ice_when_possible = true;

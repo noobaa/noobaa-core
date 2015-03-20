692c0d38cc10bda146e1f31fc137ed21ba2d92a2
gu <guymguym@gmail.com>
2015-03-20 05:07:25 +0200

    remove exception on finalize for reentrancy

--- src/server/object_mapper.js
+++ src/server/object_mapper.js
@@ -223,7 +223,10 @@ function finalize_object_parts(bucket, obj, parts) {
                 dbg.log3('going to finalize block ', block);
                 if (!block.building) {
                     dbg.log0("ERROR block not in building mode ", block);
-                    throw new Error('block not in building mode');
+                    // for reentrancy we avoid failing if the building mode
+                    //  was already unset, and just go ahead with calling build_chunks
+                    // which will handle it all.
+                    // throw new Error('block not in building mode');
                 }
                 var chunk = chunk_by_id[block.chunk];
                 if (!chunk) {

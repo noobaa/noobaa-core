19945484bc3af9ee8c422e17f1ed88c1d8dcccc5
gu <guymguym@gmail.com>
2015-03-22 21:45:19 +0200

    make s3 rest server generate ssl certificate

--- src/s3/app.js
+++ src/s3/app.js
@@ -1,10 +1,18 @@
 'use strict';
+
+var _ = require('lodash');
+var Q = require('q');
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
 var dbg = require('noobaa-util/debug_module')(__filename);
+var pem = require('pem');
 
-var app = function(params) {
+
+module.exports = s3_app;
+
+
+function s3_app(params) {
 
 
     var express = require('express'),
@@ -24,32 +32,29 @@ var app = function(params) {
     //     }));
     // });
 
-    app.use(function (req, res, next) {
-        dbg.log0('Time:', Date.now(),req.headers,req.query,req.query.prefix,req.query.delimiter);
-      if (req.headers.host)
-      {
-          if (req.headers.host.indexOf(params.bucket)===0)
-          {
-              req.url = req.url.replace('/','/'+params.bucket+'/');
-              dbg.log0('update path with bucket name',req.url,req.path,params.bucket);
-          }
-          if (req.query.prefix && req.query.delimiter){
-              if (req.query.prefix.indexOf(req.query.delimiter)<0)
-              {
-                  req.url = req.url.replace(req.query.prefix,req.query.prefix+req.query.delimiter);
-                  dbg.log0('updated prefix',req.url,req.query.prefix);
-                  req.query.prefix = req.query.prefix+req.query.delimiter;
+    app.use(function(req, res, next) {
+        dbg.log0('Time:', Date.now(), req.headers, req.query, req.query.prefix, req.query.delimiter);
+        if (req.headers.host) {
+            if (req.headers.host.indexOf(params.bucket) === 0) {
+                req.url = req.url.replace('/', '/' + params.bucket + '/');
+                dbg.log0('update path with bucket name', req.url, req.path, params.bucket);
+            }
+            if (req.query.prefix && req.query.delimiter) {
+                if (req.query.prefix.indexOf(req.query.delimiter) < 0) {
+                    req.url = req.url.replace(req.query.prefix, req.query.prefix + req.query.delimiter);
+                    dbg.log0('updated prefix', req.url, req.query.prefix);
+                    req.query.prefix = req.query.prefix + req.query.delimiter;
 
-              }
-          }
-          if (req.url.indexOf('/'+params.bucket+'?')>=0){
-              //req.url = req.url.replace(params.bucket,params.bucket+'/');
-              dbg.log0('updated bucket name with delimiter',req.url);
-          }
+                }
+            }
+            if (req.url.indexOf('/' + params.bucket + '?') >= 0) {
+                //req.url = req.url.replace(params.bucket,params.bucket+'/');
+                dbg.log0('updated bucket name with delimiter', req.url);
+            }
 
 
-      }
-      next();
+        }
+        next();
     });
 
     app.disable('x-powered-by');
@@ -67,22 +72,49 @@ var app = function(params) {
     app.delete('/:bucket/:key(*)', controllers.bucketExists, controllers.deleteObject);
 
     return {
-        serve: function(done) {
-            var privateKey = fs.readFileSync(params.s3_key);
-            var certificate = fs.readFileSync(params.s3_cert);
-            http.createServer(app.handle.bind(app)).listen(params.port);
-            https.createServer({
-                key: privateKey,
-                cert: certificate
-            }, app.handle.bind(app)).listen(params.ssl_port, params.hostname, function(err) {
-                return done(err, params.hostname, params.port);
-            }).on('error', function(err) {
-                return done(err);
-            });
+        serve: function() {
+            var certificate;
+            return Q.nfcall(pem.createCertificate.bind(pem), {
+                    days: 365 * 100,
+                    selfSigned: true
+                })
+                .then(function(certificate_arg) {
+                    certificate = certificate_arg;
+                    return Q.Promise(function(resolve, reject) {
+                        dbg.log0('Starting HTTP', params.port);
+                        http.createServer(app.handle.bind(app))
+                            .listen(params.port, function(err) {
+                                dbg.log0('HTTP listen', err);
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                    });
+                })
+                .then(function() {
+                    return Q.Promise(function(resolve, reject) {
+                        dbg.log0('Starting HTTPS', params.ssl_port);
+                        https.createServer({
+                                key: certificate.serviceKey,
+                                cert: certificate.certificate
+                            }, app.handle.bind(app))
+                            .listen(params.ssl_port, function(err) {
+                                dbg.log0('HTTPS listen', err);
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                    });
+                })
+                .then(null, function(err) {
+                    dbg.log0('SERVE ERROR', err.stack || err);
+                });
         }
     };
 
 
 };
-
-module.exports = app;

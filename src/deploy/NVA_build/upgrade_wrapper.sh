4a43f7070bd01a9283e1f5ad2be9adbb5f19d052
nimrod-becker <nimrod.becker@noobaa.com>
2015-05-25 18:36:17 +0300

    Upgrade package wrapper
    Install / Run command on pre and post of source extraction

---
+++ src/deploy/NVA_build/upgrade_wrapper.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+. /root/node_modules/noobaa-core/src/deploy/NVA_build/deploy_base.sh
+
+function pre_upgrade {
+  #yum install -y lsof
+  
+  #fix iptables
+  #TODO: CHECK if rules already exist, is so skip this part
+  iptables -I INPUT 1 -i eth0 -p tcp --dport 443 -j ACCEPT
+  /sbin/iptables -A INPUT -m limit --limit 15/minute -j LOG --log-level 2 --log-prefix "Dropped by firewall: "
+  /sbin/iptables -A OUTPUT -m limit --limit 15/minute -j LOG --log-level 2 --log-prefix "Dropped by firewall: "
+  service iptables save
+
+  #set locale
+  echo "export LC_ALL=C" >> ~/.bashrc
+
+  #helper aliases in bashrc
+  echo "alias services_status='/usr/bin/supervisorctl status'" >> ~/.bashrc
+  echo "alias ll='ls -lha'" >> ~/.bashrc
+  echo "alias less='less -R'" >> ~/.bashrc
+  echo "alias zless='zless -R'" >> ~/.bashrc
+  echo "export GREP_OPTIONS='--color=auto'" >> ~/.bashrc
+
+}
+
+function post_upgrade {
+  #.env changes
+  cp -f ${CORE_DIR}/src/deploy/NVA_build/env.orig ${CORE_DIR}/.env
+
+  #NooBaa supervisor services configuration changes
+  cp -f ${CORE_DIR}/src/deploy/NVA_build/supervisord.orig /etc/rc.d/init.d/supervisord
+	chmod 777 /etc/rc.d/init.d/supervisord
+}
+
+
+case $1 in
+  pre)
+    pre_upgrade
+    ;;
+  post)
+    post_upgrade
+    ;;
+  *)
+    exit 1
+    ;;
+esac

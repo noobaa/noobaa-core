bf4741033f673190dc25c259425f6886ca7d3784
nimrod-becker <nimrod.becker@noobaa.com>
2015-06-08 13:19:02 +0300

    finish system collection

--- src/server/stats_aggregator.js
+++ src/server/stats_aggregator.js
@@ -6,9 +6,11 @@ var Q = require('q');
 var promise_utils = require('../util/promise_utils');
 var dbg = require('noobaa-util/debug_module')(__filename);
 var config = require('../../config.js');
-var systems_server = require('./systems_server.js');
+var system_server = require('./system_server.js');
 var bucket_server = require('./bucket_server.js');
 var tier_server = require('./tier_server.js');
+var account_server = require('./account_server');
+var node_server = require('./node_server');
 var object_mapper = require('./object_mapper.js');
 
 
@@ -28,7 +30,11 @@ var SYS_STATS = {
         roles: 0,
         allocated_space: 0,
         used_space: 0,
-        associated_nodes: 0,
+        total_space: 0,
+        associated_nodes: {
+            on: 0,
+            off: 0,
+        },
     }]
 };
 
@@ -44,10 +50,10 @@ var NODES_STATS = {
 };
 
 var OPS_STATS = {
-  deletes: 0,
-  writes: 0,
-  reads: 0,
-  list_objects: 0,
+    deletes: 0,
+    writes: 0,
+    reads: 0,
+    list_objects: 0,
 };
 
 /*
@@ -62,9 +68,9 @@ function systems_stats() {
     sys_stats.version = process.env.CURRENT_VERSION || 'Unknown';
     sys_stats.agent_version = process.env.AGENT_VERSION || 'Unknown';
 
-    Q.fcall(function() {
+    return Q.fcall(function() {
             //Get ALL systems
-            return systems_server.list_systems({
+            return system_server.list_systems({
                 account: {
                     is_support: true
                 }
@@ -73,10 +79,10 @@ function systems_stats() {
         .then(function(res) {
             sys_stats.count = res.systems.length;
             //Per each system fill out the needed info
-            _.each(res.systems, function(sys, i) {
-                Q.fcall(function() {
+            return Q.all(_.map(res.systems, function(sys, i) {
+                return Q.fcall(function() {
                         return tier_server.list_tiers({
-                            system: sys.id,
+                            system: sys,
                         });
                     })
                     .then(function(tiers) {
@@ -89,20 +95,52 @@ function systems_stats() {
                         sys_stats.systems[i].buckets = buckets.length;
                         return object_mapper.chunks_and_objects_count(sys.id);
                     })
-                    .then(function(res) {
-                        sys_stats.systems[i].chunks = res.chunks_num;
-                        sys_stats.systems[i].objects = res.objects_num;
+                    .then(function(objects) {
+                        sys_stats.systems[i].chunks = objects.chunks_num;
+                        sys_stats.systems[i].objects = objects.objects_num;
+                        return account_server.get_system_accounts({
+                            system: sys
+                        });
                     })
-                    .then(function() {
+                    .then(function(accounts) {
+                        sys_stats.systems[i].roles = accounts.length;
+                        return system_server.read_system({
+                            system: sys
+                        });
+                    })
+                    .then(function(res_system) {
+                        sys_stats.systems[i].allocated_space = res_system.storage.alloc;
+                        sys_stats.systems[i].used_space = res_system.storage.used;
+                        sys_stats.systems[i].total_space = res_system.storage.total;
+                        sys_stats.systems[i].associated_nodes.on = res_system.nodes.online;
+                        sys_stats.systems[i].associated_nodes.off = res_system.nodes.count - res_system.nodes.online;
                         return sys_stats;
+                    })
+                    .then(null, function(err) {
+                        dbg.log0('Error in collecting systems stats, skipping current sampling point');
                     });
-            });
+            }));
         });
 }
 
 //Collect nodes related stats and usage
 function nodes_stats() {
+    //var nodes_stats = NODES_STATS.prototype;
+    /*
+    use node_server.group_nodes
+    add the following info avg allocation
+    avg_usage
+    OS
 
+
+    return Q.fcall(function() {
+            //Get ALL systems
+            //return node_server.group_nodes({});
+        })
+        .then(function(res) {})
+        .then(null, function(err) {
+            dbg.log0('Error in collecting nodes stats, skipping current sampling point');
+        });*/
 }
 
 //Collect operations related stats and usage
@@ -118,9 +156,7 @@ if ((config.central_stats.send_stats !== 'true') &&
     promise_utils.run_background_worker({
         name: 'system_server_stats_aggregator',
         batch_size: 1,
-        time_since_last_build: 60000, // TODO increase...
-        building_timeout: 300000, // TODO increase...
-        delay: (60 * 60 * 1000), //60m
+        delay: (60 * 1000), //60m //TODO:: up to (60*60*1000) //60m
 
         //Run the system statistics gatheting
         run_batch: function() {
@@ -151,6 +187,7 @@ if ((config.central_stats.send_stats !== 'true') &&
                     dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', 'SENDING');
                 })
                 .then(function() {
+                    console.log('NB:: stats collected ', stats_payload.sys_stats);
                     dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', 'END');
                 });
         }

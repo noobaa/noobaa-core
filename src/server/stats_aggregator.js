c1f328e1cd58017f6a152c12aa47d6a015681795
nimrod-becker <nimrod.becker@noobaa.com>
2015-06-16 18:36:06 +0300

    Fix systems stats collection

--- src/server/stats_aggregator.js
+++ src/server/stats_aggregator.js
@@ -3,6 +3,7 @@
 
 var _ = require('lodash');
 var Q = require('q');
+var util = require('util');
 var promise_utils = require('../util/promise_utils');
 var dbg = require('noobaa-util/debug_module')(__filename);
 var config = require('../../config.js');
@@ -31,49 +32,62 @@ module.exports = stats_aggregator;
 /*
  * Stats Collction API
  */
+var SYSTEM_STATS_DEFAULTS = {
+    installid: '',
+    version: '',
+    agent_version: '',
+    count: 0,
+    systems: [{
+        tiers: 0,
+        buckets: 0,
+        chunks: 0,
+        objects: 0,
+        roles: 0,
+        allocated_space: 0,
+        used_space: 0,
+        total_space: 0,
+        associated_nodes: 0,
+        properties: {
+            on: 0,
+            off: 0,
+        },
+    }],
+};
 
 //Collect systems related stats and usage
 function get_systems_stats(req) {
-    var sys_stats;
-
+    var sys_stats = _.defaults(SYSTEM_STATS_DEFAULTS);
     sys_stats.installid = ''; //TODO: Actual uniq & persistent installtion ID
     sys_stats.version = process.env.CURRENT_VERSION || 'Unknown';
     sys_stats.agent_version = process.env.AGENT_VERSION || 'Unknown';
 
-    dbg.log('NB1:: starting get_systems_stats');
     return Q.fcall(function() {
             //Get ALL systems
             return system_server.list_systems({
-                account: {
-                    is_support: true
-                }
+                /*is_support: true,*/
+                get_id: true,
             });
         })
         .then(function(res) {
-            dbg.log('NB1:: got all systems');
             sys_stats.count = res.systems.length;
             //Per each system fill out the needed info
             return Q.all(_.map(res.systems, function(sys, i) {
                 return Q.fcall(function() {
-                        dbg.log('NB1:: running on system #', i);
                         return tier_server.list_tiers({
                             system: sys,
                         });
                     })
                     .then(function(tiers) {
-                        dbg.log('NB1:: got tiers');
                         sys_stats.systems[i].tiers = tiers.length;
                         return bucket_server.list_buckets({
                             system: sys
                         });
                     })
                     .then(function(buckets) {
-                        dbg.log('NB1:: got buckets');
-                        sys_stats.systems[i].buckets = buckets.length;
+                        sys_stats.systems[i].buckets = buckets.buckets.length;
                         return object_mapper.chunks_and_objects_count(sys.id);
                     })
                     .then(function(objects) {
-                        dbg.log('NB1:: got objects');
                         sys_stats.systems[i].chunks = objects.chunks_num;
                         sys_stats.systems[i].objects = objects.objects_num;
                         return account_server.get_system_accounts({
@@ -81,20 +95,18 @@ function get_systems_stats(req) {
                         });
                     })
                     .then(function(accounts) {
-                        dbg.log('NB1:: got accounts');
                         sys_stats.systems[i].roles = accounts.length;
                         return system_server.read_system({
                             system: sys
                         });
                     })
                     .then(function(res_system) {
-                        dbg.log('NB1:: got read system');
                         sys_stats.systems[i].allocated_space = res_system.storage.alloc;
                         sys_stats.systems[i].used_space = res_system.storage.used;
                         sys_stats.systems[i].total_space = res_system.storage.total;
-                        sys_stats.systems[i].associated_nodes.on = res_system.nodes.online;
-                        sys_stats.systems[i].associated_nodes.off = res_system.nodes.count - res_system.nodes.online;
-                        dbg.log('NB1:: FULL sys stats ', sys_stats);
+                        sys_stats.systems[i].associated_nodes = res_system.nodes.count;
+                        sys_stats.systems[i].properties.on = res_system.nodes.online;
+                        sys_stats.systems[i].properties.off = res_system.nodes.count - res_system.nodes.online;
                         return sys_stats;
                     })
                     .then(null, function(err) {
@@ -105,9 +117,20 @@ function get_systems_stats(req) {
         });
 }
 
+/*var NODES_STATS_DEFAULTS = {
+  count: 0,
+  avg_allocation: 0,
+  avg_usage: 0,
+  avg_uptime: 0,
+  os: {
+          win: 0,
+          osx: 0,
+      },
+};*/
+
 //Collect nodes related stats and usage
 function get_nodes_stats(req) {
-    //var nodes_stats;
+    //var nodes_stats = _.defaults(NODES_STATS_DEFAULTS);
     /*
     use node_server.group_nodes
     add the following info avg allocation
@@ -142,24 +165,23 @@ function get_all_stats(req) {
     dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', 'BEGIN');
     return Q.fcall(function() {
             dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', '  Collecting Systems');
-            return get_systems_stats();
+            return get_systems_stats({});
         })
         .then(function(sys_stats) {
             dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', '  Collecting Nodes');
             stats_payload.sys_stats = sys_stats;
-            return get_nodes_stats();
+            return get_nodes_stats({});
         })
         .then(function(node_stats) {
             dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', '  Collecting Ops');
             stats_payload.node_stats = node_stats;
-            return get_ops_stats();
+            return get_ops_stats({});
         })
         .then(function(ops_stats) {
             stats_payload.ops_stats = ops_stats;
             dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', 'SENDING');
         })
         .then(function() {
-            dbg.log('NB:: stats collected ', stats_payload.sys_stats);
             dbg.log2('SYSTEM_SERVER_STATS_AGGREGATOR:', 'END');
             return stats_payload;
         })
@@ -180,7 +202,7 @@ if ((config.central_stats.send_stats !== 'true') &&
         batch_size: 1,
         time_since_last_build: 60000, // TODO increase...
         building_timeout: 300000, // TODO increase...
-        delay: (60 * 1000), //60m //TODO:: up to (60*60*1000) //60m
+        delay: (60 * 60 * 1000), //60m
 
         //Run the system statistics gatheting
         run_batch: function() {

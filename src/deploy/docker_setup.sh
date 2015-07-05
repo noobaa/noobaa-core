bc9ed007a3a6b9c8af1ee591b34dcbcc346d2738
tamireran <tamir.eran@gmail.com>
2015-07-05 12:23:32 +0300

    weave setup for dockers (missing from previous commit)

--- src/deploy/docker_setup.sh
+++ src/deploy/docker_setup.sh
@@ -3,12 +3,38 @@
 mkdir /noobaa
 cd /noobaa
 sudo apt-get update
-sudo apt-get install -y docker.io unzip
+#sudo apt-get install -y docker.io unzip
+
+sudo apt-get install -y linux-image-generic-lts-trusty
+sudo apt-get install -y apparmor unzip
+wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker "root"
+
+network=$(curl http://metadata/computeMetadata/v1/instance/attributes/network -H "Metadata-Flavor: Google")
+router=$(curl http://metadata/computeMetadata/v1/instance/attributes/router -H "Metadata-Flavor: Google")
+
+sudo curl -L git.io/weave -o /usr/local/bin/weave
+sudo chmod a+x /usr/local/bin/weave
+sudo weave create-bridge
+sudo ip addr add dev weave 10.2.0.$network/16
+sudo chmod 777 /etc/default/docker
+sudo echo 'DOCKER_OPTS="--bridge=weave --fixed-cidr=10.2.'$network'.0/24"' >>/etc/default/docker
+sudo restart docker
+if [ $router != "0.0.0.0" ] ; then
+	echo "here"
+	sudo weave launch $router #104.155.2.195
+else
+	echo "not here"
+	sudo weave launch
+fi
+
+
 curl http://noobaa-download.s3.amazonaws.com/DockerClientAmazon.zip >DockerClientAmazon.zip
 unzip DockerClientAmazon.zip
 #read metadata variable with env name. Defined in the gcloud.js (unique per instance)
 #replace ENV_PLACEHOLDER in Dockerfile and run-agent with current env
 ENV_NAME=$(curl http://metadata/computeMetadata/v1/instance/attributes/env -H "Metadata-Flavor: Google")
+
 echo '+++++ENV::::' $ENV_NAME
 if [ ${#ENV_NAME} -eq 0 ]; then
 	#for amazon we will set it (for now), by replacing the env_name string from ec2_deploy_agents.js

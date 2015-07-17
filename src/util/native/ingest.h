f01f74958d89a6e62978a8a0bbccc136b5852989
gu <guymguym@gmail.com>
2015-07-17 19:58:21 +0300

    mostly whitespace

--- src/util/native/ingest.h
+++ src/util/native/ingest.h
@@ -29,7 +29,7 @@ private:
     {
     }
 
-    ~Ingest()
+    virtual ~Ingest()
     {
     }
 
--- src/util/native/tpool.h
+++ src/util/native/tpool.h
@@ -9,15 +9,20 @@ class ThreadPool : public node::ObjectWrap
 public:
     explicit ThreadPool(int nthreads);
     virtual ~ThreadPool();
+
+    void set_nthreads(int nthreads);
+    int get_nthreads() {
+        return _nthreads;
+    }
+
     struct Job
     {
         virtual ~Job() {}
         virtual void run() = 0; // called from pooled thread
         virtual void done() = 0; // called on event loop
     };
-    void set_nthreads(int nthreads);
-    int get_nthreads() { return _nthreads; }
     void submit(Job* job);
+
 private:
     struct ThreadSpec
     {
@@ -26,12 +31,14 @@ private:
         ThreadSpec(ThreadPool* tp, int i)
             : tpool(tp)
             , index(i)
-            {}
+        {
+        }
     };
     void thread_main(ThreadSpec& spec);
     void done_cb();
     static void thread_main_uv(void* arg);
     static void job_done_uv(uv_async_t* async, int);
+
 private:
     MutexCond _mutex;
     int _nthreads;
@@ -40,8 +47,10 @@ private:
     std::list<Job*> _run_queue;
     std::list<Job*> _done_queue;
     int _refs;
+
 public:
     static void setup(v8::Handle<v8::Object> exports);
+
 private:
     static v8::Persistent<v8::Function> _ctor;
     static NAN_METHOD(new_instance);

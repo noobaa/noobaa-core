f2a32ad4144afd9ce5b424733dd2254f02ffe6c9
Eran Tamir <tamir.eran@gmail.com>
2015-08-04 16:13:57 +0300

    Update noobaa_service_installer.sh

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 /usr/local/noobaa/node_modules/forever-service/bin/forever-service delete noobaa_local_service
+sleep 20
 /usr/local/noobaa/node_modules/forever-service/bin/forever-service install noobaa_local_service -s /usr/local/noobaa/noobaa_local_service.sh -p /usr/local/noobaa/node_modules/forever/bin/  -f " -c 'bash'" --start

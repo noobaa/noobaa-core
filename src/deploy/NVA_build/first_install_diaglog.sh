802468c1c173085fb836f2ab54b531958f561941
nimrod-becker <nimrod.becker@noobaa.com>
2015-08-09 12:36:42 +0300

    "UX" Changes to the first install dialog
    1) no cancel
    2) add “use Up/Down to navigate”
    3) color the IP at the end (last msg)

--- src/deploy/NVA_build/first_install_diaglog.sh
+++ src/deploy/NVA_build/first_install_diaglog.sh
@@ -37,8 +37,8 @@ function run_wizard {
 
   local current_ip=$(ifconfig eth0  |grep 'inet addr' | cut -f 2 -d':' | cut -f 1 -d' ')
 
-  dialog --colors --backtitle "NooBaa First Install" --menu "Current IP: \Z4\Zb${current_ip}\Zn .\nChoose IP
- Assignment:" 12 55 2 1 "Static IP" 2 "Dynamic IP" 2> choice
+  dialog --colors --nocancel --backtitle "NooBaa First Install" --menu "Current IP: \Z4\Zb${current_ip}\Zn .\nChoose IP
+ Assignment (Use \Z4\ZbUp/Down\Zn to navigate):" 12 55 2 1 "Static IP" 2 "Dynamic IP" 2> choice
 
   local dynamic=$(cat choice)
   local rc
@@ -46,8 +46,8 @@ function run_wizard {
   #Choice of Static IP
   if [ "${dynamic}" -eq "1" ]; then
     #sudo echo "First Install Chose Static IP" >> /var/log/noobaa_deploy.log
-    dialog --colors --backtitle "NooBaa First Install" --title "IP Configuration" --form "\nPlease enter the IP address to be used by \Z5\ZbNooBaa\Zn.\nThis
- IP address should be associated with noobaa.local in the DNS." 12 75 4 "IP Address:" 1 1 "" 1 25 25 30 "Netmask:" 2 1 "" 2 25 25 30 "Default Gateway:" 3 1 "" 3 25 25 30 2> answer_ip
+    dialog --colors --nocancel --backtitle "NooBaa First Install" --title "IP Configuration" --form "\nPlease enter the IP address to be used by \Z5\ZbNooBaa\Zn.\nThis
+ IP address should be associated with noobaa.local in the DNS (Use \Z4\ZbUp/Down\Zn to navigate)." 12 70 4 "IP Address:" 1 1 "" 1 25 25 30 "Netmask:" 2 1 "" 2 25 25 30 "Default Gateway:" 3 1 "" 3 25 25 30 2> answer_ip
 
     local ip=$(head -1 answer_ip)
     local mask=$(head -2 answer_ip | tail -1)
@@ -60,9 +60,9 @@ function run_wizard {
     local ok_gw=$?
 
     while [ "${ok_ip}" -ne "0" ] || [ "${ok_mask}" -ne "0" ] || [ "${ok_gw}" -ne "0" ]; do
-      dialog --colors --backtitle "NooBaa First Install" --title "IP Configuration" --form "\Z1The Provided IP, Netmask or GW are not valid\Zn\n\nPlease enter
+      dialog --colors --nocancel --backtitle "NooBaa First Install" --title "IP Configuration" --form "\Z1The Provided IP, Netmask or GW are not valid\Zn\n\nPlease enter
  the IP address to be used by \Z5\ZbNooBaa\Zn.\nThis IP address should be associated with noobaa.local in the
- DNS." 12 75 4 "IP Address:" 1 1 "${ip}" 1 25 25 30 "Netmask:" 2 1 "${mask}" 2 25 25 30 "Default Gateway:" 3 1 "${gw}" 3 25 25 30 2> answer_ip
+ DNS (Use \Z4\ZbUp/Down\Zn to navigate)." 12 70 4 "IP Address:" 1 1 "${ip}" 1 25 25 30 "Netmask:" 2 1 "${mask}" 2 25 25 30 "Default Gateway:" 3 1 "${gw}" 3 25 25 30 2> answer_ip
 
     ip=$(head -1 answer_ip)
     mask=$(head -2 answer_ip | tail -1)
@@ -83,15 +83,15 @@ function run_wizard {
     sudo bash -c "echo 'NETMASK=${mask}'>> /etc/sysconfig/network-scripts/ifcfg-eth0"
     sudo bash -c "echo 'GATEWAY=${gw}'>> /etc/sysconfig/network-scripts/ifcfg-eth0"
 
-    dialog --colors --backtitle "NooBaa First Install" --title "DNS Configuration" --form "\nPlease supply a primary and secodnary
- DNS servers." 12 65 4 "Primary DNS:" 1 1 "" 1 25 25 30 "Secondary DNS:" 2 1 "" 2 25 25 30 2> answer_dns
+    dialog --colors --nocancel --backtitle "NooBaa First Install" --title "DNS Configuration" --form "\nPlease supply a primary and secodnary
+DNS servers (Use \Z4\ZbUp/Down\Zn to navigate)." 12 80 4 "Primary DNS:" 1 1 "" 1 25 25 30 "Secondary DNS:" 2 1 "" 2 25 25 30 2> answer_dns
 
     local dns1=$(head -1 answer_dns)
     local dns2=$(tail -1 answer_dns)
 
     while [ "${dns1}" == "" ]; do
-      dialog --colors --backtitle "NooBaa First Install" --title "DNS Configuration" --form "\nPlease supply a primary and secodnary
-   DNS servers." 12 65 4 "Primary DNS:" 1 1 "${dns1}" 1 25 25 30 "Secondary DNS:" 2 1 "{dns2}" 2 25 25 30 2> answer_dns
+      dialog --colors --nocancel --backtitle "NooBaa First Install" --title "DNS Configuration" --form "\nPlease supply a primary and secodnary
+   DNS servers (Use \Z4\ZbUp/Down\Zn to navigate)." 12 65 4 "Primary DNS:" 1 1 "${dns1}" 1 25 25 30 "Secondary DNS:" 2 1 "${dns2}" 2 25 25 30 2> answer_dns
       dns1=$(head -1 answer_dns)
       #sudo echo "First Install adding dns ${dns}" >> /var/log/noobaa_deploy.log
       dns2=$(tail -1 answer_dns)
@@ -109,21 +109,21 @@ function run_wizard {
     sudo bash -c "echo 'BOOTPROTO=dhcp' >> /etc/sysconfig/network-scripts/ifcfg-eth0"
   fi
   sudo service network restart
-  dialog --colors --backtitle "NooBaa First Install" --title "Hostname Configuration" --form "\nPlease supply a hostname for this
+  dialog --colors --nocancel --backtitle "NooBaa First Install" --title "Hostname Configuration" --form "\nPlease supply a hostname for this
  \Z5\ZbNooBaa\Zn installation." 12 65 4 "Hostname:" 1 1 "" 1 25 25 30 2> answer_host
 
   local host=$(cat answer_host)
   rc=$(sudo sysctl kernel.hostname=${host})
   #sudo echo "First Install configure hostname ${host}, sysctl rc ${rc}" >> /var/log/noobaa_deploy.log
 
-  dialog --colors --backtitle "NooBaa First Install" --infobox "Finalizing \Z5\ZbNooBaa\Zn first install..." 4 40 ; sleep 2
+  dialog --colors --nocancel --backtitle "NooBaa First Install" --infobox "Finalizing \Z5\ZbNooBaa\Zn first install..." 4 40 ; sleep 2
 
   end_wizard
 }
 
 function end_wizard {
   local current_ip=$(ifconfig eth0  |grep 'inet addr' | cut -f 2 -d':' | cut -f 1 -d' ')
-  dialog --colors --backtitle "NooBaa First Install" --title '\Z5\ZbNooBaa\Zn is Ready' --msgbox "\n\Z5\ZbNooBaa\Zn was configured and is ready to use. You can access https://${current_ip}:8443 to start using your system." 7 65
+  dialog --colors --nocancel --backtitle "NooBaa First Install" --title '\Z5\ZbNooBaa\Zn is Ready' --msgbox "\n\Z5\ZbNooBaa\Zn was configured and is ready to use. You can access \Z5\Zbhttps://${current_ip}:8443\Zn to start using your system." 7 65
   #date > ${FIRST_INSTALL_MARK}
   clear
 

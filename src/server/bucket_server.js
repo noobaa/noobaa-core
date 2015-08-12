d3d565bec9f6e971d1b1ad19ad02a5cad541628f
nimrod-becker <nimrod.becker@noobaa.com>
2015-08-12 12:48:03 +0300

    Cloud Sync Policy API Implementation

--- src/server/bucket_server.js
+++ src/server/bucket_server.js
@@ -4,7 +4,7 @@
 var _ = require('lodash');
 var Q = require('q');
 var db = require('./db');
-
+var dbg = require('noobaa-util/debug_module')(__filename);
 
 /**
  *
@@ -17,6 +17,11 @@ var bucket_server = {
     update_bucket: update_bucket,
     delete_bucket: delete_bucket,
     list_buckets: list_buckets,
+
+    //Cloud Sync policies
+    cloud_sync_policies: cloud_sync_policies,
+    delete_cloud_sync: delete_cloud_sync,
+    set_cloud_sync: set_cloud_sync
 };
 
 module.exports = bucket_server;
@@ -29,7 +34,7 @@ module.exports = bucket_server;
  *
  */
 function create_bucket(req) {
-    return resolve_tiering(req.system.id,req.rpc_params.tiering)
+    return resolve_tiering(req.system.id, req.rpc_params.tiering)
         .then(function(tiering) {
             var info = _.pick(req.rpc_params, 'name');
             info.system = req.system.id;
@@ -38,8 +43,8 @@ function create_bucket(req) {
             }
             return db.Bucket.create(info);
         })
-        .then(function(bucket){
-            console.log('create bucket:',bucket);
+        .then(function(bucket) {
+            console.log('create bucket:', bucket);
             return db.ActivityLog.create({
                 system: req.system,
                 level: 'info',
@@ -84,7 +89,7 @@ function read_bucket(req) {
  *
  */
 function update_bucket(req) {
-    return resolve_tiering(req.system.id,req.rpc_params.tiering)
+    return resolve_tiering(req.system.id, req.rpc_params.tiering)
         .then(function(tiering) {
             var updates = {};
             if (req.rpc_params.new_name) {
@@ -115,7 +120,7 @@ function delete_bucket(req) {
     return Q.when(db.Bucket
             .findOneAndUpdate(get_bucket_query(req), updates)
             .exec())
-        .then(function(bucket_info){
+        .then(function(bucket_info) {
             return db.ActivityLog.create({
                 system: req.system,
                 level: 'info',
@@ -151,7 +156,96 @@ function list_buckets(req) {
         });
 }
 
+/**
+ *
+ * CLOUD_SYNC_POLICIES
+ *
+ */
+function cloud_sync_policies(req) {
+    dbg.log3('cloud_sync_policies');
+    var reply = [];
+    return Q.when(db.Bucket
+            .find({
+                system: req.system.id,
+                deleted: null,
+            })
+            .exec())
+        .then(function(buckets) {
+            _.each(buckets, function(bucket, i) {
+                if (bucket.cloud_sync.endpoint) {
+                    reply.push({
+                        name: bucket.name,
+                        policy: {
+                            endpoint: bucket.cloud_sync.endpoint,
+                            access_keys: bucket.cloud_sync.access_keys,
+                            schedule: bucket.cloud_sync.schedule,
+                            last_sync: bucket.cloud_sync.last_sync
+                        }
+                    });
+                }
+            });
+            return {
+                cloud_sync_policies: reply
+            };
+        });
+}
 
+/**
+ *
+ * DELETE_CLOUD_SYNC
+ *
+ */
+function delete_cloud_sync(req) {
+    dbg.log2('delete_cloud_sync:', req.rpc_params.name, 'on', req.system.id);
+    var updates = {
+        cloud_sync: {}
+    };
+    return Q.when(db.Bucket
+            .find({
+                system: req.system.id,
+                name: req.rpc_params.name,
+                deleted: null,
+            })
+            .exec())
+        .then(function(bucket) {
+            dbg.log3('delete_cloud_sync: delete on bucket', bucket);
+            return Q.when(db.Bucket
+                .findOneAndUpdate(get_bucket_query(req), updates)
+                .exec());
+        });
+}
+
+/**
+ *
+ * SET_CLOUD_SYNC
+ *
+ */
+function set_cloud_sync(req) {
+    dbg.log2('set_cloud_sync:', req.rpc_params.name, 'on', req.system.id, 'with', req.rpc_params.policy);
+    var updates = {
+        cloud_sync: {
+            endpoint: req.rpc_params.policy.endpoint,
+            access_keys: {
+                access_key: req.rpc_params.policy.access_keys.acces_key,
+                secret_key: req.rpc_params.policy.access_keys.secret_key
+            },
+            schedule: req.rpc_params.policy.schedule,
+        }
+    };
+    return Q.when(db.Bucket
+            .find({
+                system: req.system.id,
+                name: req.rpc_params.name,
+                deleted: null,
+            })
+            .exec())
+        .then(function(bucket) {
+            return Q.when(db.Bucket
+                .findOneAndUpdate(get_bucket_query(req), updates)
+                .exec());
+        })
+        .thenResolve();
+}
 
 // UTILS //////////////////////////////////////////////////////////
 
@@ -174,7 +268,7 @@ function get_bucket_info(bucket) {
     return reply;
 }
 
-function resolve_tiering(system_id,tiering) {
+function resolve_tiering(system_id, tiering) {
     if (!tiering) return Q.resolve();
     return Q.when(db.Tier
             .find({

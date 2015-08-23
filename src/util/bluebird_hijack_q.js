bcff5c7770dd6a1922838171ca546729dc906bff
gu <guymguym@gmail.com>
2015-08-24 02:54:50 +0300

    comment for bluebird_hijack_q

--- src/util/bluebird_hijack_q.js
+++ src/util/bluebird_hijack_q.js
@@ -4,6 +4,20 @@ var _ = require('lodash');
 var Q = require('q');
 var Promise = require('bluebird');
 
+/**
+ *
+ * TODO this module is a temporary hack and should be removed once we switch existing code to bluebird
+ *
+ * this module hijacks Q promises and replaces it with bluebird promises
+ * since Q are extremely slow and waste a lot of cpu.
+ * see this:
+ * https://github.com/petkaantonov/bluebird/issues/381
+ * https://github.com/petkaantonov/bluebird/issues/63
+ *
+ * notice that we do not handle calls to the function Q() itself because
+ * we did not hijack the module reference, only its content.
+ *
+ */
 module.exports = Promise;
 
 // Promise.longStackTraces();
@@ -39,9 +53,9 @@ Q.delay = Promise.delay;
 Q.timeout = Promise.timeout;
 Q.all = Promise.all;
 Q.allSettled = Promise.settle;
-Q.nfcall = function(/*func, args...*/) {
+Q.nfcall = function( /*func, args...*/ ) {
     var args = _.toArray(arguments);
-    return Promise.fromNode(function(callback){
+    return Promise.fromNode(function(callback) {
         var fargs = args.slice(1);
         fargs.push(callback);
         args[0].apply(null, fargs);
@@ -49,7 +63,7 @@ Q.nfcall = function(/*func, args...*/) {
 };
 Q.ninvoke = function() {
     var args = _.toArray(arguments);
-    return Promise.fromNode(function(callback){
+    return Promise.fromNode(function(callback) {
         var fargs = args.slice(2);
         fargs.push(callback);
         args[0][args[1]].apply(args[0], fargs);
@@ -57,7 +71,7 @@ Q.ninvoke = function() {
 };
 Q.npost = function() {
     var args = _.toArray(arguments);
-    return Promise.fromNode(function(callback){
+    return Promise.fromNode(function(callback) {
         var fargs = args[2] ? args[2].slice() : [];
         fargs.push(callback);
         args[0][args[1]].apply(args[0], fargs);

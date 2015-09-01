f0f2ec3d9e3d98b79b4e2495a90921eea800b991
nimrod-becker <nimrod.becker@noobaa.com>
2015-09-01 10:43:22 +0300

    prevent registering servers when there's no need

--- src/server/server_rpc.js
+++ src/server/server_rpc.js
@@ -5,7 +5,7 @@ var api = require('../api');
 // using the api rpc instance.
 // we can certainly create a new one, but we don't need for now.
 var server_rpc = api.rpc;
-var bg_workers_rpc =  api.new_rpc();
+var bg_workers_rpc = api.new_rpc();
 
 // base rpc address for server is redirected locally
 server_rpc.base_address = 'fcall://fcall';
@@ -15,19 +15,22 @@ bg_workers_rpc.base_address = bg_workers_rpc.get_default_base_address('backgroun
 
 module.exports = {
     server_rpc: server_rpc,
-    bg_workers_rpc: bg_workers_rpc
+    bg_workers_rpc: bg_workers_rpc,
+    register_servers: register_servers,
 };
 
-var options = {
-    // setup the rpc authorizer to check the request auth_token
-    authorize: require('./auth_server').authorize,
-};
+function register_servers() {
+    var options = {
+        // setup the rpc authorizer to check the request auth_token
+        authorize: require('./auth_server').authorize,
+    };
 
-server_rpc.register_service(api.schema.auth_api, require('./auth_server'), options);
-server_rpc.register_service(api.schema.account_api, require('./account_server'), options);
-server_rpc.register_service(api.schema.system_api, require('./system_server'), options);
-server_rpc.register_service(api.schema.tier_api, require('./tier_server'), options);
-server_rpc.register_service(api.schema.node_api, require('./node_server'), options);
-server_rpc.register_service(api.schema.bucket_api, require('./bucket_server'), options);
-server_rpc.register_service(api.schema.object_api, require('./object_server'), options);
-server_rpc.register_service(api.schema.stats_api, require('./stats_aggregator'), options);
+    server_rpc.register_service(api.schema.auth_api, require('./auth_server'), options);
+    server_rpc.register_service(api.schema.account_api, require('./account_server'), options);
+    server_rpc.register_service(api.schema.system_api, require('./system_server'), options);
+    server_rpc.register_service(api.schema.tier_api, require('./tier_server'), options);
+    server_rpc.register_service(api.schema.node_api, require('./node_server'), options);
+    server_rpc.register_service(api.schema.bucket_api, require('./bucket_server'), options);
+    server_rpc.register_service(api.schema.object_api, require('./object_server'), options);
+    server_rpc.register_service(api.schema.stats_api, require('./stats_aggregator'), options);
+}
--- src/server/web_server.js
+++ src/server/web_server.js
@@ -158,6 +158,7 @@ function use_exclude(path, middleware) {
 /////////
 
 // register RPC services and transports
+require('./server_rpc').register_servers();
 var server_rpc = require('./server_rpc').server_rpc;
 server_rpc.register_http_transport(app);
 // server_rpc.register_n2n_transport();

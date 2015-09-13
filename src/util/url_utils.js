f04f2a42abfb8ce86263b9683b463fe8f8f56c87
gu <guymguym@gmail.com>
2015-09-13 07:29:16 +0300

    small fix make url quick_parse to mostly match url.parse

--- src/util/url_utils.js
+++ src/util/url_utils.js
@@ -1,14 +1,14 @@
 'use strict';
 
+var _ = require('lodash');
 var url = require('url');
-var assert = require('assert');
 var querystring = require('querystring');
 
 module.exports = {
     quick_parse: quick_parse,
 };
 
-var QUICK_PARSE_REGEXP = /^\s*(\w+\:)(\/\/)?(([^\:\/\[\]]+)|\[([a-fA-F0-9\:\.]+)\])(\:\d+)?(\/[^?#]*)?(\?[^\#]*)?(\#.*)?\s*$/;
+var QUICK_PARSE_REGEXP = /^\s*(\w+\:)?(\/\/)?(([^:/\[\]]*)|\[([a-fA-F0-9:.]*)\])?(\:\d*)?(\/[^?#]*)?(\?[^\#]*)?(\#.*)?\s*$/;
 
 /**
  * parse url string much faster than url.parse() - reduce the time to 1/10.
@@ -28,10 +28,10 @@ function quick_parse(url_string, parse_query_string) {
     u.protocol = match[1] || null;
     u.slashes = match[2] ? true : null;
     u.hostname = match[4] || match[5] || '';
-    u.port = match[6] ? parseInt(match[6].slice(1), 10) : null;
+    u.port = match[6] && match[6].slice(1) || null;
     u.host = (match[3] || '') + (match[6] || '');
     u.pathname = match[7] || null;
-    u.search = match[8] || null;
+    u.search = match[8] || '';
     u.query = match[8] ? match[8].slice(1) : null;
     u.path = (match[7] + '') + (match[8] || '');
     u.hash = match[9] || null;
@@ -46,13 +46,28 @@ function quick_parse(url_string, parse_query_string) {
 
 
 function benchmark() {
-    var testing_url = "http://127.0.0.1:4545/";
+    var testing_url = process.argv[2] || "http://127.0.0.1:4545/";
     var url_parse_res = url.parse(testing_url, true);
     var quick_parse_res = quick_parse(testing_url, true);
     console.log('\nurl.parse(\"' + testing_url + '\") = ', url_parse_res);
     console.log('\nquick_parse(\"' + testing_url + '\") = ', quick_parse_res);
-    assert(url.format(url_parse_res) === testing_url);
-    assert(url.format(quick_parse_res) === testing_url);
+    console.log(' ');
+    _.forIn(url_parse_res, function(v1, k) {
+        var v2 = quick_parse_res[k];
+        if (!_.isEqual(v1, v2)) {
+            console.log('!!! Bad value quick_parse()',
+                k + ': ' + JSON.stringify(v2),
+                'expected', JSON.stringify(v1));
+        }
+    });
+    var url_parse_fmt = url.format(url_parse_res);
+    var quick_parse_fmt = url.format(quick_parse_res);
+    if (url_parse_fmt !== testing_url) {
+        console.log('!!! Bad format(url.parse) =', url_parse_fmt, 'expected', testing_url);
+    }
+    if (quick_parse_fmt !== testing_url) {
+        console.log('!!! Bad format(quick_parse) =', quick_parse_fmt, 'expected', testing_url);
+    }
     var tests = [
         function test_url_parse() {
             return url.parse(testing_url, true);

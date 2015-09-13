4161d36608645cc386beda2befbf720d1eb5df79
gu <guymguym@gmail.com>
2015-09-13 05:18:31 +0300

    url quick parse return Url object, and set fields, even faster.

--- src/util/url_utils.js
+++ src/util/url_utils.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var url = require('url');
+var assert = require('assert');
 var querystring = require('querystring');
 
 module.exports = {
     quick_parse: quick_parse,
 };
 
-var QUICK_PARSE_REGEXP = /^(\w+\:)(\/\/)?([^\:\/\[\]]+|\[[a-fA-F0-9\:\.]+\])(\:\d+)?(\/[^?#]*)?(\?[^\#]*)?(\#.*)?$/;
+var QUICK_PARSE_REGEXP = /^\s*(\w+\:)(\/\/)?(([^\:\/\[\]]+)|\[([a-fA-F0-9\:\.]+)\])(\:\d+)?(\/[^?#]*)?(\?[^\#]*)?(\#.*)?\s*$/;
 
 /**
  * parse url string much faster than url.parse() - reduce the time to 1/10.
@@ -20,34 +22,37 @@ var QUICK_PARSE_REGEXP = /^(\w+\:)(\/\/)?([^\:\/\[\]]+|\[[a-fA-F0-9\:\.]+\])(\:\
  */
 function quick_parse(url_string, parse_query_string) {
     var match = url_string.match(QUICK_PARSE_REGEXP);
-    if (!match) return;
-    var u = {
-        href: url_string,
-        protocol: match[1] || null,
-        slashes: match[2] ? true : null,
-        hostname: match[3] || '',
-        port: match[4] ? parseInt(match[4].slice(1), 10) : null,
-        host: (match[3] || '') + (match[4] || ''),
-        pathname: match[5] || null,
-        search: match[6] || null,
-        query: match[6] ? match[6].slice(1) : null,
-        path: (match[5] + '') + (match[6] || ''),
-        hash: match[7] || null,
-        // auth: null,
-    };
+    var u = new url.Url();
+    if (!match) return u;
+    u.href = url_string;
+    u.protocol = match[1] || null;
+    u.slashes = match[2] ? true : null;
+    u.hostname = match[4] || match[5] || '';
+    u.port = match[6] ? parseInt(match[6].slice(1), 10) : null;
+    u.host = (match[3] || '') + (match[6] || '');
+    u.pathname = match[7] || null;
+    u.search = match[8] || null;
+    u.query = match[8] ? match[8].slice(1) : null;
+    u.path = (match[7] + '') + (match[8] || '');
+    u.hash = match[9] || null;
+    // u.auth = null;
     if (parse_query_string && u.query) {
         u.query = querystring.parse(u.query);
+    } else {
+        u.query = {};
     }
     return u;
 }
 
 
 function benchmark() {
-    var url = require('url');
-    var assert = require('assert');
     var testing_url = "http://127.0.0.1:4545/";
-    assert(testing_url === url.format(url.parse(testing_url, true)));
-    assert(testing_url === url.format(quick_parse(testing_url, true)));
+    var url_parse_res = url.parse(testing_url, true);
+    var quick_parse_res = quick_parse(testing_url, true);
+    console.log('\nurl.parse(\"' + testing_url + '\") = ', url_parse_res);
+    console.log('\nquick_parse(\"' + testing_url + '\") = ', quick_parse_res);
+    assert(url.format(url_parse_res) === testing_url);
+    assert(url.format(quick_parse_res) === testing_url);
     var tests = [
         function test_url_parse() {
             return url.parse(testing_url, true);
@@ -58,7 +63,7 @@ function benchmark() {
     ];
     for (var t = 0; t < tests.length; ++t) {
         var test = tests[t];
-        console.log(test.name);
+        console.log('\nbenchmarking', test.name, '...');
         var count = 0;
         var start = Date.now();
         var now = start;
@@ -78,8 +83,8 @@ function benchmark() {
         } while (now - start < 5000);
         speed = (count * 1000 / (now - start)).toFixed(0);
         console.log('\tOVERALL times per second:', speed);
-        console.log(' ');
     }
+    console.log('\ndone.\n');
 }
 
 if (require.main === module) {

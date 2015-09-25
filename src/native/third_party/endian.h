8e1c7baad9b3705da601bf2129bd54b7d59d1984
gu <guymguym@gmail.com>
2015-09-25 03:38:36 +0300

    fix endian macros for windows

--- src/native/third_party/endian.h
+++ src/native/third_party/endian.h
@@ -65,38 +65,38 @@
 
 #	if BYTE_ORDER == LITTLE_ENDIAN
 
-#		define htobe16(x) htons(x)
+#		define htobe16(x) _byteswap_uint16(x)
 #		define htole16(x) (x)
-#		define be16toh(x) ntohs(x)
+#		define be16toh(x) _byteswap_uint16(x)
 #		define le16toh(x) (x)
 
-#		define htobe32(x) htonl(x)
+#		define htobe32(x) _byteswap_uint32(x)
 #		define htole32(x) (x)
-#		define be32toh(x) ntohl(x)
+#		define be32toh(x) _byteswap_uint32(x)
 #		define le32toh(x) (x)
 
-#		define htobe64(x) htonll(x)
+#		define htobe64(x) _byteswap_uint64(x)
 #		define htole64(x) (x)
-#		define be64toh(x) ntohll(x)
+#		define be64toh(x) _byteswap_uint64(x)
 #		define le64toh(x) (x)
 
 #	elif BYTE_ORDER == BIG_ENDIAN
 
 		/* that would be xbox 360 */
 #		define htobe16(x) (x)
-#		define htole16(x) __builtin_bswap16(x)
+#		define htole16(x) _byteswap_uint16(x)
 #		define be16toh(x) (x)
-#		define le16toh(x) __builtin_bswap16(x)
+#		define le16toh(x) _byteswap_uint16(x)
 
 #		define htobe32(x) (x)
-#		define htole32(x) __builtin_bswap32(x)
+#		define htole32(x) _byteswap_uint32(x)
 #		define be32toh(x) (x)
-#		define le32toh(x) __builtin_bswap32(x)
+#		define le32toh(x) _byteswap_uint32(x)
 
 #		define htobe64(x) (x)
-#		define htole64(x) __builtin_bswap64(x)
+#		define htole64(x) _byteswap_uint64(x)
 #		define be64toh(x) (x)
-#		define le64toh(x) __builtin_bswap64(x)
+#		define le64toh(x) _byteswap_uint64(x)
 
 #	else
 

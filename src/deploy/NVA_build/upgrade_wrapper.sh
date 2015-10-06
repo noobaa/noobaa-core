9beebc4029cf9ec41f44ff8df13253e25728f52a
nimrod-becker <nimrod.becker@noobaa.com>
2015-10-06 09:38:36 +0300

    Login Message

--- src/deploy/NVA_build/upgrade_wrapper.sh
+++ src/deploy/NVA_build/upgrade_wrapper.sh
@@ -130,7 +130,13 @@ function post_upgrade {
   fi
   echo "${AGENT_VERSION_VAR}" >> ${CORE_DIR}/.env
 
-  echo -e "Welcome to your \x1b[0;35;40mNooBaa\x1b[0m server,\n" > /etc/issue
+	echo  " _   _            ______ "   > /etc/issue
+	echo  "| \\ | |           | ___ \\"    >> /etc/issue
+	echo  "|  \\| | ___   ___ | |_/ / __ _  __ _ " >> /etc/issue
+	echo  "| . \` |/ _ \\ / _ \\| ___ \\/ _\` |/ _\` |" >> /etc/issue
+	echo  "| |\\  | (_) | (_) | |_/ / (_| | (_| |" >> /etc/issue
+	echo  "\\_| \\_/\\___/ \\___/\\____/ \\__,_|\\__,_|" >> /etc/issue
+  echo -e "\nWelcome to your \x1b[0;35;40mNooBaa\x1b[0m server,\n" >> /etc/issue
   echo -e "You can use \x1b[0;32;40mnoobaa/Passw0rd\x1b[0m login to configure IP,DNS,GW and Hostname" >>/etc/issue
   deploy_log "NooBaa supervisor services configuration changes"
   #NooBaa supervisor services configuration changes

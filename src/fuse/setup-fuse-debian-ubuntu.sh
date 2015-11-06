1f0ebe0e4fa0bbd04e38d6480a3897da49cfe04d
gu <guymguym@gmail.com>
2015-11-06 14:56:24 +0200

    small changes to fuse scripts

--- src/fuse/setup-fuse-debian-ubuntu.sh
+++ src/fuse/setup-fuse-debian-ubuntu.sh
@@ -1,6 +1,7 @@
-# Fuse setup script for debian
-
+echo "Setting up s3fs-fuse for debian/ubuntu ..."
 pushd /tmp
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+pushd s3fs-fuse
 
 #optional settings for sources.
 #echo deb http://ftp.ca.debian.org/debian testing main >/etc/apt/sources.list
@@ -10,9 +11,6 @@ pushd /tmp
 #apt-get update
 
 sudo apt-get install libfuse-dev libcurl4-openssl-dev libxml++2.6-dev libssl-dev git automake make g++ -y
-
-git clone https://github.com/s3fs-fuse/s3fs-fuse.git
-pushd s3fs-fuse
 
 ./autogen.sh
 ./configure

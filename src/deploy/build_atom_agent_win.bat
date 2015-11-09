35716945cc7e956b56218f25fbbbc74982245446
tamireran <tamir.eran@gmail.com>
2015-11-09 11:52:19 +0200

    typo fix

--- src/deploy/build_atom_agent_win.bat
+++ src/deploy/build_atom_agent_win.bat
@@ -37,7 +37,7 @@ REM remove irrelevant packages
 type package.json  | findstr /v forever-service | findstr /v jsonwebtoken | findstr /v forever | findstr /v googleapis | findstr /v gulp | findstr /v bower | findstr /v bootstrap | findstr /v browserify | findstr /v rebuild | findstr /v nodetime| findstr /v newrelic > package.json_s
 del /Q package.json
 rename package.json_s package.json
-copy ../../binding.gyp .
+copy ..\..\binding.gyp .
 call npm install -dd
 xcopy /Y/I/E ..\..\build\Release .\Release
 del /Q node.exe

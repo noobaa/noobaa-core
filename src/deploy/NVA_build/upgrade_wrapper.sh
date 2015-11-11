498f31569466b2658ab77b18ab986384b99ae7f7
tamireran <tamir.eran@gmail.com>
2015-11-11 14:00:48 +0200

    fix build path for nvm

--- src/deploy/NVA_build/upgrade_wrapper.sh
+++ src/deploy/NVA_build/upgrade_wrapper.sh
@@ -114,7 +114,7 @@ function pre_upgrade {
 	cp ${EXTRACTION_PATH}/noobaa-core/build/public/node-v4.2.2-linux-x64.tar.xz /tmp/
 	tar -xJf /tmp/node-v4.2.2-linux-x64.tar.xz -C /tmp/nvm422 --strip-components 1
 	mkdir -p ~/.nvm/versions/node/v4.2.2/
-	mv /tmp/nvm422/* ~/.nvm/versions/node/v4.2.2/
+	mv /tmp/v4.2.2/* ~/.nvm/versions/node/v4.2.2/
 	export NVM_DIR="$HOME/.nvm"
 	. "$NVM_DIR/nvm.sh"
 	export PATH=~/.nvm/versions/node/v4.2.2/bin:$PATH

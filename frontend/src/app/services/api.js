d7e4152de162e95c279f9da585e72c24e46b0786
nb-ohad <mitrani.ohad@gmail.com>
2015-11-25 19:06:50 +0200

    Fix default web server address

--- frontend/src/app/services/api.js
+++ frontend/src/app/services/api.js
@@ -1,5 +1,5 @@
 import env from 'env';
 import { rpc, Client } from 'nb-api';
 
-rpc.base_address = env.serverAddress || 'localhost:5001';
+rpc.base_address = env.serverAddress || 'ws://127.0.0.1:5001';
 export default Object.assign(new Client(), { rpc });

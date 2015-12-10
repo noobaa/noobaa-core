2aea079a85685134d000e7880a406a85e24d05ff
Guy <guymguym@gmail.com>
2015-12-10 04:47:32 +0200

    fix 3 read path bugs with large files (writes were not affected)
    1. Incorrect end offset used for buffer slicing in
    combine_parts_buffers_in_range caused nodes buffer to return a 0 length
    buffer (weird behavior that was fixed in newer nodes versions)
    
    2. Implementing offset alignment cannot use javascript bitwise which is
    limited to 32bits.
    
    3. Mapper sanity used bitwise on offsets to make sure they are integers
    and have no fraction (just paranoia I guess).

--- src/util/range_utils.js
+++ src/util/range_utils.js
@@ -5,9 +5,9 @@ var size_utils = require('./size_utils');
 
 module.exports = {
     intersection: intersection,
-    align_down_bitwise: align_down_bitwise,
-    align_up_bitwise: align_up_bitwise,
-    truncate_range_end_to_boundary_bitwise: truncate_range_end_to_boundary_bitwise,
+    align_down: align_down,
+    align_up: align_up,
+    truncate_range_end_to_boundary: truncate_range_end_to_boundary,
     human_range: human_range,
 };
 
@@ -26,30 +26,29 @@ function intersection(start1, end1, start2, end2) {
 
 
 /**
- * align the given offset down with boundary 1<<nbits
+ * align the given offset down with boundary
+ * NOTE! cannot use javascript bitwise on offsets as it is limited to 32bits
  */
-function align_down_bitwise(offset, nbits) {
-    var mask_up = ((~0) >>> nbits << nbits);
-    return offset & mask_up;
+function align_down(offset, boundary) {
+    return offset - (offset % boundary);
 }
 
 
 /**
- * align the given offset up with boundary 1<<nbits
+ * align the given offset up with boundary
+ * NOTE! cannot use javascript bitwise on offsets as it is limited to 32bits
  */
-function align_up_bitwise(offset, nbits) {
-    var mask_up = ((~0) >>> nbits << nbits);
-    var size = (1 << nbits);
-    return (offset + size - 1) & mask_up;
+function align_up(offset, boundary) {
+    return align_down(offset + boundary - 1, boundary);
 }
 
 
 /**
- * return the end of the range aligned down to the closest boundary of 1<<nbits
+ * return the end of the range aligned down to the closest boundary
  * but only if such boundary exists between start and end.
  */
-function truncate_range_end_to_boundary_bitwise(start, end, nbits) {
-    var new_end = align_down_bitwise(end, nbits);
+function truncate_range_end_to_boundary(start, end, boundary) {
+    var new_end = align_down(end, boundary);
     return (new_end > start) ? new_end : end;
 }
 

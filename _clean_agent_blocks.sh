b53366584519128647d095d5fb6176b9aa2cc7e5
Guy <guymguym@gmail.com>
2015-12-17 17:12:58 +0200

    added clean_test_nodes_and_data.sh that also removes the test nodes, and extracted the general are_you_sure.sh script

--- _clean_agent_blocks.sh
+++
@@ -1,5 +0,0 @@
-echo "disk usage of agent_storage:"
-du -sh agent_storage/*
-echo "deleting agent_storage/*/blocks ..."
-rm -rf agent_storage/*/blocks
-echo "done."
---
+++ are_you_sure.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p "1. Are you sure? [y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "You managed to abort me. bye."
+    exit 1
+fi
+
+read -p "2. Still sure? [y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "You almost fooled me. bye."
+    exit 1
+fi
+
+read -p "3. Last chance? [y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "You are living on the edge. bye."
+    exit 1
+fi
+
+echo -n "4. You have 3 seconds to abort ... "
+for i in `seq 3`
+do
+    echo -n "$i "
+    sleep 1
+done

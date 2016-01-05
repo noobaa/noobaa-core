5b048f74e719c2baf3487f55eb15d072574b02f9
Guy Margalit <guymguym@gmail.com>
2016-01-05 15:09:28 +0200

    fix the map_vs_object_benchmark to correctly use strings and not consecutive numbers which makes it an array test (thanks Ohad). map is x3-10 faster as was promised.

--- src/test/map_vs_object_benchmark.js
+++ src/test/map_vs_object_benchmark.js
@@ -1,10 +1,15 @@
 'use strict';
 
+test(1);
+test(10);
+test(100);
+test(1000);
 test(10000);
 test(100000);
-// apparently exactly after 114467 items object delete becomes 600x slower
-test(114467);
-test(114468);
+test(1000000);
+test(10000000);
+
+var PREFIX = 'BOOM';
 
 function test(n) {
     var i;
@@ -13,43 +18,53 @@ function test(n) {
     console.log('Testing', n, 'items ...');
     compare('[set]   ', function() {
         for (i = 0; i < n; ++i) {
-            map.set(i, i * 2);
+            map.set(PREFIX + i, i * 2);
         }
     }, function() {
         for (i = 0; i < n; ++i) {
-            obj[i] = i * 2;
+            obj[PREFIX + i] = i * 2;
         }
     });
     compare('[get]   ', function() {
         var sum = 0;
         for (i = 0; i < n; ++i) {
-            sum += map.get(i);
+            sum += map.get(PREFIX + i);
         }
         return sum;
     }, function() {
         var sum = 0;
         for (i = 0; i < n; ++i) {
-            sum += obj[i];
+            sum += obj[PREFIX + i];
         }
         return sum;
     });
     compare('[delete]', function() {
         for (i = 0; i < n; ++i) {
-            map.delete(i);
+            map.delete(PREFIX + i);
         }
     }, function() {
         for (i = 0; i < n; ++i) {
-            delete obj[i];
+            delete obj[PREFIX + i];
         }
     });
     console.log('---------------------------------');
 }
 
 function compare(name, func1, func2) {
-    var time1 = Date.now();
-    func1();
-    var time2 = Date.now();
-    func2();
-    var time3 = Date.now();
-    console.log(name, 'MAP', (time2 - time1) + 'ms', 'OBJ', (time3 - time2) + 'ms');
+    var sum1 = 0;
+    var sum2 = 0;
+    var count = 0;
+    while (sum1 < 100 || sum2 < 100) {
+        var time1 = Date.now();
+        func1();
+        var time2 = Date.now();
+        func2();
+        var time3 = Date.now();
+        sum1 += time2 - time1;
+        sum2 += time3 - time2;
+        count += 1;
+    }
+    console.log(name,
+        'MAP is x' + (sum2 / sum1).toFixed(1) + ' faster than OBJ',
+        '  (' + (sum1 / count).toFixed(6) + ' ms vs. ' + (sum2 / count).toFixed(6) + ' ms)');
 }

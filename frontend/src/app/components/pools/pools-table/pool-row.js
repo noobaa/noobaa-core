b89df54d08ce0a6956385682d74fde4f118e42b2
nb-ohad <mitrani.ohad@gmail.com>
2016-01-21 20:03:48 +0200

    Minor fixs
    - change in bucket policy modal text
    - fix bucket policy modal clear all pools button
    - remove deprecated files
    - add Pool column to node selection table in pool creation
    - add delete action to pools table
    - add a new component: tiled-select
    - remove the broken single select component

--- frontend/src/app/components/pools/pools-table/pool-row.js
+++ frontend/src/app/components/pools/pools-table/pool-row.js
@@ -1,6 +1,7 @@
 import ko from 'knockout';
 import numeral from 'numeral';
 import { formatSize } from 'utils';
+import { deletePool } from 'actions';
 
 export default class PoolRowViewModel {
 	constructor(pool, deleteCandidate) {
@@ -11,35 +12,35 @@ export default class PoolRowViewModel {
 		this.stateIcon = '/fe/assets/icons.svg#pool';
 
 		this.name = ko.pureComputed(
-			() => pool().name
+			() => this.isVisible() && pool().name
 		);
 
 		this.href = ko.pureComputed(
-			() => `/fe/systems/:system/pools/${pool().name}`
+			() => this.isVisible() && `/fe/systems/:system/pools/${pool().name}`
 		);
 
 		this.nodeCount = ko.pureComputed(
-			() => numeral(pool().nodes.count).format('0,0')
+			() => this.isVisible() && numeral(pool().nodes.count).format('0,0')
 		);
 
 		this.onlineCount = ko.pureComputed(
-			() => numeral(pool().nodes.online).format('0,0')
+			() => this.isVisible() && numeral(pool().nodes.online).format('0,0')
 		);
 
 		this.offlineCount = ko.pureComputed(
-			() => numeral(this.nodeCount() - this.onlineCount()).format('0,0')
+			() => this.isVisible() && numeral(this.nodeCount() - this.onlineCount()).format('0,0')
 		);
 
 		this.usage = ko.pureComputed(
-			() => pool().storage ? formatSize(pool().storage.used) : 'N/A'
+			() => this.isVisible() && (pool().storage ? formatSize(pool().storage.used) : 'N/A')
 		);
 
 		this.capacity = ko.pureComputed(
-			() => pool().storage ? formatSize(pool().storage.total) : 'N/A'
+			() => this.isVisible() && (pool().storage ? formatSize(pool().storage.total) : 'N/A')
 		);
 
 		this.allowDelete = ko.pureComputed(
-			() => this.nodeCount() === 0
+			() => this.isVisible() && (pool().nodes.count === 0)
 		);
 
 		this.isDeleteCandidate = ko.pureComputed({
@@ -48,18 +49,20 @@ export default class PoolRowViewModel {
 		});
 
 		this.deleteIcon = ko.pureComputed(
-			() => `/fe/assets/icons.svg#${
-				this.isDeleteCandidate() ? 'trash-opened' : 'trash-closed'
+			() => `/fe/assets/icons.svg#trash-${
+				this.allowDelete() ? 
+					(this.isDeleteCandidate() ? 'opened' : 'closed') : 
+					'disabled'
 			}`
 		);
 
 		this.deleteTooltip = ko.pureComputed( 
-			() => this.allowDelete() ? 'delete pool' : 'pool is not empty'
+			() => this.allowDelete() ? 'delete pool' : 'pool has nodes'
 		);
 	}
 
 	delete() {
-		//deletePool(this.name());
+		deletePool(this.name());
 		this.isDeleteCandidate(false);
 	}	
 }

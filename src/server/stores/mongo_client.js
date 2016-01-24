3511f0f25ccb57230da0c2eab690b4c1cf6d6b21
Guy Margalit <guymguym@gmail.com>
2016-01-24 06:39:26 +0200

    mongo_client - refactor to javascript class

--- src/server/stores/mongo_client.js
+++ src/server/stores/mongo_client.js
@@ -4,55 +4,58 @@
 var P = require('../../util/promise');
 var mongodb = require('mongodb');
 
-module.exports = new MongoClient(); // singleton
+class MongoClient {
 
-function MongoClient() {
-    this.db = null; // will be set once connected
-    this.url =
-        process.env.MONGODB_URL ||
-        process.env.MONGOHQ_URL ||
-        process.env.MONGOLAB_URI ||
-        'mongodb://127.0.0.1/nbcore';
-    this.config = {
-        promiseLibrary: P,
-        server: {
-            // setup infinit retries to connect
-            reconnectTries: -1,
-            reconnectInterval: 1000,
-            socketOptions: {
-                autoReconnect: true
+    constructor() {
+        this.db = null; // will be set once connected
+        this.url =
+            process.env.MONGODB_URL ||
+            process.env.MONGOHQ_URL ||
+            process.env.MONGOLAB_URI ||
+            'mongodb://127.0.0.1/nbcore';
+        this.config = {
+            promiseLibrary: P,
+            server: {
+                // setup infinit retries to connect
+                reconnectTries: -1,
+                reconnectInterval: 1000,
+                socketOptions: {
+                    autoReconnect: true
+                }
+            },
+            db: {
+                // bufferMaxEntries=0 is required for autoReconnect
+                // see: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/connection_failures/
+                bufferMaxEntries: 0
             }
-        },
-        db: {
-            // bufferMaxEntries=0 is required for autoReconnect
-            // see: http://mongodb.github.io/node-mongodb-native/2.0/tutorials/connection_failures/
-            bufferMaxEntries: 0
-        }
-    };
+        };
+    }
+
+    /**
+     * connect and return the db instance which will handle reconnections.
+     * mongodb_url is optional and by default takes from env or local db.
+     */
+    connect(url) {
+        if (this.promise) return this.promise;
+        if (url) this.url = url;
+        this.promise = this._connect();
+        return this.promise;
+    }
+
+    _connect() {
+        return mongodb.MongoClient.connect(this.url, this.config)
+            .then(db => {
+                console.log('MongoClient: connected', this.url);
+                this.db = db;
+                return db;
+            }, err => {
+                // autoReconnect only works once initial connection is created,
+                // so we need to handle retry in initial connect.
+                console.error('MongoClient: initial connect failed, will retry', err.message);
+                return P.delay(3000).then(() => this._connect());
+            });
+    }
+
 }
 
-
-/**
- * connect and return the db instance which will handle reconnections.
- * mongodb_url is optional and by default takes from env or local db.
- */
-MongoClient.prototype.connect = function(url) {
-    var self = this;
-    if (url) {
-        self.url = url;
-    }
-    if (self.db) return self.db;
-    return mongodb.MongoClient.connect(self.url, self.config)
-        .then(function(db) {
-            console.log('MongoClient: connected', self.url);
-            self.db = db;
-            return db;
-        }, function(err) {
-            // autoReconnect only works once initial connection is created,
-            // so we need to handle retry in initial connect.
-            console.error('MongoClient: initial connect failed, will retry', err.message);
-            return P.delay(3000).then(function() {
-                return self.connect();
-            });
-        });
-};
+module.exports = new MongoClient(); // singleton

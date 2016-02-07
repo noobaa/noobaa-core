59b1efb4eeb5ac4265959ac897e76b7f6f457d95
nimrod-becker <nimrod.becker@noobaa.com>
2016-02-07 13:48:52 +0200

    Remove irrelevant stats from reslove_cloud_info

--- src/server/utils/cloud_sync_utils.js
+++ src/server/utils/cloud_sync_utils.js
@@ -5,7 +5,6 @@ module.exports = {
 };
 
 var _ = require('lodash');
-var moment = require('moment');
 
 /**
  *
@@ -14,17 +13,6 @@ var moment = require('moment');
  */
 function resolve_cloud_sync_info(sync_policy, bucket) {
     if (!_.isEmpty(sync_policy)) {
-        var interval_text = 0;
-        if (sync_policy.policy.schedule < 60) {
-            interval_text = sync_policy.policy.schedule + ' minutes';
-        } else {
-            if (sync_policy.policy.schedule < 60 * 24) {
-                interval_text = sync_policy.policy.schedule / 60 + ' hours';
-            } else {
-                interval_text = sync_policy.policy.schedule / (60 * 24) + ' days';
-            }
-        }
-        bucket.policy_schedule_in_min = interval_text;
         //If sync time is epoch (never synced) change to never synced
         if (sync_policy.paused) {
             bucket.cloud_sync_status = 'NOTSET';
@@ -38,10 +26,7 @@ function resolve_cloud_sync_info(sync_policy, bucket) {
             bucket.cloud_sync_status = 'SYNCING';
         }
         if (sync_policy.policy.last_sync === 0) {
-            bucket.last_sync = 'Waiting for first sync';
             bucket.cloud_sync_status = 'UNSYNCED';
-        } else {
-            bucket.last_sync = moment(sync_policy.policy.last_sync).format('LLL');
         }
     } else {
         bucket.cloud_sync_status = 'NOTSET';

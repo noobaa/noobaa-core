b244200e1922106c4acc3e30b0e6b9bfcab71351
tamireran <tamir.eran@gmail.com>
2016-02-28 00:21:54 +0200

    dockers - support latest versions

--- src/deploy/start_noobaa_docker.sh
+++ src/deploy/start_noobaa_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#saving "lock" file to make sure we start with the next allocated number. 
+#saving "lock" file to make sure we start with the next allocated number.
 #docker ps sometimes not fast enough and returns newly allocated ports as free.
 
 file="/noobaa/port.txt"
@@ -13,9 +13,9 @@ portnum=$(<"$file")
 portnum=$((portnum+1))
 echo $portnum
 #print all current dockers and check for the next available port.
-sudo docker ps | 
-cut -d'>' -f 2|  
+sudo docker ps |
+cut -d'>' -f 2|
 awk 'NR > 0{gsub(/.*->/,"",$1); print $1}' |
-awk 'NR > 1{gsub(/\/tcp,/,"",$1); print $1}'|  
-sort -un |   
-awk -v n=$portnum '$0 < n {next}; $0 == n {n++; next}; {exit}; END { system("sudo echo "n">/noobaa/port.txt") system("sudo docker run -t -p "n":"n" -p "n"2:22 noobaa <ENV_PLACEHOLDER> "n "&")}'
+awk 'NR > 1{gsub(/\/tcp,/,"",$1); print $1}'|
+sort -un |
+awk -v n=$portnum '$0 < n {next}; $0 == n {n++; next}; {exit}; END { system("sudo echo "n">/noobaa/port.txt") system("sudo docker run --net=weave -t -p "n":"n" -p "n"2:22 noobaa <ENV_PLACEHOLDER> "n "&")}'

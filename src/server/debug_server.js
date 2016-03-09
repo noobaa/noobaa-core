50b10af4ebc948c2043538eaccd21050b6693b2d
nimrod-becker <nimrod.becker@noobaa.com>
2016-03-09 17:09:51 +0200

    Return istanbul collector coverage data

--- src/server/debug_server.js
+++ src/server/debug_server.js
@@ -8,7 +8,8 @@
  */
 
 var debug_server = {
-    set_debug_level: set_debug_level
+    set_debug_level: set_debug_level,
+    get_istanbul_collector: get_istanbul_collector
 };
 
 module.exports = debug_server;
@@ -19,5 +20,16 @@ function set_debug_level(req) {
     dbg.log0('Recieved set_debug_level req for level', req.rpc_params.level, 'mod', req.rpc_params.module);
     dbg.set_level(req.rpc_params.level, req.rpc_params.module);
     return;
+}
 
+function get_istanbul_collector(req) {
+    //ONLY applicable during TESTRUN
+    if (!process.env.TESTRUN) {
+        console.error('Coverage only applicable in TESTRUN mode');
+        throw new Error('Coverage only applicable in TESTRUN mode');
+    } else {
+        return {
+            data: new Buffer(global.NOOBAA_COV.toString()),
+        };
+    }
 }

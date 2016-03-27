dd7dda39efee7625c8c8ba782c0856069d48e90e
dannyz <danny.zaken@noobaa.com>
2016-03-27 16:22:19 +0300

    changed test to test for sync every 10 seconds
    instead of waiting for 3 minutes every time, check if sync is
    successful in 10 seconds intervals, and timeout of 3 minutes.

--- src/test/system_tests/test_cloud_sync.js
+++ src/test/system_tests/test_cloud_sync.js
@@ -44,7 +44,7 @@ function authenticate() {
     return P.fcall(function() {
             return client.create_auth_token(auth_params);
         })
-            .delay(1000)
+        .delay(1000)
         .then(function() {
             return target_client.create_auth_token(auth_params);
         });
@@ -54,7 +54,7 @@ function authenticate() {
 function set_cloud_sync(params) {
     var cloud_sync_policy = {
         endpoint: TEST_CTX.target_ip,
-        target_bucket:  TEST_CTX.target_bucket,
+        target_bucket: TEST_CTX.target_bucket,
         access_keys: [{
             access_key: '123',
             secret_key: 'abc'
@@ -74,33 +74,52 @@ function set_cloud_sync(params) {
         });
 }
 
-function compare_object_lists(file_names, expected_len) {
+function compare_object_lists(file_names, fail_msg, expected_len) {
+    let timeout_ms = 3 * 60 * 1000; // 3 wait up to 3 minutes for changes to sync
+    let start_ts = Date.now();
+    let done = false;
     var source_list;
     var target_list;
-    return client.object.list_objects({
-            bucket: TEST_CTX.source_bucket
-        })
-        // get objects list on the source
-        .then(function(source_objects) {
-            source_list = _.map(source_objects.objects, 'key');
-            return target_client.object.list_objects({
-                bucket: TEST_CTX.target_bucket
-            });
-        })
-        .then(function(target_objects) {
-            target_list = _.map(target_objects.objects, 'key');
-            // sort all lists:
-            source_list.sort();
-            target_list.sort();
-            console.log('got source list:', source_list);
-            console.log('got target list:', target_list);
-            assert(target_list.length === source_list.length, 'mismatch between lists length.');
+    return promise_utils.pwhile(
+            () => {
+                let diff = Date.now() - start_ts;
+                return (diff < timeout_ms && !done);
+            },
+            () => {
+                return client.object.list_objects({
+                        bucket: TEST_CTX.source_bucket
+                    })
+                    // get objects list on the source
+                    .then(function(source_objects) {
+                        source_list = _.map(source_objects.objects, 'key');
+                        return target_client.object.list_objects({
+                            bucket: TEST_CTX.target_bucket
+                        });
+                    })
+                    .then((target_objects) => {
+                        target_list = _.map(target_objects.objects, 'key');
+                        // sort all lists:
+                        source_list.sort();
+                        target_list.sort();
+                        if (!_.isUndefined(expected_len)) {
+                            done = (source_list.length === expected_len && target_list.length === expected_len);
+                        } else {
+                            done = (target_list.length === source_list.length);
+                        }
+                    })
+                    .delay(10000); // wait 10 seconds between each check
+            }
+        )
+        .then(function() {
+            assert(target_list.length === source_list.length, fail_msg + ': mismatch between lists length.');
             for (var i = 0; i < source_list.length; i++) {
-                assert(target_list[i] === source_list[i]);
+                assert(target_list[i] === source_list[i], fail_msg + ': mismatch between sorce\\target objects:',
+                    target_list[i], source_list[i]);
             }
             if (!_.isUndefined(expected_len)) {
-                assert(source_list.length === expected_len, 'expected_len is ' + expected_len + '. got source_list.length=' + source_list.length);
+                assert(source_list.length === expected_len, fail_msg + ': expected_len is ' + expected_len + '. got source_list.length=' + source_list.length);
             }
+            console.log('source bucket and target bucket are synced. sync took ~', (Date.now() - start_ts) / 1000, 'seconds');
         });
 }
 
@@ -130,11 +149,13 @@ function main() {
                 deletions: true
             };
             return set_cloud_sync(cloud_sync_params)
-                .then(() => console.log('set cloud_sync with these params:', cloud_sync_params, ' sleeping for 60 seconds'))
-                .delay(60000)
+                .then(() => {
+                    console.log('set cloud_sync with these params:', cloud_sync_params, ' waiting for changes to sync');
+
+                })
                 .then(function() {
                     //check target file list against local
-                    return compare_object_lists(file_names);
+                    return compare_object_lists(file_names, 'sync source to target failed');
                 });
         })
         .then(() => P.all(_.map(file_sizes, ops.generate_random_file)))
@@ -151,11 +172,11 @@ function main() {
                         .delay(1000);
                 });
         })
-        .then(() => console.log('uploaded files to target bucket. waiting for changes to sync for 3 minutes'))
-        .delay(60000 * 2)
+        .then(() => console.log('uploaded files to target bucket. waiting for changes to sync'))
+        // .delay(60000 * 2)
         .then(function() {
             //check target file list against local
-            return compare_object_lists(file_names);
+            return compare_object_lists(file_names, 'sync target additions to source failed');
         })
         .then(function() {
             let i = 0;
@@ -182,8 +203,8 @@ function main() {
             expected_after_del = obj_list.objects.length;
             console.log('waiting for deletions to sync for 3 minutes..');
         })
-        .delay(2 * 60000)
-        .then(() => compare_object_lists(file_names, expected_after_del))
+        // .delay(2 * 60000)
+        .then(() => compare_object_lists(file_names, 'sync deletions from source to target failed', expected_after_del))
         .then(function() {
             // remove cloud_sync policy
             console.log('removing cloud_sync policy');

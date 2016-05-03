13e15a709f6570bab785490b06e0dd0dd989c026
nimrod-becker <nimrod.becker@noobaa.com>
2016-05-03 10:54:11 +0300

    Use remove_program instead of remove_agent

--- src/server/pool_server.js
+++ src/server/pool_server.js
@@ -200,7 +200,7 @@ function delete_cloud_pool(req) {
     dbg.log0('Deleting cloud pool', req.rpc_params.name);
     var pool = find_pool_by_name(req);
 
-    // construct the cloud node name according to convention 
+    // construct the cloud node name according to convention
     let cloud_node_name = 'noobaa-cloud-agent-' + os.hostname() + '-' + pool.name;
     return P.resolve()
         .then(function() {
@@ -214,7 +214,7 @@ function delete_cloud_pool(req) {
                 }
             });
         })
-        .then(() => SupervisorCtl.remove_agent(pool.name))
+        .then(() => SupervisorCtl.remove_program('agent_' + pool.name))
         .then(() => nodes_store.delete_node_by_name({
             system: {
                 _id: req.system._id

b4f2c7b8c40bfc75c64a8dcc6619b16b03c3f9d6
nimrod-becker <nimrod.becker@noobaa.com>
2016-05-09 11:19:12 +0300

    fix require path for system_store

--- src/server/utils/clustering_utils.js
+++ src/server/utils/clustering_utils.js
@@ -11,7 +11,7 @@ exports.is_single_server = is_single_server;
 exports.get_all_cluster_members = get_all_cluster_members;
 
 var _ = require('lodash');
-var system_store = require('./stores/system_store');
+var system_store = require('../stores/system_store');
 var dbg = require('../util/debug_module')(__filename);
 
 

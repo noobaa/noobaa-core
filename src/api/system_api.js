10d959721538b1d6ff5154ce4238a46982ec9a35
jeniawhite <jeniawhite92@gmail.com>
2016-06-09 14:04:23 +0300

    Adding External Log Support

--- src/api/system_api.js
+++ src/api/system_api.js
@@ -458,6 +458,29 @@ module.exports = {
             }
         },
 
+        configure_external_syslog: {
+            method: 'POST',
+            params: {
+                type: 'object',
+                required: ['connection_type'],
+                properties: {
+                    connection_type: {
+                        type: 'string',
+                        enum: ['TCP', 'UDP', 'NONE']
+                    },
+                    address: {
+                        type: 'string'
+                    },
+                    port: {
+                        type: 'number'
+                    }
+                }
+            },
+            auth: {
+                system: 'admin',
+            }
+        },
+
         update_system_certificate: {
             method: 'POST',
             auth: {
@@ -604,6 +627,22 @@ module.exports = {
                 },
                 phone_home_proxy: {
                     type: 'string'
+                },
+                external_syslog_config: {
+                    type: 'object',
+                    required: ['connection_type'],
+                    properties: {
+                        connection_type: {
+                            type: 'string',
+                            enum: ['TCP', 'UDP', 'NONE']
+                        },
+                        address: {
+                            type: 'string'
+                        },
+                        port: {
+                            type: 'number'
+                        }
+                    }
                 },
                 dns_name: {
                     type: 'string'

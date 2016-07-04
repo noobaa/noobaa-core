554948b959d7b22d4df52b94ae896264c46991e2
nb-ohad <ohad.mitrani@noobaa.com>
2016-07-04 16:51:01 +0300

    FE - Fix data table bugs

--- frontend/src/app/components/shared/data-table/column.js
+++ frontend/src/app/components/shared/data-table/column.js
@@ -1,14 +1,22 @@
-import { isObject, toDashedCase } from 'utils';
+import { isObject, noop, toDashedCase } from 'utils';
 
 export default class ColumnViewModel {
     constructor(config) {
         config = isObject(config) ? config : { name: config.toString() };
-        let { name, label = name, template = 'text', sortable = false } = config;
+        let {
+            name,
+            label = name,
+            template = 'text',
+            sortable = false,
+            accessor = noop,
+            css = `${toDashedCase(name)}-col`
+        } = config;
 
         this.name = name;
         this.label = label;
+        this.accessor = accessor;
         this.template = template;
         this.sortable = sortable;
-        this.cellCss = `${toDashedCase(name)}-col`;
+        this.css = css;
     }
 }

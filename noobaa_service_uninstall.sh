f93b8dcee158dda6a9fd1172a209d4b93cbdc1c3
nb-ohad <ohad.mitrani@noobaa.com>
2016-07-21 15:39:48 +0300

    Add empty table message to bucket policedata placement modal
    Also move table in said modal to use the data-table component

--- noobaa_service_uninstall.sh
+++ noobaa_service_uninstall.sh
@@ -3,3 +3,4 @@ echo "Uninstall NooBaa"
 PATH=/usr/local/noobaa:$PATH;/usr/local/noobaa/node_modules/forever-service/bin/forever-service delete noobaa_local_service
 rm -rf /usr/local/noobaa
 echo "Done"
+rm -rf ./agent_storage/

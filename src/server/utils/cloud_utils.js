cbae2a100167ebd4fbad89fedb110a508f6542ce
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-08-22 13:58:33 +0300

    bug #1021

--- src/server/utils/cloud_utils.js
+++ src/server/utils/cloud_utils.js
@@ -14,21 +14,19 @@ const AWS = require('aws-sdk');
  */
 function resolve_cloud_sync_info(sync_policy) {
     var stat;
-    if (!_.isEmpty(sync_policy)) {
-        //If sync time is epoch (never synced) change to never synced
-        if (sync_policy.paused) {
-            stat = 'PAUSED';
-        } else if (!sync_policy.health) {
-            stat = 'UNABLE';
-        } else if (sync_policy.last_sync.getTime() === 0) {
-            stat = 'PENDING';
-        } else if (sync_policy.status === 'IDLE') {
-            stat = 'SYNCED';
-        } else {
-            stat = 'SYNCING';
-        }
-    } else {
+    if (_.isEmpty(sync_policy)) {
         stat = 'NOTSET';
+        //If sync time is epoch (never synced) change to never synced
+    } else if (sync_policy.paused) {
+        stat = 'PAUSED';
+    } else if (!sync_policy.health) {
+        stat = 'UNABLE';
+    } else if (sync_policy.status === 'SYNCING') {
+        stat = 'SYNCING';
+    } else if (sync_policy.last_sync.getTime() === 0) {
+        stat = 'PENDING';
+    } else {
+        stat = 'SYNCED';
     }
     return stat;
 }

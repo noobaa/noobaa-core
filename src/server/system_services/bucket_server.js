d373b58a81c9e4ebb43d00b5c3f8a4cd3622dc0b
Danny Zaken <danny.zaken@noobaa.com>
2016-08-23 08:09:49 +0300

    use size_utils.json_to_bigint to create BigInteger
    handle the case that storage is over petabyte

--- src/server/system_services/bucket_server.js
+++ src/server/system_services/bucket_server.js
@@ -751,9 +751,9 @@ function get_bucket_info(bucket, nodes_aggregate_pool, num_of_objects, cloud_syn
 
     info.num_objects = num_of_objects || 0;
     let placement_mul = (tier_of_bucket.data_placement === 'MIRROR') ? Math.max(tier_of_bucket.pools.length, 1) : 1;
-    let bucket_used = new BigInteger((bucket.storage_stats && bucket.storage_stats.chunks_capacity) || 0).multiply(tier_of_bucket.replicas).multiply(placement_mul);
-    let bucket_free = new BigInteger((info.tiering && info.tiering.storage && info.tiering.storage.free) || 0);
-    let bucket_used_other = new BigInteger((info.tiering && info.tiering.storage && info.tiering.storage.used_other) || 0);
+    let bucket_used = size_utils.json_to_bigint((bucket.storage_stats && bucket.storage_stats.chunks_capacity) || 0).multiply(tier_of_bucket.replicas).multiply(placement_mul);
+    let bucket_free = size_utils.json_to_bigint((info.tiering && info.tiering.storage && info.tiering.storage.free) || 0);
+    let bucket_used_other = size_utils.json_to_bigint((info.tiering && info.tiering.storage && info.tiering.storage.used_other) || 0);
 
     info.storage = size_utils.to_bigint_storage({
         used: bucket_used,

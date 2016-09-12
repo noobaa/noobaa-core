93b9b14fc8c06d639fce36445142b205911b956b
nb-ohad <ohad.mitrani@noobaa.com>
2016-09-12 16:04:31 +0300

    Fix use case where table is empty

--- frontend/src/app/components/shared/data-table/data-table-binding.js
+++ frontend/src/app/components/shared/data-table/data-table-binding.js
@@ -4,9 +4,13 @@ function setHeadPadding(table) {
     const head = table.querySelector('thead');
     const body = table.querySelector('tbody');
 
-    let diff = head.clientWidth - body.clientWidth;
-    head.style.paddingRight = `${diff}px`;
-}
+    if (body.clientWidth > 0) {
+        let diff = head.clientWidth - body.clientWidth;
+        head.style.paddingRight = `${diff}px`;
+    } else {
+        head.style.paddingRight = 'auto';
+    }
+
 
 ko.bindingHandlers.dataTable = {
     init(element, valueAccessor, allBindings, viewModel, bindingContext) {

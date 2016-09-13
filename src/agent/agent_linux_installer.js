2562d7ae0e3c65153281611eec21fcbfc85a0776
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-13 18:32:19 +0300

    Fixes to agent installer

--- src/agent/agent_linux_installer.js
+++ src/agent/agent_linux_installer.js
@@ -5,7 +5,9 @@ const Service = require('node-linux').Service;
 var srv = new Service({
     name: 'Noobaa Local Service',
     description: 'The Noobaa node service.',
-    script: '/usr/local/noobaa/src/agent/agent_wrap.js'
+    script: '/usr/local/noobaa/src/agent/agent_wrap.js',
+    wait: 10,
+    logpath: '/var/log/noobaaServiceWrapper.log' //TODO: DIS??
 });
 
 srv.on('install', () => {
--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -5,6 +5,7 @@ sleep 10
 /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
 
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
+  sleep 10
   systemctl enable noobaalocalservice
 else
   # TODO: equivilant for UpStart

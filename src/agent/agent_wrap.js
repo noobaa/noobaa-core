2ce3a0cc67c575ddf0ffb074387e0828019d7119
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-13 19:35:04 +0300

    Cleanups in wrapper

--- src/agent/agent_wrap.js
+++ src/agent/agent_wrap.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const P = require('../util/promise');
 const promise_utils = require('../util/promise_utils');
 const request = require('request');
-//const IP_PORT_REGEX = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,4}/;
 const url = require('url');
+const dbg = require('../util/debug_module')(__filename);
+
 const SETUP_FILENAME = './noobaa-setup';
 const EXECUTABLE_MOD_VAL = 511;
 
@@ -16,18 +17,19 @@ process.chdir('/usr/local/noobaa');
 fs.readFileAsync('./agent_conf.json')
     .then(agent_conf_file => {
         address = url.parse(JSON.parse(agent_conf_file).address).host;
+        dbg.log0('Starting agent_cli');
         return promise_utils.fork('./src/agent/agent_cli');
     }) //TODO: handle agent_conf not being where expected by inserting default value into address
     .catch(err => {
         if (err.code && err.code === 1) {
-            console.log('Upgrading Noobaa agent...');
+            dbg.log0('Upgrading Noobaa agent');
             return promise_utils.fork('./src/agent/agent_cli', '--duplicate');
         }
         throw err;
     })
     .then(() =>
         new P((resolve, reject) => {
-            console.log('Downloading Noobaa agent upgrade package...');
+            dbg.log0('Downloading Noobaa agent upgrade package');
             const input = request({
                 url: `https://${address}/public/noobaa-setup`,
                 strictSSL: false,
@@ -47,9 +49,9 @@ fs.readFileAsync('./agent_conf.json')
         }))
     .then(() => fs.chmodAsync(SETUP_FILENAME, EXECUTABLE_MOD_VAL))
     .then(() => {
-        console.log('Upgrading Noobaa agent...');
+        dbg.log0('Upgrading Noobaa agent');
         return promise_utils.spawn(SETUP_FILENAME);
     })
     .catch(err => {
-        console.error(`An error occured: ${err}`);
+        dbg.error(err);
     });

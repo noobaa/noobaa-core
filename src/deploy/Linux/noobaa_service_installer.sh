90e8ac59ca0af16a83b3d92c55bbde8e502e7623
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-13 17:51:08 +0300

    place holder for else

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 PATH=/usr/local/noobaa:$PATH;
 /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_uninstaller
 sleep 10
@@ -8,5 +7,6 @@ sleep 10
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
   systemctl enable noobaalocalservice
 else
-  #upstart equivilant
+  # TODO: equivilant for UpStart
+  echo upstart enable service
 fi

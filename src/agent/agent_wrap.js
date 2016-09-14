1c2d6922a8f87f591d820a4852a2ec59eefb1003
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-14 14:22:56 +0300

    commenting out dbg

--- src/agent/agent_wrap.js
+++ src/agent/agent_wrap.js
@@ -5,7 +5,7 @@ const P = require('../util/promise');
 const promise_utils = require('../util/promise_utils');
 const request = require('request');
 const url = require('url');
-const dbg = require('../util/debug_module')(__filename);
+//const dbg = require('../util/debug_module')(__filename);
 
 const SETUP_FILENAME = './noobaa-setup';
 const EXECUTABLE_MOD_VAL = 511;
@@ -17,12 +17,14 @@ process.chdir('/usr/local/noobaa');
 fs.readFileAsync('./agent_conf.json')
     .then(agent_conf_file => {
         address = url.parse(JSON.parse(agent_conf_file).address).host;
-        dbg.log0('Starting agent_cli');
+        //dbg.log0('Starting agent_cli');
+        console.log('Starting agent_cli');
         return promise_utils.fork('./src/agent/agent_cli');
     }) //TODO: handle agent_conf not being where expected by inserting default value into address
     .catch(err => {
         if (err.code && err.code === 1) {
-            dbg.log0('Duplicate token');
+            //dbg.log0('Duplicate token');
+            console.log('Duplicate token');
             return promise_utils.fork('./src/agent/agent_cli', '--duplicate');
         }
         throw err;
@@ -30,7 +32,8 @@ fs.readFileAsync('./agent_conf.json')
     .then(() => {
         const output = fs.createWriteStream(SETUP_FILENAME);
         return new P((resolve, reject) => {
-            dbg.log0('Downloading Noobaa agent upgrade package');
+            console.log('Downloading Noobaa agent upgrade package');
+            //dbg.log0('Downloading Noobaa agent upgrade package');
             request.get({
                     url: `https://${address}/public/noobaa-setup`,
                     strictSSL: false,
@@ -44,7 +47,9 @@ fs.readFileAsync('./agent_conf.json')
     })
     .then(() => fs.chmodAsync(SETUP_FILENAME, EXECUTABLE_MOD_VAL))
     .then(() => {
-        dbg.log0('Upgrading Noobaa agent');
+        //dbg.log0('Upgrading Noobaa agent');
+        console.log('Upgrading Noobaa agent');
         return promise_utils.spawn(SETUP_FILENAME);
     })
-    .catch(err => dbg.error(err));
+    .catch(err => console.error(err));
+//dbg.error(err));

5ce070bdee19257e85d057a39d20a1307ff27e08
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-15 15:52:09 +0300

    Added upstart support
    Added upstart initialisation script. Will be copied to /init whenever
    on non-systemd os. A bit naive but it’s a pretty solid heuristic.

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "installing Noobaa"
+echo "installing NooBaa"
 PATH=/usr/local/noobaa:$PATH;
 /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
 
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
+  echo "systemd detected. Registering service"
   systemctl enable noobaalocalservice
 else
-  # TODO: equivilant for UpStart
-  echo "upstart enable service"
+  echo "No systemd detected. Installing upstart script"
+  cp /usr/local/noobaa/src/deploy/Linux/noobaalocalservice.conf /etc/init/noobaalocalservice.conf
 fi
 
-echo "done here"
+echo "NooBaa installation complete"
--- src/deploy/Linux/noobaa_service_uninstall.sh
+++ src/deploy/Linux/noobaa_service_uninstall.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Uninstalling NooBaa"
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
+  echo "No systemd detected. Disabling service"
   systemctl disable noobaalocalservice
 else
-  # TODO: equivilant for UpStart
-  echo "upstart disable service"
+  echo "No systemd detected. Removing upstart script"
+  rm /etc/init/noobaalocalservice.conf
 fi
 
 PATH=/usr/local/noobaa:$PATH;
 /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer --uninstall
 
 rm -rf /usr/local/noobaa
-echo "Done"
+echo "NooBaa agent removed"

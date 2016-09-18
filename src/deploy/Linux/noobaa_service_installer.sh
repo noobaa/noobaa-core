12046a31154e52545d8b04bb350e6d5e59d27163
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-18 18:45:10 +0300

    not attempting service install on upstart

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 echo "installing NooBaa"
 PATH=/usr/local/noobaa:$PATH;
-/usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
-mkdir /usr/local/noobaa/logs
 
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
   echo "systemd detected. Registering service"
+  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
   systemctl enable noobaalocalservice
 elif [[ -d /etc/init.d ]]; then
   echo "upstart detected. Creating startup script"
   cp /usr/local/noobaa/src/agent/noobaalocalservice.conf /etc/init/noobaalocalservice.conf
   service noobaalocalservice start
 else
+  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
   echo "No systemd or upstart detected. Using System V"
 fi
+mkdir /usr/local/noobaa/logs
 
 echo "NooBaa installation complete"
--- src/deploy/Linux/noobaa_service_uninstall.sh
+++ src/deploy/Linux/noobaa_service_uninstall.sh
@@ -3,16 +3,19 @@ echo "Uninstalling NooBaa"
 if [[ $(ps -elf|grep 'systemd+\{0,1\}[ ]\{1,\}[0-9]\{1,\}[ ]\{1,\}1 ') ]]; then
   echo "systemd detected. Disabling service"
   systemctl disable noobaalocalservice
+  PATH=/usr/local/noobaa:$PATH;
+  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer --uninstall
+
 elif [[ -d /etc/init.d ]]; then
   echo "Removing upstart script"
   service noobaalocalservice stop
   rm /etc/init/noobaalocalservice.conf
 else
+  PATH=/usr/local/noobaa:$PATH;
+  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer --uninstall
   echo "System V in use"
 fi
 
-PATH=/usr/local/noobaa:$PATH;
-/usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer --uninstall
 
 rm -rf /usr/local/noobaa
 echo "NooBaa agent removed"

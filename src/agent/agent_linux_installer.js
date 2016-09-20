cb691e6d48e46026a2f660a5d99403d3ab5db061
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-20 15:33:42 +0300

    debugging
    need to revert this later

--- src/agent/agent_linux_installer.js
+++ src/agent/agent_linux_installer.js
@@ -12,35 +12,47 @@ var srv = new Service({
     cwd: '/usr/local/noobaa'
 });
 
+console.log(process.pid, "is registering to events");
+
 srv.on('doesnotexist', () => {
-    console.log('NooBaa local service was not previously installed');
+    var err = new Error();
+    console.log('Stack trace: ', err.stack);
+    console.log(process.pid, 'NooBaa local service was not previously installed');
     srv.install();
 });
 
 srv.on('install', () => {
-    console.log('Done installing NooBaa local service');
+    var err = new Error();
+    console.log('Stack trace: ', err.stack);
+    console.log(process.pid, 'Done installing NooBaa local service');
     srv.start();
 });
 
 srv.on('alreadyinstalled', () => {
-    console.log('NooBaa local service is already installed');
+    var err = new Error();
+    console.log('Stack trace: ', err.stack);
+    console.log(process.pid, 'NooBaa local service is already installed');
 });
 
 srv.on('uninstall', () => {
+    var err = new Error();
+    console.log('Stack trace: ', err.stack);
     // Will only get here if the script was run with --repair cli arg
-    console.log('Done uninstalling NooBaa local service. Now reinstalling.');
+    console.log(process.pid, 'Done uninstalling NooBaa local service. Now reinstalling.');
     srv.install();
 });
 
 srv.on('start', () => {
-    console.log('Starting NooBaa local service');
+    var err = new Error();
+    console.log('Stack trace: ', err.stack);
+    console.log(process.pid, 'Starting NooBaa local service');
 });
 
 if (argv.uninstall) {
     // Only using uninstall event for reinstalls. else suspending it.
     if (!srv.isSuspended('uninstall')) srv.suspendEvent('uninstall');
     if (!srv.isSuspended('doesnotexist')) srv.suspendEvent('doesnotexist');
-    console.log('Uninstalling NooBaa local service');
+    console.log(process.pid, 'Uninstalling NooBaa local service');
     srv.uninstall();
 } else {
     if (srv.isSuspended('uninstall')) srv.resumeEvent('uninstall');

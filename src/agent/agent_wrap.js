a349cce42bd68eb45487a50f3392e93b452304a4
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-21 16:54:07 +0300

    debugging

--- src/agent/agent_wrap.js
+++ src/agent/agent_wrap.js
@@ -7,9 +7,9 @@
 process.chdir('/usr/local/noobaa');
 
 const fs = require('fs');
-const P = require('../util/promise');
+//const P = require('../util/promise');
 const promise_utils = require('../util/promise_utils');
-const request = require('request');
+//const request = require('request');
 const url = require('url');
 const dbg = require('../util/debug_module')(__filename);
 
@@ -35,6 +35,6 @@ fs.readFileAsync('./agent_conf.json')
     .then(() => fs.chmodAsync(SETUP_FILENAME, EXECUTABLE_MOD_VAL))
     .then(() => {
         dbg.log0('Upgrading Noobaa agent');
-        return promise_utils.spawn(SETUP_FILENAME);
+        return promise_utils.spawn(SETUP_FILENAME, ['&>>/var/log/setup.out']);
     })
     .catch(err => dbg.error(err));

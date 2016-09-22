6874df4e16a02e9c4934d32b078c02a0088fa315
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-22 17:10:54 +0300

    debugging

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -9,8 +9,6 @@ mkdir /usr/local/noobaa/logs
 
 if [ -f /usr/bin/systemctl ] || [ -f /bin/systemctl ]; then
   echo "Systemd detected. Installing service"
-  systemctl disable noobaalocalservice
-  systemctl stop noobaalocalservice
   rm /etc/systemd/system/multi-user.target.wants/noobaalocalservice.service
   cp /usr/local/noobaa/src/agent/system_d.conf /etc/systemd/system/multi-user.target.wants/noobaalocalservice.service
   systemctl enable noobaalocalservice

7a55307bcaa62769c9c8a781cc9b2315b3a20051
nb-ohad <ohad.mitrani@noobaa.com>
2016-09-22 16:03:43 +0300

    Move "unable to activate" modal to "loading server info screen"

--- frontend/src/app/actions.js
+++ frontend/src/app/actions.js
@@ -371,6 +371,7 @@ export function signOut(shouldRefresh = true) {
 export function loadServerInfo() {
     logAction('loadServerInfo');
 
+    model.serverInfo(null);
     api.account.accounts_status()
         .then(
             reply => reply.has_accounts ?

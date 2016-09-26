88b884533e4a1cab272c55fee340f7c0cc66dc69
nimrod-becker <nimrod.becker@noobaa.com>
2016-09-26 14:32:29 +0300

    fix server secret return on OS X

--- src/util/os_utils.js
+++ src/util/os_utils.js
@@ -400,8 +400,7 @@ function read_server_secret() {
     } else if (os.type() === 'Darwin') {
         return fs.readFileAsync(config.CLUSTERING_PATHS.DARWIN_SECRET_FILE)
             .then(function(data) {
-                var sec = data.toString();
-                return sec.substring(0, sec.length - 1);
+                return data.toString();
             })
             .catch(err => {
                 //For Darwin only, if file does not exist, create it

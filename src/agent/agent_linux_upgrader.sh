69a37863f53f93827eb8b03d9396760b109a2e4a
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-28 18:12:50 +0300

    Revert "debugging prints"
    This reverts commit fc2eb85e77171914392055a72b52db5181eb5fca.

--- src/agent/agent_linux_upgrader.sh
+++ src/agent/agent_linux_upgrader.sh
@@ -1,4 +1,3 @@
 # This runs the upgrade in a separate session in order to make sure it doesn't die
 
-mkdir -p /usr/local/noobaa/logs >& /dev/null
-setsid /usr/local/noobaa/noobaa-setup >> /usr/local/noobaa/logs/upgrade.log 2>&1
+setsid /usr/local/noobaa/noobaa-setup >& /dev/null &

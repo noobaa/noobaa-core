fc2eb85e77171914392055a72b52db5181eb5fca
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2016-09-28 18:11:13 +0300

    debugging prints
    The redirection of the upgrade process output to the logs is there to
    stay.

--- src/agent/agent_linux_upgrader.sh
+++ src/agent/agent_linux_upgrader.sh
@@ -1,3 +1,4 @@
 # This runs the upgrade in a separate session in order to make sure it doesn't die
 
-setsid /usr/local/noobaa/noobaa-setup >& /dev/null &
+mkdir -p /usr/local/noobaa/logs >& /dev/null
+setsid /usr/local/noobaa/noobaa-setup >> /usr/local/noobaa/logs/upgrade.log 2>&1

d983717bd0ce24122b04ed1382eafc7f29b961b6
nb-ohad <ohad.mitrani@noobaa.com>
2016-10-02 12:07:37 +0300

    Add subrows to servers table

--- frontend/src/app/components/cluster/server-table/server-row.js
+++ frontend/src/app/components/cluster/server-table/server-row.js
@@ -1,6 +1,5 @@
 import Disposable from 'disposable';
 import ko from 'knockout';
-import numeral from 'numeral';
 import { deepFreeze } from 'utils';
 
 const stateIconMapping = deepFreeze({
@@ -40,19 +39,50 @@ export default class ServerRowViewModel extends Disposable {
         );
 
         this.memoryUsage = ko.pureComputed(
-            () => server() ? numeral(server().memory_usage).format('%') : 'N/A'
-        );
+            () => server().memory_usage
+        ).extend({
+            formatNumber: { format: '%' }
+        });
 
         this.cpuUsage = ko.pureComputed(
-            () => server() ? numeral(server().cpu_usage).format('%') : 'N/A'
-        );
+            () => server().cpu_usage
+        ).extend({
+            formatNumber: { format: '%' }
+        });
 
         this.version = ko.pureComputed(
             () => server() ? server().version : 'N/A'
         );
 
-        this.actions = ko.pureComputed(
+        this.secret = ko.pureComputed(
             () => server() && server().secret
+        );
+
+        this.primaryDNS = ko.pureComputed(
+            () => (server() && server().dns_servers[0]) || 'Not set'
+        );
+
+        this.secondaryDNS = ko.pureComputed(
+            () => (server() && server().dns_servers[1]) || 'Not set'
+        );
+
+        this.timeConfig = ko.pureComputed(
+            () => {
+                let ntpServer = server() && server().ntp_server;
+
+                if (ntpServer) {
+                    return 'Using ';
+                } else {
+                    return 'No NTP (network time protocol) server configured';
+                }
+            }
+            // {
+            //
+            //     // return ntpServer ?
+            //     //     ~Using network time server at <span
+
+            //     // return this.ntpServer()
+            // }`Set to use ${this.ntpServer ? 'network' : 'manual'} time`
         );
     }
 }

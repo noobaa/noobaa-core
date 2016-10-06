d2473e2076a946ce0181fdd823fd6e9c17248e01
Vital P <vital@pinchas.co.il>
2016-10-06 17:44:48 +0300

    adding setServerDebugLevel & downloadServerDiagnosticPack

--- frontend/src/app/actions.js
+++ frontend/src/app/actions.js
@@ -1363,6 +1363,21 @@ export function downloadNodeDiagnosticPack(nodeName) {
         .done();
 }
 
+export function downloadServerDiagnosticPack(secret) {
+    logAction('downloadServerDiagnosticPack', { secret });
+
+    notify('Collecting data... might take a while');
+    api.cluster_server.diagnose_system(secret)
+        .catch(
+            err => {
+                notify('Packing system diagnostic file failed', 'error');
+                throw err;
+            }
+        )
+        .then(downloadFile)
+        .done();
+}
+
 export function downloadSystemDiagnosticPack() {
     logAction('downloadSystemDiagnosticPack');
 
@@ -1403,6 +1418,24 @@ export function setNodeDebugLevel(node, level) {
         .then(
             () => loadNodeInfo(node)
         )
+        .done();
+}
+
+export function setServerDebugLevel(secret, hostname, level){
+    logAction('setServerDebugLevel', { secret, hostname, level });
+
+    api.cluster_server.set_debug_level({ secret, level })
+        .then(
+            () => notify(
+                `Debug level has been ${level === 0 ? 'lowered' : 'rasied'} for server ${hostname}`,
+                'success'
+            ),
+            () => notify(
+                `Cloud not ${level === 0 ? 'lower' : 'raise'} debug level for server ${hostname}`,
+                'error'
+            )
+        )
+        .then(loadSystemInfo)
         .done();
 }
 

e096a7c2ddc4d49f51dfe446762414f66816cd3a
nb-ohad <ohad.mitrani@noobaa.com>
2016-10-20 12:17:53 +0300

    Fix tolerance calculation

--- frontend/src/app/components/cluster/cluster-summary/cluster-summary.js
+++ frontend/src/app/components/cluster/cluster-summary/cluster-summary.js
@@ -27,22 +27,22 @@ class ClusterSummaryViewModel extends Disposable{
                 'Not Highly Available'
         );
 
-        this.faultTolerance = ko.pureComputed(
-            () => {
-                let tolerance = this.serverCount() / 3 | 0;
-
-                return tolerance > 0 ?
-                    `${tolerance} server${tolerance > 1 ? 's' : ''}` :
-                    'No Tolerance';
-            }
-        );
-
         this.connectedCount = ko.pureComputed(
             () => servers()
                 .filter(
                     server => server.status === 'CONNECTED'
                 )
                 .length
+        );
+
+        this.faultTolerance = ko.pureComputed(
+            () => {
+                let tolerance = Math.ceil(this.connectedCount() / 2 - 1);
+
+                return tolerance > 0 ?
+                    `${tolerance} server${tolerance > 1 ? 's' : ''}` :
+                    'No Tolerance';
+            }
         );
 
         this.inProcessCount = ko.pureComputed(

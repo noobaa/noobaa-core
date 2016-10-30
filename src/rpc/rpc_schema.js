e7814dfb925fe8e541ecd0edd2bc9af146d0cb5a
Guy Margalit <guymguym@gmail.com>
2016-10-30 15:11:33 +0200

    rpc: support for buffer type in schema when the path is optional

--- src/rpc/rpc_schema.js
+++ src/rpc/rpc_schema.js
@@ -35,9 +35,7 @@ class RpcSchema {
             inline: (it, keyword, schema, parent) => {
                 if (this._buffer_paths) {
                     const buf_path = it.dataPathArr
-                        .slice(1, it.dataLevel + 1)
-                        .map(x => '[' + x + ']')
-                        .join('');
+                        .slice(1, it.dataLevel + 1);
                     this._buffer_paths.add(buf_path);
                 }
                 return 'Buffer.isBuffer(data' + (it.dataLevel || '') + ')';

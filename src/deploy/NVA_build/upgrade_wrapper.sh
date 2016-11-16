1e4e822e7986ed3dd117ccdb7963501b75c9790d
nimrod-becker <nimrod.becker@noobaa.com>
2016-11-16 19:09:43 +0200

    Move to node 6.9.1
    Also need to fix Jenkins :
    LinuxS3Build
    MDServer
    Vitaly

--- src/deploy/NVA_build/upgrade_wrapper.sh
+++ src/deploy/NVA_build/upgrade_wrapper.sh
@@ -263,23 +263,23 @@ function pre_upgrade {
         echo "bash /root/node_modules/noobaa-core/src/deploy/NVA_build/fix_server_sec.sh" >> /etc/rc.local
     fi
 
-	#install nvm use v4.4.4
+	#install nvm use v6.9.1
 	rm -rf ~/.nvm
 	mkdir ~/.nvm
 	cp ${EXTRACTION_PATH}/noobaa-core/build/public/nvm.sh ~/.nvm/
 	chmod 777 ~/.nvm/nvm.sh
-	mkdir /tmp/v4.4.4
-	cp ${EXTRACTION_PATH}/noobaa-core/build/public/node-v4.4.4-linux-x64.tar.xz /tmp/
-	tar -xJf /tmp/node-v4.4.4-linux-x64.tar.xz -C /tmp/v4.4.4 --strip-components 1
-	mkdir -p ~/.nvm/versions/node/v4.4.4/
-	mv /tmp/v4.4.4/* ~/.nvm/versions/node/v4.4.4/
+	mkdir /tmp/v6.9.1
+	cp ${EXTRACTION_PATH}/noobaa-core/build/public/node-v6.9.1-linux-x64.tar.xz /tmp/
+	tar -xJf /tmp/node-v6.9.1-linux-x64.tar.xz -C /tmp/v6.9.1 --strip-components 1
+	mkdir -p ~/.nvm/versions/node/v6.9.1/
+	mv /tmp/v6.9.1/* ~/.nvm/versions/node/v6.9.1/
 	export NVM_DIR="$HOME/.nvm"
 	. "$NVM_DIR/nvm.sh"
-	export PATH=~/.nvm/versions/node/v4.4.4/bin:$PATH
+	export PATH=~/.nvm/versions/node/v6.9.1/bin:$PATH
 	rm -f /usr/local/bin/node
-	ln -s  ~/.nvm/versions/node/v4.4.4/bin/node /usr/local/bin/node
-	nvm alias default 4.4.4
-	nvm use 4.4.4
+	ln -s  ~/.nvm/versions/node/v6.9.1/bin/node /usr/local/bin/node
+	nvm alias default 6.9.1
+	nvm use 6.9.1
 
 }
 

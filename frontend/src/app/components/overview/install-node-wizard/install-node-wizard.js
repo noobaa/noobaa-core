c259d9380038350f32e628524d7baf38116c0357
nimrod-becker <nimrod.becker@noobaa.com>
2016-11-23 10:38:59 +0200

    fix windows install command

--- frontend/src/app/components/overview/install-node-wizard/install-node-wizard.js
+++ frontend/src/app/components/overview/install-node-wizard/install-node-wizard.js
@@ -17,7 +17,7 @@ const steps = deepFreeze([
 
 const installCommands = deepFreeze({
     NETWORK_WINDOWS(pkg, conf, server) {
-        return `Start-BitsTransfer -Source http://${server}:8080/public/${pkg} -Destination C:\\${pkg}; C:\\${pkg} /S /config ${conf}`;
+        return `Import-Module BitsTransfer ; Start-BitsTransfer -Source http://${server}:8080/public/${pkg} -Destination C:\\${pkg}; C:\\${pkg} /S /config ${conf}`;
     },
 
     NETWORK_LINUX(pkg, conf, server) {

0c6887dcde074d22e3049a827b250df09d834b8a
nb-ohad <ohad.mitrani@noobaa.com>
2016-12-07 11:05:10 +0200

    Add beta label to navigation icons

--- frontend/src/app/components/layout/main-nav/main-nav.js
+++ frontend/src/app/components/layout/main-nav/main-nav.js
@@ -28,14 +28,15 @@ const navItems = deepFreeze([
         name: 'funcs',
         route: 'funcs',
         icon: 'working',
-        label: 'Functions'
+        label: 'Functions',
+        beta: true
     },
     {
         name: 'cluster',
         route: 'cluster',
         icon: 'cluster',
         label: 'Cluster',
-        preview: true
+        beta: true
     },
     {
         name: 'management',

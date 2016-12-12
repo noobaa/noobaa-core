0b2f45eb6211942ff93816b094bd9e1ca50665d7
nimrod-becker <nimrod.becker@noobaa.com>
2016-12-12 20:01:08 +0200

    Redirect linux installer messages to a log file (#2341)
    Redirect linux installer/uninstaller messages to a log file & verify
    execution of commands

--- src/deploy/Linux/noobaa_service_installer.sh
+++ src/deploy/Linux/noobaa_service_installer.sh
@@ -1,46 +1,53 @@
 #!/bin/bash
 #this script installs the service on linux systems.
 #first we find the newest init mechanism, then we install
+
 echo "installing NooBaa"
+instdate=$(date -u +"%m-%d-%H:%M")
 echo $(date)
+
+function verify_command_run {
+        $@ >> /var/log/noobaa_service_${instdate}
+        local rc=$?
+        if [ $rc -ne 0 ]; then
+                echo "NooBaa installation failed"
+                exit 1
+        fi
+}
+
 PATH=/usr/local/noobaa:$PATH;
 mkdir /usr/local/noobaa/logs
 chmod 777 /usr/local/noobaa/remove_service.sh
-/usr/local/noobaa/remove_service.sh
+/usr/local/noobaa/remove_service.sh ignore_rc >> /var/log/noobaa_service_${instdate}
 if [ -f /usr/bin/systemctl ] || [ -f /bin/systemctl ]; then
   echo "Systemd detected. Installing service"
   cp /usr/local/noobaa/src/agent/system_d.conf /etc/systemd/system/multi-user.target.wants/noobaalocalservice.service
-  systemctl daemon-reload
-  systemctl enable noobaalocalservice
-  systemctl start noobaalocalservice
-  systemctl daemon-reload
+  echo "Updating systemctl"
+  verify_command_run systemctl daemon-reload
+  verify_command_run systemctl enable noobaalocalservice
+  echo "Starting Service"
+  verify_command_run systemctl start noobaalocalservice
+  verify_command_run systemctl daemon-reload
 elif [[ -d /etc/init ]]; then
   echo "Upstart detected. Creating startup script"
   cp /usr/local/noobaa/src/agent/upstart.conf /etc/init/noobaalocalservice.conf
   sleep 1
-  initctl start noobaalocalservice
+  echo "Starting Service"
+  verify_command_run initctl start noobaalocalservice
 elif [[ -d /etc/init.d ]]; then
   echo "System V detected. Installing service"
-  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
+  verify_command_run /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer >> /var/log/noobaa_service_${instdate}
   type chkconfig &> /dev/null
   if [ $? -eq 0 ]; then
-    chkconfig noobaalocalservice on
+    verify_command_run chkconfig noobaalocalservice on
   else
-    update-rc.d noobaalocalservice enable
+    verify_command_run update-rc.d noobaalocalservice enable
   fi
-  service noobaalocalservice start
+  echo "Starting Service"
+  verify_command_run service noobaalocalservice start
 else
-  echo "ERROR: Cannot detect init mechanism! Attempting to force service installation"
-  /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_linux_installer
-  systemctl enable noobaalocalservice
-  cp /usr/local/noobaa/src/agent/noobaalocalservice.conf /etc/init/noobaalocalservice.conf
-  if [ $? -eq 0 ]; then
-    chkconfig noobaalocalservice on
-  else
-    update-rc.d noobaalocalservice enable
-  fi
-  systemctl enable noobaalocalservice
-  service noobaalocalservice start
+  echo "ERROR: Cannot detect init mechanism, NooBaa installation failed"
+  exit 1
 fi
 
-echo "NooBaa installation complete"
+echo "NooBaa installation completed successfully"

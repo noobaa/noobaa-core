75f6929d2330ee8e9398907c81e49915700ded76
Nimrod Becker <bnimrod@gmail.com>
2016-12-14 11:00:23 +0200

    Set cluster to preview

--- frontend/src/app/components/layout/main-nav/main-nav.js
+++ frontend/src/app/components/layout/main-nav/main-nav.js
@@ -36,7 +36,8 @@ const navItems = deepFreeze([
         route: 'cluster',
         icon: 'cluster',
         label: 'Cluster',
-        beta: true
+        beta: true,
+        preview: true
     },
     {
         name: 'management',

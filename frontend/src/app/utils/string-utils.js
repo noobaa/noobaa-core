4f18bd5e3e6e647b9db6f2cb83a7e487d288ef89
nb-ohad <ohad.mitrani@noobaa.com>
2016-12-18 14:08:40 +0200

    Add default value in case of undefined

--- frontend/src/app/utils/string-utils.js
+++ frontend/src/app/utils/string-utils.js
@@ -71,7 +71,7 @@ export function equalNoCase(str1, str2) {
     return str1.toLowerCase() === str2.toLowerCase();
 }
 
-export function lastSegment(str, delimiter) {
+export function lastSegment(str = '', delimiter) {
     return str.substr(str.lastIndexOf(delimiter) + 1);
 }
 

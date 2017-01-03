165926d0ced0bd989d4792d25b23a0e5056c2c7c
nb-ohad <ohad.mitrani@noobaa.com>
2017-01-03 20:18:56 +0200

    Revert some changes - reintroduce FF modal bug

--- frontend/src/app/base-view-model.js
+++ frontend/src/app/base-view-model.js
@@ -18,14 +18,17 @@ export default class BaseViewModel {
     }
 
     dispose() {
+        // THIS CODE CREATE A SECONDARY BUG WHERE THING GOT DISPOSED WITH NO REASON
+        // COMMENTING THIS COUNT OUT AND ALLOWING VALIDATORS CODE LEAK AGAIN
+
         // Used to clean up subscriptions created by knockout validation extenders.
-        for (const value of Object.values(this)) {
-            if (ko.isObservable(value) && value.isValid) {
-                value.extend({
-                    validatable: false
-                });
-            }
-        }
+        // for (const value of Object.values(this)) {
+        //     if (ko.isObservable(value) && value.isValid) {
+        //         value.extend({
+        //             validatable: false
+        //         });
+        //     }
+        // }
 
         this.disposeList.forEach(
             disposer => disposer()

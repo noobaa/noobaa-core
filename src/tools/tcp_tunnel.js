7560829e9fca440b38211ac0e8edf7b4b65320df
Nimrod Geva <nimrod.geva@mail.huji.ac.il>
2017-01-11 17:47:05 +0200

    Added signature test cases and a tcp_tunnel tool

--- src/tools/tcp_tunnel.js
+++ src/tools/tcp_tunnel.js
@@ -2,6 +2,8 @@
 'use strict';
 
 const net = require('net');
+const tls = require('tls');
+const path = require('path');
 const HTTPRecorder = require('../util/http_recorder');
 
 exports.tunnel_port = tunnel_port;
@@ -13,72 +15,111 @@ if (require.main === module) {
 
 function main() {
     // eslint-disable-next-line global-require
-    const argv = require('minimist')(process.argv);
-    argv.host = argv.host || '127.0.0.1';
-    argv.port = parseInt(argv.port, 10) || 80;
-    argv.port2 = parseInt(argv.port2, 10) || 6001;
-    tunnel_port({
-        source_port: argv.port,
-        target_port: argv.port2,
-        hostname: argv.host,
-        name: 'TCP TUNNEL',
-        record_http: argv.record_http,
-    });
-    if (argv.ssl || argv.ssl2) {
-        argv.ssl = parseInt(argv.ssl, 10) || 443;
-        argv.ssl2 = parseInt(argv.ssl2, 10) || 6443;
+    const argv = require('minimist')(process.argv.slice(2));
+    if (argv.help) {
+        console.log(`
+Usage: node ${path.relative('.', __filename)} (options) [80:6001 443s:6443s ...]
+
+Options:
+    --help                      Show this help
+    --host <name|ip>            Tunnel to that host (default localhost)
+    --record_http [extension]   Parse the stream as HTTP and record requests to files (see below).
+                                Optional file extension (default 'sreq')
+
+List of tunnels:
+    Each tunnel will have the following format: source:target[:optional-tag]
+    Source and target are port numbers, with optional 's' suffix for SSL.
+    The tunnel will listen on the source port on the running machine.
+    For every incoming connection it will connect to the target port and tunnel data in both directions.
+
+SSL:
+    SSL for source means to listen on the source port for SSL socket and do the SSL handshake by the tunnel (self signed certificate).
+    SSL for target means to connect to the target port with SSL socket.
+    Mixing between SSL and non-SSL source and target ports is supported.
+    Examples:
+    1. 443:6443     will tunnel encrypted data without deciphering it at all. Notice that record_http will not work in that case.
+    2. 443s:6443s   the tunnel will decipher the SSL against the client and will connect with SSL to the target.
+    3. 443s:80      the tunnel will do SSL handshake with the client and will decipher and tunnel the plain data to port 8080.
+    4. 80:443s      the tunnel will create an SSL connection to port 443 and will tunnel data from port 80.
+
+Recording HTTP:
+    The TCP stream is parsed as HTTP stream and every request is saved to a file on the current working directory.
+    In order to parse and record HTTP requests the tunnel must be able to read the plain data of the stream.
+    So it will not work in example number 1 of the SSL section were the data is tunneled without deciphering.
+`);
+        return;
+    }
+    const hostname = argv.host || '127.0.0.1';
+    const record_http = argv.record_http;
+    const tunnels = argv._ || [];
+    if (!tunnels.length) {
+        tunnels.push('80:6001');
+        tunnels.push('443s:6443s');
+    }
+    for (const tun of tunnels) {
+        const tun_args = tun.split(':');
+        const source = tun_args[0];
+        const target = tun_args[1];
+        const name = `[${tun}]`;
         tunnel_port({
-            source_port: argv.ssl,
-            target_port: argv.ssl2,
-            hostname: argv.host,
-            name: 'SSL TUNNEL',
-            record_http: argv.record_http,
+            source,
+            target,
+            hostname,
+            name,
+            record_http,
         });
     }
 }
 
 function tunnel_port({
-    source_port,
-    target_port,
+    source,
+    target,
     hostname,
     name,
     record_http
 }) {
-    name = (name || '') + ' [' + source_port + '->' +
-        human_addr(hostname + ':' + target_port) + ']';
-    return net.createServer()
-        .on('connection', conn => {
+    const source_port = parseInt(source, 10);
+    const source_ssl = source.endsWith('s');
+    const server = source_ssl ? tls.createServer({
+        key: ssl_key(),
+        cert: ssl_cert(),
+    }) : net.createServer();
+    return server
+        .on(source_ssl ? 'secureConnection' : 'connection', conn =>
             tunnel_connection({
                 conn,
-                target_port,
+                target,
                 hostname,
                 name,
-                record_http
-            });
-        })
-        .on('error', err => {
-            console.error(name, 'server error', err.stack || err);
-        })
-        .on('listening', () => {
-            console.log(name, 'listening ...');
-        })
+                record_http,
+            }))
+        .on('error', err => console.error(name, 'server error', err.stack || err))
+        .on('listening', () => console.log(name, 'listening ...'))
         .listen(source_port);
 }
 
 function tunnel_connection({
     conn,
-    target_port,
+    target,
     hostname,
     name,
     record_http
 }) {
+    const target_port = parseInt(target, 10);
+    const target_ssl = target.endsWith('s');
     const conn_name = human_addr(conn.remoteAddress + ':' + conn.remotePort);
-    const target_conn = net.connect(target_port, hostname);
+    const target_conn = target_ssl ?
+        tls.connect({
+            port: target_port,
+            host: hostname,
+            rejectUnauthorized: false,
+        }) :
+        net.connect(target_port, hostname);
     conn.on('close', () => on_error('source closed'));
     target_conn.on('close', () => on_error('target closed'));
     conn.on('error', err => on_error('source error', err));
     target_conn.on('error', err => on_error('target error', err));
-    target_conn.on('connect', () => {
+    target_conn.on(target_ssl ? 'secureConnect' : 'connect', () => {
         console.log(name, conn_name, 'tunneling ...');
         conn.pipe(target_conn);
         target_conn.pipe(conn);
@@ -92,7 +133,7 @@ function tunnel_connection({
                     .replace(/-/g, '')
                     .toLowerCase();
                 const extension = typeof(record_http) === 'string' ? record_http : 'sreq';
-                return `${prefix}_${Date.now().toString(36)}.${extension}`;
+                return `${prefix}_${msg.method}_${Date.now().toString(36)}.${extension}`;
             }));
         }
     });
@@ -124,4 +165,58 @@ function human_addr(addr) {
         .replace(/^::0:1:/, '') // ipv6 localhost
         .replace(/^127\.0\.0\.1:/, '') // ipv4 localhost
         .replace(/^localhost:/, ''); // named localhost
+}
+
+function ssl_key() {
+    return `-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAx1dRbX6F9q7V56EmMGlhuksnqXPcQM4cL/FfwQG/15CPG8Mp
+3bEPIOSGvbhaDiTgisAJ5sKhIfI7Ac5BQpps8Y9YJQDhIr4hXkFFjZ7nu2t/KxbC
+g4S/9+p+USySt9zsr+ER0W3k69G7pHcngLbiuUkJMz9ku4Zq2iDBmZLvO+H98Wwk
+u5udx0yuAq0rvM24JE4dBRTk57bOmBp9L6R6AhZKh9q8YigXwtoB2JKy5pCxu8Jj
+G+1FdQ9AO+JwS6cHRM9XVhbpTg3V5Y98rP0jlNMiGEixFfJ0xFv4cslXkO+pbw3N
+Ek25RXCILUbbk7ZMQoH6X6Xc9vIOKqA5KYs7FwIDAQABAoIBAQCLE6TAG/IjNcAP
+pyMZy6xfaWf2ldspa6PG30TLSAkswLLXz8Y54fqIHGjVnPVXwOrYYzuFQG1jXblF
+fT2S1mMD9dqtlnt83eIx/KAmOqO64zkKOwri749vzK7su4hxtzV7UDA6Sc3Zqa6d
+BHUPHIn6c7Zzhtsk6pdKMGhiWV2IDNwazXkZZU7CzEBZzpaWHDNWwgxR9ACwDtuG
+1TqJt++xgxybCpGhtma7JCFSAHMIxwecjv8QJsQYT/yJ5foc8/qxZEZd3vuxNZc9
+ZzRvNrU4LDVHPDDGrq65UN8G8Sll+n1SQrpWFOvz0b/jmjDIs2qi5/8yD2YUCHZG
+D/onrwmBAoGBAPO5xlX2jR5BFt4g6ethJxJ1iRUxmNMkH41XjZfFfBEd0fqIfj/L
+U+bJVu5WH2cZrU+T1V5a6jN+wMYh/EW+jygy4pxUv/Wj1T824Lsa1Y12X2MSIqRH
+TMp3/SrnQGW5WvG493phYnDG8AaX8fw+YdlUZOjfToyby4QE0Y1j80YHAoGBANFh
+UP9Z8vk4Kxbodv5Pf0SsHobxBaxhY+epZpH51PUIcR0PBSny/C/NLQI7HJNvk/14
+IYmbd6LR2L2LinUn2uQfqKZ6q2VApIf5bJzaANzTthKg6faqJVrigqIk5YCYHQqX
+hHWfhaoiHLE13xI+cNmN9L7DBQK3BiSpJI42R55xAoGBAMrcxWYFyp31BWisMqfV
+fKUTqZ83YgHUXmLSDivWl5bToFQMyjClN9evnCjTDF5Pc/75iK55s9ha12/TF9yh
+aRzHhfEjZYe67yMntVRnWHrfnTOkA6uDITqhNttAEkzZRAZQs8RsTHicTWrfi+4t
+qlovsbJXNFU60+G19QnfuLx/AoGAJzDrA1Dn3OQ5lIkgtMtWjBkXgbSdlj2IgLVB
+oLM9vDu/SGwmUErOD3h9IzzDRYmODtSsmmBCTXSv+BKlcPZrz2VpoPe3GzW1VkpG
+nTllDfCG1QfPoz8HzPI293imiKJwVSo1PfsE/upxqm3l+jk70Ez08bv5NR/jA3ux
+fvkEANECgYAlh8o3iPTNXRieiwDiHMxJL2zY876ExZGyxBcTA5q1dQNGGJfWH0+L
+ZXuxpKQV/SgP8FrUali23z7cZ4jPTU+Ziav6O8gd5u3limwVA99QcXax2pTYXycS
+gn1SstfEENVozLwgTsYdBhk7H6cK7tZHHF0mKTenpz1SaMbK5E8DLw==
+-----END RSA PRIVATE KEY-----
+`;
+}
+
+function ssl_cert() {
+    return `-----BEGIN CERTIFICATE-----
+MIIDBjCCAe4CCQDPzi0wXa1CGTANBgkqhkiG9w0BAQUFADBFMQswCQYDVQQGEwJB
+VTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50ZXJuZXQgV2lkZ2l0
+cyBQdHkgTHRkMB4XDTE3MDExMDE2NDc1NloXDTE3MDIwOTE2NDc1NlowRTELMAkG
+A1UEBhMCQVUxEzARBgNVBAgTClNvbWUtU3RhdGUxITAfBgNVBAoTGEludGVybmV0
+IFdpZGdpdHMgUHR5IEx0ZDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
+AMdXUW1+hfau1eehJjBpYbpLJ6lz3EDOHC/xX8EBv9eQjxvDKd2xDyDkhr24Wg4k
+4IrACebCoSHyOwHOQUKabPGPWCUA4SK+IV5BRY2e57trfysWwoOEv/fqflEskrfc
+7K/hEdFt5OvRu6R3J4C24rlJCTM/ZLuGatogwZmS7zvh/fFsJLubncdMrgKtK7zN
+uCROHQUU5Oe2zpgafS+kegIWSofavGIoF8LaAdiSsuaQsbvCYxvtRXUPQDvicEun
+B0TPV1YW6U4N1eWPfKz9I5TTIhhIsRXydMRb+HLJV5DvqW8NzRJNuUVwiC1G25O2
+TEKB+l+l3PbyDiqgOSmLOxcCAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAg5uxXk8P
+fCVkRWQDEIJVbJPpWV/sAtHHjGIaFthxykhk0hqN1zDIu5APCw3Is6BJ/EkcmC/3
+BzALcQBcxhtGPqmjCyYZ0EOj01F9tICUNYU/aAg2P5Mds2yAY7OH8vPiE7oD22p6
+vxSof1T0MFVSZ6hm9X7Y6lH4rr6dOhTNQRIbJDFZzNQmRk1EBzLCZXorRKXbi/e+
+eqD+tdztCQN5uK2LReJgrrwDlMzh9OR5aTInuqdGzX9PdWimXo+SPUheZUSm366S
+S1hOP3iKPBufXI+z1xVaGqvJcqoCswgA1YF3Yiy3FVk06UVQbGaLSOjV/N4Qn9EF
+fzMKyfTfm1GeLw==
+-----END CERTIFICATE-----
+`;
 }

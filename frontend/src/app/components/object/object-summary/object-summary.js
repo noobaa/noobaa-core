af8e6ca414bdf929307fb39944b7f2505c46118b
nb-ohad <ohad.mitrani@noobaa.com>
2017-02-06 19:36:25 +0200

    Ohad 1.0 fixes (#2672)
    * Bug fixes
    
    Fixes #2647
    Fixes #2658
    Fixes #2664
    
    * Make not available text param more verbose

--- frontend/src/app/components/object/object-summary/object-summary.js
+++ frontend/src/app/components/object/object-summary/object-summary.js
@@ -27,9 +27,9 @@ class ObjectSummaryViewModel extends BaseViewModel {
         });
 
         this.lastRead = ko.pureComputed(
-            () => obj() && obj().stats.last_read
+            () => (obj() && obj().stats.last_read) || undefined
         ).extend({
-            formatTime: true
+            formatTime: { notAvailableText: 'File not read' }
         });
 
         this.readCount = ko.pureComputed(

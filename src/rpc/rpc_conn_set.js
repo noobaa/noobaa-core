e607f550786cc164b5455655d343f006a9c52010
Guy Margalit <guymguym@gmail.com>
2017-02-23 01:33:10 +0200

    redirector: handle closed connections, refactor into RpcConnSet util

---
+++ src/rpc/rpc_conn_set.js
@@ -0,0 +1,52 @@
+/* Copyright (C) 2016 NooBaa */
+'use strict';
+
+const dbg = require('../util/debug_module')(__filename);
+
+const CLOSE_LISTENER_SYMBOL = Symbol('CLOSE_LISTENER_SYMBOL');
+
+class RpcConnSet {
+
+    constructor(name) {
+        this.name = name;
+        this.set = new Set();
+    }
+
+    add(conn) {
+        if (conn.is_closed()) {
+            dbg.warn(this.name, 'ignore closed connection', conn.url.href);
+            return;
+        }
+        if (this.set.has(conn)) {
+            dbg.log0(this.name, 'already registered', conn.url.href);
+            return;
+        }
+        dbg.log0(this.name, 'adding connection', conn.url.href);
+        this.set.add(conn);
+        const close_listener = () => this.remove(conn);
+        conn[CLOSE_LISTENER_SYMBOL] = close_listener;
+        conn.once('close', close_listener);
+    }
+
+    remove(conn) {
+        dbg.warn(this.name, 'removing connection', conn.url.href);
+        const close_listener = conn[CLOSE_LISTENER_SYMBOL];
+        delete conn[CLOSE_LISTENER_SYMBOL];
+        conn.removeListener('close', close_listener);
+        this.set.delete(conn);
+    }
+
+    cleanup() {
+        for (const conn of this.set) {
+            if (conn.is_closed()) this.remove(conn);
+        }
+    }
+
+    list() {
+        this.cleanup();
+        return Array.from(this.set);
+    }
+
+}
+
+module.exports = RpcConnSet;

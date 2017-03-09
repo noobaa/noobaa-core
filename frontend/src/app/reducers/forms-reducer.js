c9882c038c56eb91f130efb3756e4fac8939c516
nb-ohad <ohad.mitrani@noobaa.com>
2017-03-09 14:24:10 +0200

    Add extenders mechanism to forms-reducer
    Also add an extender for install-nodes-from-

--- frontend/src/app/reducers/forms-reducer.js
+++ frontend/src/app/reducers/forms-reducer.js
@@ -1,3 +1,4 @@
+import { mapValues } from 'utils/core-utils';
 import { createReducer } from 'utils/reducer-utils';
 
 // ------------------------------
@@ -12,58 +13,114 @@ function onInitApplication() {
     return initialState;
 }
 
-function onInitForm(forms, { form, values }) {
+function onInitForm(forms, { form: formName, values }) {
+    const fields = mapValues(
+        values,
+        value => ({
+            initial: value,
+            value: value,
+            touched: false,
+            dirty: false
+        })
+    );
+
     return {
         ...forms,
-        [form]: {
-            initialValues: values,
-            values: {}
+        [formName]: {
+            fields: fields,
+            errors: {},
+            warnings: {}
         }
     };
 }
 
 function onUpdateForm(forms, { form, field, value }) {
-    if (!forms[form]) {
-        return forms;
-    }
-
+    if (!forms[form]) return forms;
     return {
         ...forms,
-        [form]: {
-            ...forms[form],
-            values: {
-                ...forms[form].values,
-                [field] : value
-            }
-        }
+        [form]: updateField(forms[form], field, value)
     };
 }
 
-function onResetForm(forms, { form }) {
-    if (!forms[form]) {
-        return forms;
-    }
+function onResetForm(forms, { form: formName }) {
+    const form = !forms[formName];
+    if (form) return forms;
 
     return {
         ...forms,
-        [form]: {
-            ...forms[form],
-            values: {}
-        }
+        [formName]: resetForm(form)
     };
 }
 
 function onDisposeForm(forms, { form }) {
-    if (!forms[form]) {
-        return forms;
-    }
-
-    return { ...forms, [form]: undefined };
+    return _removeKey(forms, form);
 }
 
-// ------------------------------
+// --------------------------------------------
+// Exported utils for manageing the forms state
+// --------------------------------------------
+export function updateField(form, name, value) {
+    const field = form.fields[name];
+    if (!field) return form;
+
+    const updatedField = {
+        initial: field.initial,
+        value: value,
+        touched: true,
+        dirty: value !== field.initial
+    };
+
+    return {
+        ...form,
+        fields: {
+            ...form.fields,
+            [name]: updatedField
+        }
+    };
+}
+
+export function resetField(form, name) {
+    const field = form.fields[name];
+    if (!field) return form;
+
+    const updatedField = {
+        initial: field.initial,
+        value: field.initial,
+        touched: false,
+        dirty: false
+    };
+
+    return {
+        ...form,
+        fields: {
+            ...form.fields,
+            [name]: updatedField
+        }
+    };
+}
+
+export function resetForm(form) {
+    const fields = mapValues(form.fields, _restFieldState);
+    return { ...form, fields };
+}
+
+// --------------------------------------------
 // Local util functions
-// ------------------------------
+// --------------------------------------------
+function _restFieldState(field) {
+    return {
+        ...field,
+        value: field.initial,
+        touched: false,
+        dirty: false
+    };
+}
+
+function _removeKey(obj, key) {
+    const { [key]: t, ...rest } = obj;
+    t === t;
+    return rest;
+}
 
 // ------------------------------
 // Exported reducer function

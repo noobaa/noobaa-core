a087b9ee02e71117eaaae667eefc015315b5c172
nb-ohad <ohad.mitrani@noobaa.com>
2017-03-12 13:53:45 +0200

    Fix broken layout of unsupported screen

--- frontend/src/app/components/login/loading-server-information-from/loading-server-information-from.js
+++ frontend/src/app/components/login/loading-server-information-from/loading-server-information-from.js
@@ -3,21 +3,22 @@ import BaseViewModel from 'components/base-view-model';
 import ko from 'knockout';
 import { serverInfo } from 'model';
 
-class LoadingServerInformationFromViewModel extends BaseViewModel {
+class LoadingServerInformationFormViewModel extends BaseViewModel {
     constructor() {
         super();
 
         this.isUnableToActivateModalVisible = ko.pureComputed(
-            () => Boolean(
-                serverInfo() &&
-                serverInfo().config &&
-                serverInfo().config.phone_home_connectivity_status !== 'CONNECTED'
-            )
+            () => true
+            // () => Boolean(
+            //     serverInfo() &&
+            //     serverInfo().config &&
+            //     serverInfo().config.phone_home_connectivity_status !== 'CONNECTED'
+            // )
         );
     }
 }
 
 export default {
-    viewModel: LoadingServerInformationFromViewModel,
+    viewModel: LoadingServerInformationFormViewModel,
     template: template
 };

793f7862447bd07a9ce272e44d468f4f00fe843c
nb-ohad <ohad.mitrani@noobaa.com>
2017-03-20 11:14:15 +0200

    Add buckets and node pools reducers

--- frontend/src/app/reducers/forms-reducer-with-extenders.js
+++ frontend/src/app/reducers/forms-reducer-with-extenders.js
@@ -1,12 +1,10 @@
-import { combineReducers, reduceReducers, filterActions } from 'utils/reducer-utils';
+import { combineReducers, reduceReducers } from 'utils/reducer-utils';
 import formsReducer from './forms-reducer';
 import installNodesFormReducer from './install-nodes-form-reducer';
 
 const formsExtender = combineReducers({
     installNodes: installNodesFormReducer
 });
-
-
 
 export default reduceReducers(
     formsReducer,

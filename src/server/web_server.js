ec0d482c1deef61a594ecc2e175cdeff9a6791d6
nimrod-becker <nimrod.becker@noobaa.com>
2017-03-22 12:17:38 +0200

    Fix #2799 - Detailed error message on set_cert

--- src/server/web_server.js
+++ src/server/web_server.js
@@ -237,7 +237,7 @@ app.post('/upload_certificate',
     })
     .catch(err => {
         dbg.error('Was unable to set certificate', err);
-        res.status(500).send('Was unable to set certificate');
+        res.status(500).send('Was unable to set certificate (' + err + ' )');
     })
 );
 

c16e1cbfa09a81893b303079b1b4591bfafe1e7a
Danny Zaken <danny.zaken@noobaa.com>
2017-03-22 18:31:21 +0200

    fixed clean_ova
    - clean mongo_ssl folder
    - clean .env

--- src/deploy/NVA_build/fix_mongo_ssl.sh
+++ src/deploy/NVA_build/fix_mongo_ssl.sh
@@ -4,7 +4,7 @@ if [ ! -d /etc/mongo_ssl/ ]; then
   mkdir /etc/mongo_ssl/
   . ${CORE_DIR}/src/deploy/NVA_build/setup_mongo_ssl.sh
   chmod 400 -R /etc/mongo_ssl
-  local client_subject=`openssl x509 -in /etc/mongo_ssl/client.pem -inform PEM -subject -nameopt RFC2253 | grep subject | awk '{sub("subject= ",""); print}'`
+  client_subject=`openssl x509 -in /etc/mongo_ssl/client.pem -inform PEM -subject -nameopt RFC2253 | grep subject | awk '{sub("subject= ",""); print}'`
   echo "MONGO_SSL_USER=${client_subject}" >> ${CORE_DIR}/.env
   # add bash script to run mongo shell with authentications
   echo "mongo --ssl --sslPEMKeyFile /etc/mongo_ssl/client.pem --sslCAFile /etc/mongo_ssl/root-ca.pem --sslAllowInvalidHostnames -u \"${client_subject}\" --authenticationMechanism MONGODB-X509 --authenticationDatabase \"\\\$external\" \"\$@\"" > /usr/bin/mongors

929879f709d5c123887eed118553602eabe193a8
Danny Zaken <danny.zaken@noobaa.com>
2017-03-23 15:18:50 +0200

    set agent_config to empty object by default

--- src/server/node_services/nodes_monitor.js
+++ src/server/node_services/nodes_monitor.js
@@ -746,8 +746,8 @@ class NodesMonitor extends EventEmitter {
                             dbg.log0('_get_agent_info: set node name',
                                 item.node.name, 'to', updates.name);
 
-                            let agent_config = system_store.data.get_by_id(item.node.agent_config);
-                            let { use_s3 = false, use_storage = true, exclude_drive = [] } = agent_config || {};
+                            let agent_config = system_store.data.get_by_id(item.node.agent_config) || {};
+                            let { use_s3 = false, use_storage = true, exclude_drive = [] } = agent_config;
                             // on first call to get_agent_info enable\disable the node according to the configuration
                             let should_start_service = (info.s3_agent_info && use_s3) ||
                                 (!info.s3_agent_info && use_storage && exclude_drive.indexOf(info.drives[0].mount) === -1);

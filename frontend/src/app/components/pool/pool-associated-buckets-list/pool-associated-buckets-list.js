75a7fc52f358b2faba49d70d59254f33e06db718
nimrod-becker <nimrod.becker@noobaa.com>
2017-04-03 11:49:20 +0300

    fix for #buckets- Show used buckets tab in pool page

---
+++ frontend/src/app/components/pool/pool-associated-buckets-list/pool-associated-buckets-list.js
@@ -0,0 +1,40 @@
+import template from './pool-associated-buckets-list.html';
+import Observer from 'observer';
+import state$ from 'state';
+import * as routes from 'routes';
+import ko from 'knockout';
+import { realizeUri } from 'utils/browser-utils';
+
+// TODO: Replace with data from the state$ when available.
+import { routeContext } from 'model';
+
+class PoolAssociatedBucketsListViewModel extends Observer {
+    constructor({ poolName }) {
+        super();
+
+        this.buckets = ko.observable();        
+        this.bucketCount = ko.observable();
+
+        this.observe(
+            state$.get('nodePools', ko.unwrap(poolName), 'associatedBuckets'),
+            this.onBuckets
+        );
+    }
+
+    onBuckets(buckets) {
+        const system = routeContext().params.system;
+
+        this.buckets(
+            buckets.map(bucket => ({
+                name: bucket,
+                href: realizeUri(routes.bucket, { system, bucket })
+            }))
+        );
+        this.bucketCount(buckets.length);
+    }
+}
+
+export default {
+    viewModel: PoolAssociatedBucketsListViewModel,
+    template: template
+};
--- frontend/src/app/reducers/node-pools-reducer.js
+++ frontend/src/app/reducers/node-pools-reducer.js
@@ -1,4 +1,4 @@
-import { keyByProperty } from    'utils/core-utils';
+import { keyBy, keyByProperty, flatMap, groupBy } from 'utils/core-utils';
 import { createReducer } from 'utils/reducer-utils';
 
 // ------------------------------
@@ -15,23 +15,46 @@ function onApplicationInit() {
 
 function onSystemInfoFetched(state, { info }) {
     const nodePools = info.pools.filter(pool => Boolean(pool.nodes));
+    const bucketMapping = _mapPoolsToBuckets(info.buckets, info.tiers);    
     return keyByProperty(nodePools, 'name', pool => {
         const {
             name,
             mode,
             storage,
-            associated_accounts: associatedAccounts
-        } = pool;
+            associated_accounts: associatedAccounts,            
+        } = pool;        
+        const associatedBuckets = bucketMapping[pool.name];
 
-
-        return { name, mode, storage, associatedAccounts };
+        return { name, mode, storage, associatedAccounts, associatedBuckets };
     });
 }
 
 // ------------------------------
 // Local util functions
 // ------------------------------
+function _mapPoolsToBuckets(buckets, tiers) {
+    const bucketsByTierName = keyBy(
+        buckets,
+        bucket => bucket.tiering.tiers[0].tier,
+        bucket => bucket.name
+    );
 
+    const pairs = flatMap(
+        tiers,
+        tier => tier.attached_pools.map(
+            poolName => ({
+                bucket: bucketsByTierName[tier.name],
+                pool: poolName
+            })
+        )
+    );
+
+    return groupBy(
+        pairs,
+        pair => pair.pool,
+        pair => pair.bucket
+    );
+}
 // ------------------------------
 // Exported reducer function
 // ------------------------------

8e0713f9939a07a374e5c812563f2d55917643de
nb-ohad <ohad.mitrani@noobaa.com>
2017-04-07 19:14:29 +0300

    Remove deprecated animation binding

--- frontend/src/app/bindings/register.js
+++ frontend/src/app/bindings/register.js
@@ -13,7 +13,6 @@ export default function register(ko) {
     registerHandler('href',             require('./href').default);
     registerHandler('scroll',           require('./scroll').default);
     registerHandler('canvas',           require('./canvas').default);
-    registerHandler('animation',        require('./animation').default);
     registerHandler('expand',           require('./expand').default);
     registerHandler('tooltip',          require('./tooltip').default);
     registerHandler('scrollTo',         require('./scroll-to').default);

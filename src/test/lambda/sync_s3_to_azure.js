7464c208f9fc7fde37e4e39af3d5f0aa76220f88
Guy Margalit <guymguym@gmail.com>
2017-04-24 14:43:42 +0300

    sync_s3_to_azure: support more than 1000 keys, work in batches

--- src/test/lambda/sync_s3_to_azure.js
+++ src/test/lambda/sync_s3_to_azure.js
@@ -3,11 +3,25 @@
 var azure = require('azure-storage');
 var AWS = require('aws-sdk');
 
+/* sample event:
+{
+    "s3": {
+        "bucket": "images"
+    },
+    "azure": {
+        "container": "plain-blob-storage",
+        "account_name": "youraccountname",
+        "account_key": "key"
+    }
+}
+*/
+
 module.exports.handler = (event, context, callback) => {
     var s3_bucket = event.s3.bucket;
     var azure_container = event.azure.container;
     var azure_account_name = event.azure.account_name;
     var azure_account_key = event.azure.account_key;
+    var max_keys = event.max_keys || 10;
 
     var s3 = new AWS.S3();
     var blob = azure.createBlobService(
@@ -15,28 +29,34 @@ module.exports.handler = (event, context, callback) => {
         `AccountName=${azure_account_name};` +
         `AccountKey=${azure_account_key}`);
 
-    for_every_s3_object(sync_to_azure_blob)
-        .then(
-            res => callback(null, res),
-            err => callback(err)
-        );
+    var marker;
+    var truncated = true;
+    var count = 0;
 
-    function for_every_s3_object(on_each_item) {
+    loop().then(
+        res => callback(null, `${count} Objects Copied`),
+        err => callback(err)
+    );
+
+    function loop() {
+        if (!truncated) return Promise.resolve();
         return make_promise(
                 cb => s3.listObjects({
-                    Bucket: s3_bucket
+                    Bucket: s3_bucket,
+                    Marker: marker,
+                    MaxKeys: max_keys,
                 }, cb)
             )
-            .then(res => Promise.all(
-                    res.Contents.map(on_each_item)
-                )
-                .then(() => ({
-                    count: res.Contents.length
-                }))
-            );
+            .then(res => {
+                marker = res.NextMarker;
+                truncated = res.IsTruncated;
+                count += res.Contents.length;
+                return Promise.all(res.Contents.map(sync_object));
+            })
+            .then(loop);
     }
 
-    function sync_to_azure_blob(item) {
+    function sync_object(item) {
         return make_promise(
             cb => blob.createBlockBlobFromStream(
                 azure_container,

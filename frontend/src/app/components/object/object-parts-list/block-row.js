048e31f61a028a0081947d2b047844fc7934305c
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2017-05-23 14:05:23 +0300

    Refactoring: block row logic updated

--- frontend/src/app/components/object/object-parts-list/block-row.js
+++ frontend/src/app/components/object/object-parts-list/block-row.js
@@ -2,19 +2,57 @@
 
 import BaseViewModel from 'components/base-view-model';
 import { shortString } from 'utils/string-utils';
-import { getResourceTypeIcon } from 'utils/ui-utils';
-import ko from 'knockout';
-import { systemInfo } from 'model';
 
-let poolsMapObj = {};
+const cloudPoolInfo = (pool_name) => {
+    const shortenNodeName = '---';
+    return {
+        nodeName: null,
+        shortenNodeName,
+        replicaLocation: {
+            text: pool_name,
+            href: null
+        },
+        node: {
+            text: shortenNodeName,
+            href: null
+        }
+    };
+};
+
+const serverPoolInfo = (pool_name, node_name) => {
+    const shortenNodeName = shortString(node_name, 30, 8);
+    return {
+        nodeName: node_name,
+        shortenNodeName,
+        replicaLocation: {
+            text: pool_name,
+            href: {
+                route: 'pool',
+                params: {
+                    pool: pool_name,
+                    tab: null
+                }
+            }
+        },
+        node: {
+            text: shortenNodeName,
+            href: {
+                route: 'node',
+                params: {
+                    pool: pool_name,
+                    node: node_name,
+                    tab: null
+                }
+            }
+        }
+    };
+};
 
 export default class BlockRowViewModel extends BaseViewModel {
-    constructor({ adminfo }, index, count) {
+    constructor({ adminfo }, index, count, poolIconMapping) {
         super();
 
         let { online, in_cloud_pool, node_name, pool_name } = adminfo;
-
-        systemInfo.subscribe(this.updateResourceType);
 
         this.state = {
             name: online ? 'healthy' : 'problem',
@@ -22,70 +60,12 @@ export default class BlockRowViewModel extends BaseViewModel {
             css: online ? 'success' : 'error'
         };
 
-        this.replica = `Replica ${index + 1} of ${count} ${in_cloud_pool ? '(cloud replica)' : ''}`;
+        this.replica = `Replica ${index + 1} of ${count}`;
 
-
-        this.recourseType = ko.observable({});
+        this.recourseType = poolIconMapping()[pool_name];
 
         this.poolName = pool_name;
 
-        if (!in_cloud_pool) {
-            this.nodeName = node_name;
-            this.shortenNodeName = shortString(node_name, 30, 8);
-
-            this.replicaLocation = {
-                text: pool_name,
-                href: {
-                    route: 'pool',
-                    params: {
-                        pool: pool_name,
-                        tab: null
-                    }
-                }
-            };
-
-            this.node = {
-                text: this.shortenNodeName,
-                href: {
-                    route: 'node',
-                    params: {
-                        pool: pool_name,
-                        node: node_name,
-                        tab: null
-                    }
-                }
-            };
-
-        } else {
-            this.nodeName = null;
-            this.shortenNodeName = '---';
-
-            this.replicaLocation = {
-                text: pool_name,
-                href: null
-
-            };
-
-            this.node = {
-                text: this.shortenNodeName,
-                href: null
-            };
-        }
-
-        this.updateResourceType();
-    }
-
-    updateResourceType() {
-        const poolName = this.poolName;
-
-        if (!Object.keys(poolsMapObj).length) {
-            systemInfo() && systemInfo().pools.map( pool => poolsMapObj[pool.name] = pool );
-        }
-
-        const pool = poolsMapObj[poolName];
-
-        if(pool) {
-            this.recourseType(getResourceTypeIcon(pool));
-        }
+        Object.assign(this, in_cloud_pool ? cloudPoolInfo(pool_name) : serverPoolInfo(pool_name, node_name));
     }
 }

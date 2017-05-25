c876621fa92d88a5af520820e35731c0a96132e3
nb-ohad <ohad.mitrani@noobaa.com>
2017-05-25 17:58:26 +0300

    Rewrite bar draw algorithm
    Added support for:
    1. teeth
    2. limits
    3. markers

--- frontend/src/app/components/shared/bar/bar.js
+++ frontend/src/app/components/shared/bar/bar.js
@@ -4,23 +4,32 @@ import template from './bar.html';
 import BaseViewModel from 'components/base-view-model';
 import ko from 'knockout';
 import style from 'style';
-import { echo } from 'utils/core-utils';
+import { isFunction, echo, clamp } from 'utils/core-utils';
 
 const defaultEmptyColor = style['color15'];
-const labelsColor = style['color7'];
+const teethColor = style['color7'];
+const limitsColor = style['color7'];
+const markersTextColor = style['white'];
+const markersBorderColor = style['white'];
+const markersBackgroundColor = style['color1'];
+const fontSize = style['font-size1'];
+
 const defaultMinRatio = .03;
-const seperatorWidth = 1;
-const font = `${style['font-size1']} ${style['font-family1']}`;
+const markersMargin = 8;
+const markersHeight = Math.ceil(parseInt(fontSize) * 1.5 + markersMargin);
+const font = `${fontSize} ${style['font-family1']}`;
 
 class BarViewModel extends BaseViewModel {
     constructor({
         values = [],
         height = 2,
         width,
-        drawLabels = false,
-        labelFormatter = echo,
+        teeth = 0,
+        limits = false,
+        markers,
         emptyColor = defaultEmptyColor,
-        minRatio = defaultMinRatio
+        minRatio = defaultMinRatio,
+
     }) {
         super();
 
@@ -33,63 +42,128 @@ class BarViewModel extends BaseViewModel {
 
         this.emptyColor = emptyColor;
         this.values = values;
-        this.drawLabels = drawLabels;
-        this.labelFormatter = labelFormatter;
+        this.teeth  = teeth;
+        this.limits = limits;
+        this.markers = markers;
         this.barHeight = height;
         this.minRatio = minRatio;
-
-        this.barOffset = ko.pureComputed(
-            () => ko.unwrap(drawLabels) ? 16 : 0
-        );
+        this.formatter = isFunction(limits) ? limits : echo;
 
         this.canvasWidth = width;
         this.canvasHeight = ko.pureComputed(
-            () => this.barOffset() + ko.unwrap(height) + 6
+            () => {
+                const markers = ko.unwrap(this.markers);
+                const markersLine = limits || (markers && markers.length > 0);
+                return ko.unwrap(height) +
+                    2 * ko.unwrap(teeth) +
+                    (markersLine ?  markersHeight : 0);
+            }
+        );
+
+        this.barOffset = ko.pureComputed(
+            () => {
+                const limits = ko.unwrap(this.limits);
+                const markers = ko.unwrap(this.markers);
+                return limits || (markers && markers.length > 0) ? markersHeight : 0;
+            }
         );
     }
 
     draw(ctx, { width }) {
+        const hasValues = (ko.unwrap(this.values) || []).length > 0;
+
+        this._drawBar(ctx, width);
+
+        if (hasValues && ko.unwrap(this.limits)) {
+            this._drawLimits(ctx, width, 0);
+        }
+
+        if (hasValues && ko.unwrap(this.markers)) {
+            this._drawMarkers(ctx, width);
+        }
+    }
+
+    _drawBar(ctx, width ) {
+        const teeth = ko.unwrap(this.teeth);
         const values = ko.deepUnwrap(this.values);
         const total = this.total();
-        const drawLabels = ko.unwrap(this.drawLabels);
-        const formatter = this.labelFormatter;
-        const barHeight = ko.unwrap(this.barHeight);
         const minRatio = ko.unwrap(this.minRatio);
-        const y = this.barOffset() + 3;
+        const barOffset = this.barOffset();
+        const barHeight = ko.unwrap(this.barHeight);
 
-        // Draw the line background.
-        ctx.textBaseline = 'top';
-        ctx.font = font;
         ctx.fillStyle = ko.unwrap(this.emptyColor);
-        ctx.fillRect(0, y, width, barHeight);
+        ctx.fillRect(0, barOffset + teeth, width, barHeight);
 
-        // Draw the parts.
         let offset = 0;
-        let sum = 0;
         for (const { value, color } of values) {
             if (value === 0) continue;
             const ratio = Math.max(value / total, minRatio);
 
             ctx.fillStyle = color;
-            ctx.fillRect(Math.round(offset), y, Math.round(ratio * width), barHeight);
+            ctx.fillRect(
+                Math.round(offset),
+                barOffset + teeth,
+                Math.round(ratio * width),
+                barHeight
+            );
 
-            if (drawLabels) {
-                ctx.fillStyle = labelsColor;
-                ctx.fillRect(Math.round(offset), y - 3, seperatorWidth, barHeight + 6);
-                ctx.textAlign = offset === 0 ? 'left' : 'center';
-                ctx.fillText(formatter(sum), offset, 0);
+            if (teeth) {
+                ctx.fillStyle = teethColor;
+                ctx.fillRect(Math.round(offset), barOffset, 1, barHeight + 2 * teeth);
             }
 
             offset += ratio * width;
-            sum += value;
         }
 
-        // Draw the end scale.
-        if (values.length && drawLabels) {
-            ctx.fillStyle = labelsColor;
-            ctx.fillRect(width - seperatorWidth, y - 3, seperatorWidth, barHeight + 6);
-            ctx.textAlign = 'right';
-            ctx.fillText(formatter(sum), offset, 0);
+        if (values.length && teeth) {
+            ctx.fillStyle = teethColor;
+            ctx.fillRect(Math.round(offset) - 1 , barOffset, 1, barHeight + 2 * teeth);
+        }
+    }
+
+    _drawLimits(ctx, width) {
+        const formatter = this.formatter;
+
+        ctx.textBaseline = 'top';
+        ctx.font = font;
+        ctx.fillStyle = limitsColor;
+        ctx.textAlign = 'left';
+        ctx.fillText(formatter(0), 0, 2);
+        ctx.textAlign = 'right';
+        ctx.fillText(formatter(this.total()), width, 2);
+    }
+
+    _drawMarkers(ctx, width) {
+        const total = this.total();
+        const markers = ko.unwrap(this.markers);
+
+        for (const { placement, label } of markers) {
+            const offset = Math.floor(clamp(placement / total, 0, 1) * width);
+
+            const boxWidth = ctx.measureText(label).width + 8;
+            const boxHeight = markersHeight - markersMargin;
+            const x = Math.floor(clamp(offset - boxWidth / 2, 0, width - boxWidth));
+
+            ctx.textBaseline = 'top';
+            ctx.font = font;
+            ctx.fillStyle = markersBackgroundColor;
+            ctx.strokeStyle = markersBorderColor;
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(x, 1);
+            ctx.lineTo(x, boxHeight);
+            ctx.lineTo(Math.max(offset - 4, 0), boxHeight);
+            ctx.lineTo(offset, boxHeight + 4);
+            ctx.lineTo(Math.min(offset + 4, width), boxHeight);
+            ctx.lineTo(x + boxWidth, boxHeight);
+            ctx.lineTo(x + boxWidth, 1);
+            ctx.lineTo(x, 1);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+            ctx.textAlign = 'left';
+            ctx.fillStyle = markersTextColor;
+            ctx.fillText(label, x + 4, 2);
         }
     }
 }

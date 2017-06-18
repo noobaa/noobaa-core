1e2a1848df96ffc63bb978aec80b68896fa48d61
jeniawhite <jeniawhite92@gmail.com>
2017-06-18 14:36:24 +0300

    Fixing bug regarding deletion of pool

--- src/server/system_services/bucket_server.js
+++ src/server/system_services/bucket_server.js
@@ -1066,8 +1066,13 @@ function get_bucket_info(bucket, nodes_aggregate_pool, aggregate_data_free_by_ti
         last_update: _.get(bucket, 'storage_stats.last_update')
     };
 
-    info.usage_by_pool.pools = _.mapKeys(_.get(bucket, 'storage_stats.pools', {}), function(storage, pool_id) {
-        return system_store.data.get_by_id(pool_id).name;
+    info.usage_by_pool.pools = _.omitBy(_.mapKeys(_.get(bucket, 'storage_stats.pools', {}), function(storage, pool_id) {
+        const pool = system_store.data.get_by_id(pool_id);
+        if (pool) {
+            return system_store.data.get_by_id(pool_id).name;
+        }
+    }), function(value, key) {
+        return !_.isUndefined(key);
     });
 
     const stats = bucket.stats;

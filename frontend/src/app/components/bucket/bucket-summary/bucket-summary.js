d8cd46925225e994f68a35b35f681f17a65a52ad
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2017-06-27 11:54:38 +0300

    Update bucket summary graphs according to mock (start with row usage)

--- frontend/src/app/components/bucket/bucket-summary/bucket-summary.js
+++ frontend/src/app/components/bucket/bucket-summary/bucket-summary.js
@@ -1,20 +1,23 @@
 /* Copyright (C) 2016 NooBaa */
 
 import template from './bucket-summary.html';
-import BaseViewModel from 'components/base-view-model';
+import Observer from 'observer';
 import ko from 'knockout';
 import style from 'style';
-import { systemInfo } from 'model';
 import { deepFreeze } from 'utils/core-utils';
-import { toBytes } from 'utils/size-utils';
+import { toBytes, formatSize } from 'utils/size-utils';
+import { formatTime } from 'utils/string-utils';
+import { drawLine } from 'utils/canvas-utils';
+import { routeContext } from 'model';
+import { state$ } from 'state';
 
 const stateMapping = deepFreeze({
-    true: {
+    OPTIMAL: {
         text: 'Healthy',
         css: 'success',
         icon: 'healthy'
     },
-    false: {
+    NOT_WRITABLE: {
         text: 'Not enough healthy resources',
         css: 'error',
         icon: 'problem'
@@ -40,147 +43,192 @@ const quotaUnitMapping = deepFreeze({
     PETABYTE: 'PB'
 });
 
-class BucketSummrayViewModel extends BaseViewModel {
-    constructor({ bucket }) {
+const graphOptions = deepFreeze([
+    { value: 'available', label: 'Available' },
+    { value: 'dataUsage', label: 'Data Usage' },
+    { value: 'rawUsage', label: 'Raw Usage'}
+]);
+
+class BucketSummrayViewModel extends Observer {
+    constructor() {
         super();
 
-        this.graphOptions = [ 'data', 'storage' ];
+        this.graphOptions = graphOptions;
+        this.dataReady = ko.observable(false);
+        this.state = ko.observable();
 
-        this.dataReady = ko.pureComputed(
-            () => !!bucket()
-        );
+        this.dataPlacement  = ko.observable();
+        this.cloudSyncStatus = ko.observable();
+        this.viewType = ko.observable(this.graphOptions[0].value);
 
-        this.state = ko.pureComputed(
-            () => stateMapping[
-                Boolean(bucket() && bucket().writable)
-            ]
-        );
+        this.totalStorage = ko.observable();
 
-        this.dataPlacement = ko.pureComputed(
-            () => {
-                if (!bucket() || !systemInfo()) {
-                    return;
-                }
-
-                const tierName = bucket().tiering.tiers[0].tier;
-                const { data_placement , attached_pools } = systemInfo().tiers.find(
-                    tier => tier.name === tierName
-                );
-
-                return `${
-                    data_placement === 'SPREAD' ? 'Spread' : 'Mirrored'
-                } on ${
-                    attached_pools.length
-                } pool${
-                    attached_pools.length !== 1 ? 's' : ''
-                }`;
-            }
-        );
-
-        this.cloudSyncStatus = ko.pureComputed(
-            () => {
-                if (!bucket()) {
-                    return;
-                }
-
-                const { cloud_sync } = bucket();
-                return cloudSyncStatusMapping[
-                    cloud_sync ? cloud_sync.status : 'NOTSET'
-                ];
-            }
-        );
-
-        this.viewType = ko.observable(this.graphOptions[0]);
-
-        const storage = ko.pureComputed(
-            () => bucket() ? bucket().storage.values : {}
-        );
-
-        const data = ko.pureComputed(
-            () => bucket() ? bucket().data : {}
-        );
-
-        this.totalStorage = ko.pureComputed(
-            () => storage().total
-        ).extend({
-            formatSize: true
-        });
-
-        this.storageValues = [
+        this.availableValues = [
             {
-                label: 'Available',
-                color: style['color5'],
-                value: ko.pureComputed(
-                    () => toBytes(storage().free)
-                )
-            },
-            {
-                label: 'Used (this bucket)',
+                label: 'Used data:',
                 color: style['color13'],
-                value: ko.pureComputed(
-                    () => toBytes(storage().used)
-                )
+                value: ko.observable()
             },
             {
-                label: 'Used (other buckets)',
-                color: style['color14'],
-                value: ko.pureComputed(
-                    () => toBytes(storage().used_other)
-                )
+                label: 'Available to upload:',
+                color: style['color7'],
+                value: ko.observable()
+            },
+            {
+                label: 'Available spillover:',
+                color: style['color6'],
+                value: ko.observable(),
             }
         ];
 
-        this.dataValues = [
+        this.rawUsageValues = [
+            {
+                label: 'Available from resources',
+                color: style['color5'],
+                value: ko.observable()
+            },
+            {
+                label: 'Available spillover',
+                color: style['color6'],
+                value: ko.observable()
+            },
+            {
+                label: 'Used by bucket (with replicas)',
+                color: style['color13'],
+                value: ko.observable()
+            },
+            {
+                label: 'Used (on shared resources)',
+                color: style['color14'],
+                value: ko.observable()
+            }
+        ];
+
+        this.dataUsageValues = [
             {
                 label: 'Total Original Size',
-                value: ko.pureComputed(
-                    () => toBytes(data().size)
-                ),
+                value: ko.observable(),
                 color: style['color7']
             },
             {
                 label: 'Compressed & Deduped',
-                value: ko.pureComputed(
-                    () => toBytes(data().size_reduced)
-                ),
+                value: ko.observable(),
                 color: style['color13']
             }
         ];
 
+        this.legend = ko.observable();
+        this.availableForWrite = ko.observable();
+        this.availableForWriteTootlip = availableForWriteTooltip;
+        this.bucketQuota = ko.observable();
+        this.lastAccess = ko.observable();
 
         this.legend = ko.pureComputed(
-            () => this.viewType() === 'storage' ?
-                this.storageValues :
-                this.dataValues
+            () => this[`${this.viewType()}Values`]
         );
 
-        this.availableForWrite = ko.pureComputed(
-            () => data().available_for_upload
-        ).extend({
-            formatSize: true
-        });
+        this.segments = ko.observable();
+        this.redraw = ko.observable();
 
-        this.availableForWriteTootlip = availableForWriteTooltip;
+        this.observe(state$.get('buckets'), this.onState);
+    }
 
-        const stats = ko.pureComputed(
-            () => bucket() ? bucket().stats : {}
+    onState(buckets) {
+        const bucketsList = Object.values(buckets);
+        const bucket = bucketsList.find(
+            ({ name }) => routeContext().params.bucket === name
+        );
+        const { data, stats, quota, cloudSyncStatus, mode } = bucket;
+        const storage = bucket.storage.values;
+
+        this.dataPlacement(`${
+            bucket.backingResources.type === 'SPREAD' ? 'Spread' : 'Mirrored'
+            } on ${
+            bucket.backingResources.resources.length
+            } pool${
+            bucket.backingResources.resources.length !== 1 ? 's' : ''
+            }`
         );
 
-        this.bucketQuota = ko.pureComputed(
-            () => {
-                const quota = bucket() && bucket().quota;
-                return quota ?
-                    `Set to ${quota.size}${quotaUnitMapping[quota.unit]}` :
-                    'Disabled';
-            }
+        this.totalStorage = ko.observable(formatSize(storage.total));
+        this.state(stateMapping[mode]);
+        this.cloudSyncStatus(cloudSyncStatusMapping[cloudSyncStatus]);
 
-        );
+        this.availableValues[0].value(toBytes(data.size));
+        this.availableValues[1].value(toBytes(data.available_for_upload));
+        this.availableValues[2].value(toBytes(storage.spillover_free));
 
-        this.lastAccess = ko.pureComputed(
-            () => Math.max(stats().last_read, stats().last_write)
-        ).extend({
-            formatTime: true
-        });
+        this.rawUsageValues[0].value(toBytes(storage.free));
+        this.rawUsageValues[1].value(toBytes(storage.spillover_free));
+        this.rawUsageValues[2].value(toBytes(storage.used));
+        this.rawUsageValues[3].value(toBytes(storage.used_other));
+        this.dataUsageValues[0].value(toBytes(data.size));
+        this.dataUsageValues[1].value(toBytes(data.size_reduced));
+
+        this.availableForWrite = ko.observable(formatSize(data.available_for_upload));
+
+        this.bucketQuota(quota ?
+            `Set to ${quota.size}${quotaUnitMapping[quota.unit]}` :
+            'Disabled');
+
+        this.lastAccess(formatTime(Math.max(stats.last_read, stats.last_write)));
+        this.dataReady(true);
+    }
+
+    drawChart(ctx, { width }) {
+        // Create a dependency on redraw allowing us to redraw every time,
+        // the value of redraw changed.
+        this.redraw();
+
+        const baseLine = 20;
+        const vpad = 4;
+        const pointRadius = 5;
+        const lineWidth = width - vpad * 2;
+        const minSegmentSize = 10;
+
+        const usedDataSize = this.availableValues[0].value();
+        const availableSize = this.availableValues[1].value();
+        const spilloverSize = this.availableValues[2].value();
+        const totalSize = usedDataSize + availableSize + spilloverSize;
+
+        let usedDataSegmentWidth = lineWidth * (usedDataSize/totalSize);
+        let availableSegmentWidth = lineWidth * (availableSize/totalSize);
+        let spilloverSegmentWidth = lineWidth * (spilloverSize/totalSize);
+
+        // min segment size correction
+        if(minSegmentSize > usedDataSegmentWidth) {
+            usedDataSegmentWidth = minSegmentSize;
+            availableSegmentWidth = lineWidth - minSegmentSize - spilloverSegmentWidth;
+        }
+
+        if(minSegmentSize > availableSegmentWidth) {
+            availableSegmentWidth = minSegmentSize;
+        }
+
+        if(minSegmentSize > spilloverSegmentWidth) {
+            spilloverSegmentWidth = minSegmentSize;
+            availableSegmentWidth = lineWidth - minSegmentSize - usedDataSegmentWidth;
+        }
+
+        ctx.strokeStyle = this.availableValues[0].color;
+        drawLine(ctx, vpad , baseLine, usedDataSegmentWidth, baseLine);
+        ctx.strokeStyle = this.availableValues[1].color;
+        drawLine(ctx, usedDataSegmentWidth, baseLine, usedDataSegmentWidth + availableSegmentWidth, baseLine);
+        ctx.strokeStyle = this.availableValues[2].color;
+        drawLine(ctx, usedDataSegmentWidth + availableSegmentWidth, baseLine, lineWidth, baseLine);
+
+        ctx.fillStyle = style['color7'];
+        ctx.font = `12px ${style['font-family1']}`;
+        ctx.textAlign = 'center';
+        ctx.fillText('0', vpad, 12);
+        drawLine(ctx, vpad , baseLine + pointRadius, vpad, baseLine - pointRadius);
+        drawLine(ctx, usedDataSegmentWidth , baseLine + pointRadius, usedDataSegmentWidth, baseLine - pointRadius);
+        drawLine(ctx, usedDataSegmentWidth + availableSegmentWidth , baseLine + pointRadius, usedDataSegmentWidth + availableSegmentWidth, baseLine - pointRadius);
+        drawLine(ctx, lineWidth , baseLine + pointRadius, lineWidth, baseLine -pointRadius);
+    }
+
+    triggerRedraw() {
+        this.redraw.toggle();
     }
 }
 

800960b5a7b08303ff86bb5b46cbd7a30f7e9c39
nb-ohad <ohad.mitrani@noobaa.com>
2017-06-29 19:47:13 +0300

    Enhance restore_session logic with read_auth retries in case of RPC_CONNECT_TIMEOUT

--- frontend/src/app/epics/restore-session.js
+++ frontend/src/app/epics/restore-session.js
@@ -3,27 +3,50 @@
 import api from 'services/api';
 import { RESTORE_SESSION } from 'action-types';
 import { completeRestoreSession, failResotreSession } from 'action-creators';
+import { readAuthRetryCount } from 'config';
+import Rx from 'rx';
+
+function _createUnauthorizedException(message) {
+    const error = new Error(message);
+    error.rpc_code = 'UNAUTHORIZED';
+    return error;
+}
 
 export default function(action$) {
     return action$
         .ofType(RESTORE_SESSION)
-        .flatMap(async action => {
+        .flatMap(action => {
             const { token } = action.payload;
-
-            try {
-                api.options.auth_token = token;
-                const sessionInfo = await api.auth.read_auth();
-                if (!sessionInfo.account) {
-                    throw Object.assign(
-                        new Error('Account not found'),
-                        { rpc_code: 'UNAUTHORIZED'}
-                    );
-                }
-
-                return completeRestoreSession(token, sessionInfo);
-
-            } catch (error) {
-                return failResotreSession(token, error);
+            if (!token) {
+                const error = _createUnauthorizedException('Token not available');
+                throw { token, error };
             }
-        });
+
+            api.options.auth_token = token;
+            return Rx.Observable.fromPromise(() => api.auth.read_auth())
+                .map(sessionInfo => {
+                    if (!sessionInfo.account) {
+                        throw _createUnauthorizedException('Account not found');
+                    }
+
+                    return completeRestoreSession(token, sessionInfo);
+                })
+                .retryWhen(errors => {
+                    return errors
+                        .scan((count, err) => {
+                            if (err.rpc_code !== 'RPC_CONNECT_TIMEOUT' || count >= readAuthRetryCount) {
+                                throw err;
+                            }
+
+                            return count + 1;
+                        }, 0)
+                        .delay(1000);
+                })
+                .catch(error => {
+                    throw { token, error };
+                });
+        })
+        .catch(({ token, error }) => Rx.Observable.of(
+            failResotreSession(token, error)
+        ));
 }

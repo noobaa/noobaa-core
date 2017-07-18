c38a5ed7656c342e6f85af9cf67a4068d7eaabab
Danny Zaken <danny.zaken@noobaa.com>
2017-07-18 17:56:22 +0300

    added prints to mongo_upgrade of ip ranges + refactoring

--- src/deploy/mongo_upgrade/mongo_upgrade_19.js
+++ src/deploy/mongo_upgrade/mongo_upgrade_19.js
@@ -19,17 +19,32 @@ function update_cluster_address() {
     if (!res.is_clusterized) {
         res.owner_address = '127.0.0.1';
         res.shards[0].servers[0].address = '127.0.0.1';
+        db.clusters.updateOne({}, { $set: res });
     }
-    db.clusters.updateOne({}, { $set: res });
 }
 
 function update_allowed_ips() {
     // change the allowed_ips to a range format of {start, end}
     var accounts = db.accounts.find({}).toArray();
-    for (var account of accounts) {
-        if (account.allowed_ips && account.allowed_ips.length && !account.allowed_ips[0].start) {
-            account.allowed_ips = account.allowed_ips.map(ip => ({ start: ip, end: ip }));
+    print('\nupdate_allowed_ips: accounts before update: ');
+    printjson(accounts);
+    accounts.forEach(account => {
+        if (account.allowed_ips && account.allowed_ips.length) {
+            const allowed_ips = account.allowed_ips.map(ip => {
+                if (typeof ip === 'string') {
+                    return {
+                        start: ip,
+                        end: ip
+                    };
+                }
+                return ip;
+            });
+            print('update_allowed_ips: updating account ' + account.name + ' to have allowed_ips = ');
+            printjson(allowed_ips);
+            db.accounts.updateOne({ _id: account._id }, { $set: { allowed_ips: allowed_ips } });
         }
-        db.accounts.updateOne({ _id: account._id }, { $set: account });
-    }
+    });
+    accounts = db.accounts.find({}).toArray();
+    print('\nupdate_allowed_ips: accounts after update: ');
+    printjson(accounts);
 }

b18e7feb0cf4a3c6f828de4010ebd5465a67003a
nimrod-becker <nimrod.becker@noobaa.com>
2017-08-02 12:00:53 +0300

    fix path

--- src/deploy/build_windows_agent.bat
+++ src/deploy/build_windows_agent.bat
@@ -30,7 +30,7 @@ copy ..\..\config.js .
 mkdir .\src\
 xcopy /Y/I/E ..\..\src\agent .\src\agent
 xcopy /Y/I/E ..\..\src\s3 .\src\s3
-xcopy /Y/I/E ..\..\src\lambda .\src\lambda
+xcopy /Y/I/E ..\..\src\endpoint .\src\endpoint
 xcopy /Y/I/E ..\..\src\util .\src\util
 xcopy /Y/I/E ..\..\src\rpc .\src\rpc
 xcopy /Y/I/E ..\..\src\api .\src\api

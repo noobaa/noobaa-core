811f38e1739511c39d1337ed20dfd240812d7330
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2017-08-05 08:53:05 +0300

    Fixed issue #3322 Missing validation for wrong entry in bucket quota leads to "update failed"

--- frontend/src/app/validations.js
+++ frontend/src/app/validations.js
@@ -109,6 +109,10 @@ export function isEmail(value) {
     return matchPattern(value, regExp);
 }
 
+export function isInteger(value) {
+    return /^\+?(0|[1-9]\d*)$/.test(value);
+}
+
 export default function register(ko) {
     Object.assign(ko.validation.rules, {
         notIn: {
@@ -172,6 +176,10 @@ export default function register(ko) {
         inRange: {
             validator: inRange,
             message: ({ params }) => `Must be bewteen ${params.min} and ${params.max}`
+        },
+        isInteger: {
+            validator: isInteger,
+            message: 'Please enter a valid integer'
         }
     });
 

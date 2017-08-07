a2825d5d8faf0c84e222ac99f5094112c1a52427
Danny Zaken <danny.zaken@noobaa.com>
2017-08-07 08:12:18 +0300

    use 'name' as the host identifier in host api

--- src/server/node_services/node_schema.js
+++ src/server/node_services/node_schema.js
@@ -61,6 +61,11 @@ module.exports = {
             type: 'string'
         },
 
+        // an incremental sequence number
+        host_sequence: {
+            type: 'string'
+        },
+
         ip: {
             // the public ip of the node
             type: 'string',

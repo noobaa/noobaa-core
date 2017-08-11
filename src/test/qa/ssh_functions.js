5d3bedd2e45b11e9146cff3cac9e3ae42942ea05
YuliaKovalenko <1111Yvolikova6075>
2017-08-11 18:35:24 +0300

    add clean_ova and create system
    externalServerIP setup
    
    setup right js
    
    changed internal ip
    
    changed internal ip
    
    run clean on system config env
    
    run clean on system config env
    
    run clean before system config
    
    run clean before system config
    
    run clean before system config
    
    run clean before system config
    
    run all
    
    run all with esLine edit
    
    run all with esLine edit
    
    run all with esLine edit
    
    changed path secret
    
    add clean_ova and create system
    
    externalServerIP setup
    
    setup right js
    
    changed internal ip
    
    changed internal ip
    
    run clean on system config env
    
    run clean on system config env
    
    run clean before system config
    
    run clean before system config
    
    run clean before system config
    
    run clean before system config
    
    run all
    
    run all with esLine edit
    
    run all with esLine edit
    
    run all with esLine edit
    
    changed path secret
    
    review changes
    
    divided into separate functions, changed env var
    
    change env var
    
    changes after review
    
    committed main ssh
    
    committed main ssh
    
    edit secret path
    
    change ssh exe
    
    ssh close
    
    ssh dispose
    
    ssh end
    
    ssh client and stream end
    
    ssh client and stream end close
    
    change env data. reboot method
    
    reject without error
    
    do throw error
    
    add param
    
    reject without error
    
    promise without reject
    
    p.resolve
    
    always return accept
    
    always return accept
    
    always return accept
    
    keealiveinterval = 0
    
    { x11: true }
    
    { x11: true }
    
    { x11: true }
    
    { x11: true }
    
    { x11: true }
    
    delete x11 from client
    
    add client x11 resolve
    
    add client pty resolve
    
    remove x11
    
     x11: true as parameter
    
    ssh -X
    
    ssh -X
    
    --debug
    
    -x
    
    -x sudo
    
    -xt with agentForward
    
    -xt with agentForward
    
    -xt with agentForward
    
    delete ssh -xt
    
    accept
    
    reject
    
    delete test_function.js
    
    committed disconnect
    
    add new rpc client
    
    end client delete
    
    disconnect rpc
    
    disconnect rpc and create new connection
    
    setup 2 rpc connection
    
    setup 2 rpc connection
    
    setup new rpc connection
    
    try client close
    
    new connection
    
    new function get secret
    
    new function get secret
    
    separate create system
    
    separate create system
    
    separate create system and get secret
    
    setup manually secret
    
    change ssh exec
    
    clean and create system
    
    set one rpc
    
    set one rpc
    
    set rpc
    
    revert jenkins file
    
    add env arg
    
    run eslink inspection

---
+++ src/test/qa/ssh_functions.js
@@ -0,0 +1,27 @@
+/* Copyright (C) 2016 NooBaa */
+'use strict';
+
+var P = require('../../util/promise');
+
+function ssh_connect(client, options) {
+    return new P((resolve, reject) => client
+        .once('ready', resolve)
+        .once('error', reject)
+        .connect(options));
+}
+
+function ssh_exec(client, command) {
+    console.log('Execute ssh command ' + command);
+    return P.fromCallback(callback => client.exec(command, { pty: true }, callback))
+        .then(stream => new P((resolve, reject) => {
+            stream.on('data', data => console.log('ssh_exec: output', data.toString()))
+                .on('error', reject)
+                .on('end', () => {
+                    console.log('ssh_exec: Done');
+                    resolve();
+                });
+        }));
+}
+
+exports.ssh_exec = ssh_exec;
+exports.ssh_connect = ssh_connect;

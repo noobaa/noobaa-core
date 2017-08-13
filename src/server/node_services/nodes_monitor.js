6336aaa58833897ca9df9d525024fe43b1eeea08
Danny Zaken <danny.zaken@noobaa.com>
2017-08-13 15:02:29 +0300

    filter out non FS nodes in list hosts

--- src/server/node_services/nodes_monitor.js
+++ src/server/node_services/nodes_monitor.js
@@ -2034,6 +2034,7 @@ class NodesMonitor extends EventEmitter {
             const item = this._consolidate_host(host);
 
             // filter hosts according to query
+            if (item.node.node_type !== 'BLOCK_STORE_FS') continue;
             if (query.hosts && !query.hosts.includes(String(item.node.host_sequence))) continue;
             if (query.pools && !query.pools.has(String(item.node.pool))) continue;
             if (query.filter && !query.filter.test(this._item_hostname(item)) && !query.filter.test(item.node.ip)) continue;

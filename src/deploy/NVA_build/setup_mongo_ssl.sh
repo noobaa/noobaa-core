277bb3d0eae0a2f648097c516c8dccce5179c01b
nimrod-becker <nimrod.becker@noobaa.com>
2017-09-05 20:35:25 +0300

    Fix SSL Certificates creation
    Fix cert

--- src/deploy/NVA_build/setup_mongo_ssl.sh
+++ src/deploy/NVA_build/setup_mongo_ssl.sh
@@ -15,7 +15,7 @@ openssl genrsa -out ${MONGO_SSL_PATH}/root-ca.key 2048
 # !!! In production you will want to use -aes256 to password protect the keys
 # openssl genrsa -aes256 -out root-ca.key 2048
 
-openssl req -new -x509 -days 3650 -key ${MONGO_SSL_PATH}/root-ca.key -out ${MONGO_SSL_PATH}/root-ca.crt -subj "$dn_prefix/CN=ROOTCA"
+openssl req -new -x509 -days 7300 -key ${MONGO_SSL_PATH}/root-ca.key -out ${MONGO_SSL_PATH}/root-ca.crt -subj "$dn_prefix/CN=ROOTCA"
 
 mkdir -p ${MONGO_SSL_PATH}/RootCA/ca.db.certs
 echo "01" >> ${MONGO_SSL_PATH}/RootCA/ca.db.serial
@@ -83,8 +83,8 @@ openssl genrsa -out ${MONGO_SSL_PATH}/signing-ca.key 2048
 # !!! In production you will want to use -aes256 to password protect the keys
 # openssl genrsa -aes256 -out signing-ca.key 2048
 
-openssl req -new -days 3650 -key ${MONGO_SSL_PATH}/signing-ca.key -out ${MONGO_SSL_PATH}/signing-ca.csr -subj "$dn_prefix/CN=CA-SIGNER"
-openssl ca -batch -name RootCA -config ${MONGO_SSL_PATH}/root-ca.cfg -extensions v3_ca -out ${MONGO_SSL_PATH}/signing-ca.crt -infiles ${MONGO_SSL_PATH}/signing-ca.csr 
+openssl req -new -days 7300 -key ${MONGO_SSL_PATH}/signing-ca.key -out ${MONGO_SSL_PATH}/signing-ca.csr -subj "$dn_prefix/CN=CA-SIGNER"
+openssl ca -batch -name RootCA -startdate 201708010000Z -config ${MONGO_SSL_PATH}/root-ca.cfg -extensions v3_ca -out ${MONGO_SSL_PATH}/signing-ca.crt -infiles ${MONGO_SSL_PATH}/signing-ca.csr 
 
 mkdir -p ${MONGO_SSL_PATH}/SigningCA/ca.db.certs
 echo "01" >> ${MONGO_SSL_PATH}/SigningCA/ca.db.serial
@@ -105,8 +105,8 @@ echo "##### STEP 4: Create server certificates"
 for host in "${mongodb_server_hosts[@]}"; do
 	echo "Generating key for $host"
   	openssl genrsa  -out ${MONGO_SSL_PATH}/${host}.key 2048
-	openssl req -new -days 3650 -key ${MONGO_SSL_PATH}/${host}.key -out ${MONGO_SSL_PATH}/${host}.csr -subj "$dn_prefix/OU=$ou_member/CN=${host}"
-	openssl ca -batch -name SigningCA -config ${MONGO_SSL_PATH}/root-ca.cfg -out ${MONGO_SSL_PATH}/${host}.crt -infiles ${MONGO_SSL_PATH}/${host}.csr
+	openssl req -new -days 7300 -key ${MONGO_SSL_PATH}/${host}.key -out ${MONGO_SSL_PATH}/${host}.csr -subj "$dn_prefix/OU=$ou_member/CN=${host}"
+	openssl ca -batch -name SigningCA -startdate 201708010000Z -config ${MONGO_SSL_PATH}/root-ca.cfg -out ${MONGO_SSL_PATH}/${host}.crt -infiles ${MONGO_SSL_PATH}/${host}.csr
 	cat ${MONGO_SSL_PATH}/${host}.crt ${MONGO_SSL_PATH}/${host}.key > ${MONGO_SSL_PATH}/${host}.pem	
 done 
 
@@ -116,8 +116,8 @@ echo "##### STEP 5: Create client certificates"
 for host in "${mongodb_client_hosts[@]}"; do
 	echo "Generating key for $host"
   	openssl genrsa  -out ${MONGO_SSL_PATH}/${host}.key 2048
-	openssl req -new -days 3650 -key ${MONGO_SSL_PATH}/${host}.key -out ${MONGO_SSL_PATH}/${host}.csr -subj "$dn_prefix/OU=$ou_client/CN=${host}"
-	openssl ca -batch -name SigningCA -config ${MONGO_SSL_PATH}/root-ca.cfg -out ${MONGO_SSL_PATH}/${host}.crt -infiles ${MONGO_SSL_PATH}/${host}.csr
+	openssl req -new -days 7300 -key ${MONGO_SSL_PATH}/${host}.key -out ${MONGO_SSL_PATH}/${host}.csr -subj "$dn_prefix/OU=$ou_client/CN=${host}"
+	openssl ca -batch -name SigningCA -startdate 201708010000Z -config ${MONGO_SSL_PATH}/root-ca.cfg -out ${MONGO_SSL_PATH}/${host}.crt -infiles ${MONGO_SSL_PATH}/${host}.csr
 	cat ${MONGO_SSL_PATH}/${host}.crt ${MONGO_SSL_PATH}/${host}.key > ${MONGO_SSL_PATH}/${host}.pem
 done 
 

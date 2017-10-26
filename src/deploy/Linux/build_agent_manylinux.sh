b0ee536322deb5b7efb8eeda3e26858bb2bf4d71
Guy Margalit <guymguym@gmail.com>
2017-10-26 15:29:57 +0300

    build_agent_manylinux.sh: forgot to send arguments to build_agent_linux.sh

--- src/deploy/Linux/build_agent_manylinux.sh
+++ src/deploy/Linux/build_agent_manylinux.sh
@@ -4,6 +4,7 @@
 # so that the build will run against old linux glibc shared libraries
 # this is based on dockcross/manylinux-x64 which bundles centos-5 with gcc-4.8
 # see https://github.com/dockcross/dockcross
+mkdir -p build
 
 # build noobaa-manylinux-x64 image which adds node.js on top of dockcross/manylinux-x64
 # docker will use cached build if available
@@ -16,4 +17,4 @@ docker run --rm noobaa-manylinux-x64 > build/noobaa-manylinux-x64 || exit 1
 rm -rf build/linux
 
 # run build_agent_linux.sh inside the container
-bash build/noobaa-manylinux-x64 src/deploy/Linux/build_agent_linux.sh || exit 1
+bash build/noobaa-manylinux-x64 src/deploy/Linux/build_agent_linux.sh "$@" || exit 1

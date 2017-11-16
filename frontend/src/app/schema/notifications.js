7e308704b866f47275387d6248755eccff954027
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2017-11-16 16:16:44 +0200

    Schema for notifications & alerts

---
+++ frontend/src/app/schema/notifications.js
@@ -0,0 +1,40 @@
+export default {
+    type: 'object',
+    required: [
+        'nextId',
+        'list'
+    ],
+    properties: {
+        nextId: {
+            type: 'number'
+        },
+        list: {
+            type: 'array',
+            items: {
+                type: 'object',
+                required: [
+                    'id',
+                    'message',
+                    'severity'
+                ],
+                properties: {
+                    id: {
+                        type: 'number'
+                    },
+                    message: {
+                        type: 'string'
+                    },
+                    severity: {
+                        type: 'string',
+                        enum: [
+                            'info',
+                            'success',
+                            'warning',
+                            'error'
+                        ]
+                    }
+                }
+            }
+        }
+    }
+};

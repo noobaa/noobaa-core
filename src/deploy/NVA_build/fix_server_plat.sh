c665db5491af7fc197785e90d905dbea44dfc026
nimrod-becker <nimrod.becker@noobaa.com>
2017-11-23 19:13:18 +0200

    Add user registration for AWS instances

--- src/deploy/NVA_build/fix_server_plat.sh
+++ src/deploy/NVA_build/fix_server_plat.sh
@@ -14,6 +14,10 @@ if [ ! -f ${NOOBAASEC} ]; then
       jwt=$(cat /etc/noobaa_sec | openssl sha512 -hmac | cut -c10-44)
       echo "JWT_SECRET=${jwt}"  >> /root/node_modules/noobaa-core/.env
   fi
+  if grep -q PLATFORM=aws /root/node_modules/noobaa-core/.env; then 
+  	instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+  	curl -H "Content-Type: application/json" -X POST -d '{"properties__email__value":"'${instance_id}'@noobaa.com"}' https://hooks.zapier.com/hooks/catch/440450/52rywu/
+  fi
 fi
 
 # if running on AWS then write\fix the region in .env

44bf38e8cbfdb9794c008fe1aa874103c5356e17
nimrod-becker <nimrod.becker@noobaa.com>
2017-11-23 19:13:18 +0200

    Add user registration for AWS instances
    fixed committed conflict

--- src/deploy/NVA_build/fix_server_plat.sh
+++ src/deploy/NVA_build/fix_server_plat.sh
@@ -17,6 +17,11 @@ if [ ! -f ${NOOBAASEC} ]; then
 
   #Reload services so they would read the newly created JWT
   supervisorctl restart bg_workers hosted_agents s3rver webserver || true
+  
+  if grep -q PLATFORM=aws /root/node_modules/noobaa-core/.env; then 
+  	instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+  	curl -H "Content-Type: application/json" -X POST -d '{"properties__email__value":"'${instance_id}'@noobaa.com"}' https://hooks.zapier.com/hooks/catch/440450/52rywu/
+  fi
 fi
 
 # if running on AWS then write\fix the region in .env

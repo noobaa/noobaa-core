bca5d01576520a58b3fa3fb8e76deab51cec49e8
nb-ohad <ohad.mitrani@noobaa.com>
2017-12-20 12:05:49 +0200

    FE - Differentiate between aborted and failed uploads
    FE - trim email before signin and validation
    
    fixes #4106
    
    FE - fix missing upload counter on uploads indicator

--- frontend/src/app/components/modals/file-uploads-modal/upload-row.js
+++ frontend/src/app/components/modals/file-uploads-modal/upload-row.js
@@ -2,9 +2,37 @@
 
 import ko from 'knockout';
 import numeral from 'numeral';
+import { deepFreeze } from 'utils/core-utils';
 import { formatSize } from 'utils/size-utils';
 import { realizeUri } from 'utils/browser-utils';
 import * as routes from 'routes';
+
+const errorCodeToProgressText = deepFreeze({
+    NoSuchUpload: 'ABORTED'
+});
+
+function _getProgress(upload) {
+    const { completed, error, size, loaded } = upload;
+
+    if (completed) {
+        if (error) {
+            return {
+                text: errorCodeToProgressText[error.code] || 'FAILED',
+                tooltip: error.message,
+                css: 'error'
+            };
+        } else {
+            return {
+                text: 'COMPLETED',
+                css: 'success'
+            };
+        }
+    } else {
+        return {
+            text: (size > 0 ? numeral(loaded/size).format('%') : 0)
+        };
+    }
+}
 
 export default class UploadRowViewModel {
     constructor() {
@@ -15,11 +43,7 @@ export default class UploadRowViewModel {
     }
 
     update(upload, system) {
-        const { name, bucket, completed, error, size, loaded } = upload;
-        const progressText = completed ?
-            (error ? 'FAILED' : 'UPLOADED') :
-            (size > 0 ? numeral(loaded/size).format('%') : 0);
-
+        const { name, bucket, completed, error, size } = upload;
         const fileNameData = {
             text: name,
             tooltip: {
@@ -39,11 +63,7 @@ export default class UploadRowViewModel {
         this.fileName(fileNameData);
         this.bucketName(bucket);
         this.size(formatSize(size));
-        this.progress({
-            text: progressText,
-            tooltip: error,
-            css: error ? 'error' : (completed ? 'success' : '')
-        });
+        this.progress(_getProgress(upload));
 
         return this;
     }

dc9557f829c73ece1d1348d48bfc69348c4533a4
Guy Margalit <guymguym@gmail.com>
2017-12-26 02:44:02 +0200

    FE: add nb.print_json_in_new_tab function to share nb.state from QA

--- frontend/src/app/dev-cli.js
+++ frontend/src/app/dev-cli.js
@@ -12,12 +12,19 @@ const actions = mapValues(
     }
 );
 
+function print_json_in_new_tab(data) {
+    data = JSON.stringify(data, undefined, 2);
+    const blob = new Blob([data], { type: 'text/json' });
+    window.open(window.URL.createObjectURL(blob));
+}
+
 const cli = Object.seal({
     model: model,
     schema: schema.def,
     actions: actions,
     state: undefined,
-    api: api
+    api: api,
+    print_json_in_new_tab
 });
 
 state$.subscribe(state => { cli.state = state; });

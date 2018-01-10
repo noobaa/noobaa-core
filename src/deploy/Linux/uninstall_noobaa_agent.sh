a254447a5dd720e0a65749d9a02880f17140c882
jackyalbo <jacky.albo@gmail.com>
2018-01-10 17:20:03 +0200

    fixing problem with uninstall in linux

--- src/deploy/Linux/uninstall_noobaa_agent.sh
+++ src/deploy/Linux/uninstall_noobaa_agent.sh
@@ -5,9 +5,9 @@ PATH=/usr/local/noobaa:$PATH;
 
 #attempting to remove service installations
 chmod 777 /usr/local/noobaa/remove_service.sh
-systemctl stop noobaalocalservice >> /dev/null
 /usr/local/noobaa/remove_service.sh
 
 /usr/local/noobaa/node /usr/local/noobaa/src/agent/agent_uninstall.js --remove_noobaa_storage
 rm -rf /usr/local/noobaa
+systemctl stop noobaalocalservice >> /dev/null
 echo "NooBaa agent removed"

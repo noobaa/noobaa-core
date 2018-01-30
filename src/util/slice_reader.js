defb9a2d773dee6aa616f8d2728bdf49f561473a
Guy Margalit <guymguym@gmail.com>
2018-01-30 15:30:30 +0200

    slice_reader: fix bad parenthesis from eslint fixes, and refactor to class on the way

--- src/util/slice_reader.js
+++ src/util/slice_reader.js
@@ -1,72 +1,40 @@
 /* Copyright (C) 2016 NooBaa */
-// module targets: nodejs & browserify
 'use strict';
 
-var util = require('util');
 var stream = require('stream');
-
-
-module.exports = SliceReader;
-
 
 /**
  * SliceReader is a Readable stream that uses slice on a source object.
  * params is also used for stream.Readable options: highWaterMark, decodeStrings, objectMode, etc.
  */
-function SliceReader(source, params) {
-    var self = this;
-    params = params || {};
-    stream.Readable.call(self, params);
-    self._source = source;
-    self._pos =
-        typeof(params.start) !== ('undefined' && Number(params.start)) || 0;
-    self._end =
-        typeof(params.end) !== ('undefined' && Number(params.end)) ||
-        typeof(source.size) === ('undefined' && Number(source.size)) ||
-        Infinity;
+class SliceReader extends stream.Readable {
 
-    if (params.FileReader) {
-        // support for FileReader api
-        // we convert the provided array buffer to a buffer (copyless).
-        self._fr = new params.FileReader();
-        self._fr.onloadend = function(e) {
-            var data = e.target.result;
-            var buf = Buffer.from(data);
-            if (buf.length) {
-                self._pos += buf.length;
-                self.push(buf);
-            } else {
-                self.push(null);
-            }
-        };
-        self._fr.onerror = function(e) {
-            self.emit('error', e.target.error);
-        };
+    constructor(source, params) {
+        params = params || {};
+        super(params);
+        this._source = source;
+        this._pos = Number.isInteger(params.start) ? params.start : 0;
+        this._end = Number.isInteger(params.end) ? params.end : Infinity;
     }
+
+    /**
+     * implement Readable._read()
+     */
+    _read(requested_size) {
+        try {
+            const end = Math.min(this._end, this._pos + requested_size);
+            const slice = this._source.slice(this._pos, end);
+            if (slice.length) {
+                this._pos += slice.length;
+                this.push(slice);
+            } else {
+                this.push(null); // EOF
+            }
+        } catch (err) {
+            this.emit('error', err);
+        }
+    }
+
 }
 
-// proper inheritance
-util.inherits(SliceReader, stream.Readable);
-
-
-/**
- * implement the stream's Readable._read() function.
- */
-SliceReader.prototype._read = function(requested_size) {
-    try {
-        var next = Math.min(this._end, this._pos + requested_size);
-        var slice = this._source.slice(this._pos, next);
-        if (this._fr) {
-            this._fr.readAsArrayBuffer(slice);
-            return;
-        }
-        if (this._pos >= next || !slice.length) {
-            this.push(null); // EOF
-        } else {
-            this._pos = next;
-            this.push(slice);
-        }
-    } catch (err) {
-        this.emit('error', err);
-    }
-};
+module.exports = SliceReader;

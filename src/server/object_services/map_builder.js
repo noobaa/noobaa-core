b3379aa51b9e00c35443257aeafbef99794045b0
Guy Margalit <guymguym@gmail.com>
2018-02-08 12:14:32 +0200

    mapper: protect from too many deletions (see #4418)

--- src/server/object_services/map_builder.js
+++ src/server/object_services/map_builder.js
@@ -181,6 +181,7 @@ class MapBuilder {
             // only delete blocks if the chunk is in good shape,
             // that is no allocations needed, and is accessible.
             if (mapping.accessible && !mapping.allocations && mapping.deletions) {
+                dbg.log0('MapBuilder.build_chunks: print mapping on deletions', mapping);
                 this.delete_blocks = this.delete_blocks || [];
                 js_utils.array_push_all(this.delete_blocks, mapping.deletions);
             }
@@ -321,10 +322,12 @@ class MapBuilder {
             pools, avoid_nodes, allocated_hosts, { special_replica }
         );
 
-        dbg.log0('MapBuilder.allocate_block: block', block_id,
-            'node', node._id, node.name, 'chunk', chunk._id, 'frag', frag);
+        if (!node) {
+            dbg.log0('MapBuilder.allocate_block: no nodes for allocation', block_id, 'chunk', chunk._id, 'frag', frag);
+            throw new Error('MapBuilder.allocate_block: no nodes for allocation');
+        }
 
-        if (!node) throw new Error('MapBuilder.allocate_block: no nodes for allocation');
+        dbg.log0('MapBuilder.allocate_block: block', block_id, 'node', node._id, node.name, 'chunk', chunk._id, 'frag', frag);
 
         const block = {
             _id: block_id,

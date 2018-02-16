9a3b1dbeb3c909b4fd7e5f87c89f150448db1363
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2018-02-16 09:49:31 +0200

    Fixed issue: clicking "My Connections" link from "Create Namespace Resource" modal does not close the modal

---
+++ frontend/src/app/epics/close-modals-on-location-change.js
@@ -0,0 +1,10 @@
+/* Copyright (C) 2018 NooBaa */
+
+import { CHANGE_LOCATION } from 'action-types';
+import { closeModal } from 'action-creators';
+
+export default function(action$) {
+    return action$
+        .ofType(CHANGE_LOCATION)
+        .map(() => closeModal(Number.POSITIVE_INFINITY));
+}

65ecabe114d2238b52469532225a2adf92bb16f2
nb-ohad <ohad.mitrani@noobaa.com>
2018-02-20 13:50:10 +0200

    Replace colored JSONs with html trees
    Minor fixes t debug console
    
    Fix minor state bug

--- frontend/src/debug/app.js
+++ frontend/src/debug/app.js
@@ -1,99 +1,10 @@
+/* Copyright (C) 2016 NooBaa */
+
 import MessageListViewModel from './message-list';
 import ko from 'knockout';
 import { state$, action$ } from 'state';
 import numeral from 'numeral';
-
-function _highlightJson(json) {
-    json = json
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-
-    return json.replace(
-        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null|undefined)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-        match => {
-            let cls = '';
-            if (match.startsWith('"')) {
-                if (match.endsWith(':')) {
-                    cls = 'key';
-                } else {
-                    cls = 'string';
-                }
-            } else if (match === 'true' || match === 'false') {
-                cls = 'boolean';
-            } else if (match === 'null') {
-                cls = 'null';
-            } else if (!Number.isNaN(Number(match))) {
-                cls = 'number';
-            }
-            return `<span class="${cls}">${match}</span>`;
-        }
-    );
-}
-
-function _stringify(object, highlight = false, empty = '') {
-    const json = (JSON.stringify(object, null, 2) || '')
-        .replace(/\\n/g, '\n');
-
-    return json ?
-        (highlight ? _highlightJson(json) : json) :
-        empty;
-}
-
-function _diff(curr, prev, diffs = [], path = '') {
-    if (Array.isArray(curr)) {
-        if (Array.isArray(prev)) {
-            const len = Math.max(curr.length, prev.length);
-            for (let i = 0; i < len; ++i) {
-                _diff(curr[i], prev[i], diffs, `${path}[${i}]`);
-            }
-
-        } else {
-            diffs.push({
-                path: path,
-                toValue: curr,
-                form: prev
-            });
-        }
-
-    } else if (curr === null) {
-        if (prev !== null) {
-            diffs.push({
-                path: path || '.',
-                fromValue: prev,
-                toValue: curr
-            });
-        }
-
-    } else if (typeof curr === 'object'){
-        if (typeof prev === 'object') {
-            const keys = new Set([
-                ...Object.keys(curr),
-                ...Object.keys(prev)
-            ]);
-
-            for (const key of keys) {
-                _diff(curr[key], prev[key], diffs, `${path}.${key}`);
-            }
-
-        } else {
-            diffs.push({
-                path: path || '.',
-                form: prev,
-                toValue: curr
-            });
-        }
-
-    } else if (curr !== prev) {
-        diffs.push({
-            path: path || '.',
-            fromValue: prev,
-            toValue: curr
-        });
-    }
-
-    return diffs;
-}
+import { buildHtmlTree, diff, wrap } from 'utils';
 
 function _formatDiff(diff) {
     return diff.map(record => {
@@ -101,19 +12,11 @@ function _formatDiff(diff) {
 
         const parts = [];
         if (fromValue) {
-            const html = _stringify(fromValue, true)
-                .split('\n')
-                .map(line => ` <span class="prefix">-</span>  ${line}`)
-                .join('\n');
-            parts.push(`<span class="json unset">${html}</span>`);
+            parts.push(wrap(buildHtmlTree(fromValue, '-'), 'inspector unset'));
         }
 
         if (toValue) {
-            const html = _stringify(toValue, true)
-                .split('\n')
-                .map(line => ` <span class="prefix">+</span>  ${line}`)
-                .join('\n');
-            parts.push(`<span class="json set">${html}</span>`);
+            parts.push(wrap(buildHtmlTree(toValue, '+'), 'inspector set'));
         }
 
         return {
@@ -161,14 +64,15 @@ export default class AppViewModel {
         this.selectedStateTab(stateView);
 
         if (selected) {
+            const { payload } = selected.action;
             this.isMessageSelected(true);
-            this.actionPayload(_stringify(selected.action.payload, true, '[No Payload]'));
-            this.fullState(_stringify(selected.state, true));
+            this.actionPayload(payload ? buildHtmlTree(payload) : '[No Payload]');
+            this.fullState(buildHtmlTree(selected.state));
 
             const prevIndex = messages.findIndex(message => message === selected) - 1;
             const prev = messages[prevIndex] || {};
-            const diff = _diff(selected.state, prev.state);
-            this.stateDiff(diff.length && _formatDiff(diff));
+            const diffResult = diff(selected.state, prev.state);
+            this.stateDiff(diffResult.length && _formatDiff(diffResult));
 
         } else {
             this.isMessageSelected(false);
--- frontend/src/debug/main.js
+++ frontend/src/debug/main.js
@@ -1,3 +1,5 @@
+/* Copyright (C) 2016 NooBaa */
+
 import ko from 'knockout';
 import { action$ } from 'state';
 import AppViewModel from './app.js';

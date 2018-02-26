1d60e6db1cbc88d42047809952b1abdc8fc69643
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2018-02-26 17:02:47 +0200

    cloud resource table to new architecture

--- frontend/src/app/components/resources/cloud-resources-table/cloud-resources-table.js
+++ frontend/src/app/components/resources/cloud-resources-table/cloud-resources-table.js
@@ -1,89 +1,60 @@
 /* Copyright (C) 2016 NooBaa */
 
 import template from './cloud-resources-table.html';
-import BaseViewModel from 'components/base-view-model';
+import Observer from 'observer';
 import ko from 'knockout';
 import CloudResourceRowViewModel from './cloud-resource-row';
-import { systemInfo, routeContext } from 'model';
 import { deepFreeze, throttle, createCompareFunc } from 'utils/core-utils';
-import { navigateTo } from 'actions';
-import { keyByProperty } from 'utils/core-utils';
-import { inputThrottle } from 'config';
-import { action$ } from 'state';
-import { openAddCloudResrouceModal } from 'action-creators';
+import { realizeUri } from 'utils/browser-utils';
+import { inputThrottle, paginationPageSize } from 'config';
+import { action$, state$ } from 'state';
+import { openAddCloudResrouceModal, requestLocation, deleteResource } from 'action-creators';
+
 
 const columns = deepFreeze([
     {
         name: 'state',
         type: 'icon',
-        sortable: true
+        sortable: true,
+        compareKey: () => true
     },
     {
         name: 'type',
         type: 'icon',
-        sortable: true
+        sortable: true,
+        compareKey: resource => resource.type
     },
     {
         name: 'name',
         label: 'resource name',
-        sortable: true
+        sortable: true,
+        compareKey: resource => resource.name
     },
     {
         name: 'buckets',
         label: 'bucket using resource',
-        sortable: true
+        sortable: true,
+        compareKey: resource => resource.usedBy.length
     },
     {
         name: 'cloudBucket',
         label: 'cloud target bucket',
-        sortable: true
+        sortable: true,
+        compareKey: resource => resource.target
     },
     {
         name: 'usage',
         label: 'used capacity by noobaa',
-        sortable: true
+        sortable: true,
+        compareKey: resource => resource.storage.used
     },
     {
-        name: 'deleteBtn',
+        name: 'deleteButton',
         label: '',
         css: 'delete-col',
         type: 'delete'
     }
 ]);
-
-const resourcesToBuckets = ko.pureComputed(
-    () => {
-        if (!systemInfo()) {
-            return {};
-        }
-
-        const poolsByName = keyByProperty(systemInfo().pools, 'name');
-        return systemInfo().buckets.reduce(
-            (mapping, bucket) => systemInfo().tiers
-                .find(tier => tier.name === bucket.tiering.tiers[0].tier)
-                .attached_pools
-                    .filter(poolName => poolsByName[poolName].resource_type === 'CLOUD')
-                    .reduce(
-                        (mapping, pool) => {
-                            mapping[pool] = mapping[pool] || [];
-                            mapping[pool].push(bucket.name);
-                            return mapping;
-                        },
-                        mapping
-                    ),
-            {}
-        );
-    }
-);
-
-const compareAccessors = deepFreeze({
-    state: () => true,
-    type: resource => resource.cloud_info.endpoint_type,
-    name: resource => resource.name,
-    buckets: resource => (resourcesToBuckets()[resource.name] || []).length,
-    cloudBucket: resource => resource.cloud_info.target_bucket,
-    usage: resource => resource.storage.used
-});
 
 const resourceTypeOptions = [
     {
@@ -110,109 +81,161 @@ const resourceTypeOptions = [
     }
 ];
 
-class CloudResourcesTableViewModel extends BaseViewModel {
+class CloudResourcesTableViewModel extends Observer {
+    columns = columns;
+    pageSize = paginationPageSize;
+    resourceTypeOptions = resourceTypeOptions;
+    selectedResourceType = ko.observable(resourceTypeOptions[0].value);
+    resourcesLoaded = ko.observable();
+    rows = ko.observableArray();
+    filter = ko.observable();
+    typeFilter = ko.observable();
+    sorting = ko.observable();
+    selectedForDelete = ko.observable();
+    page = ko.observable();
+    resourceCount = ko.observable();
+    emptyMessage = ko.observable();
+    onFilterThrottled = throttle(this.onFilter, inputThrottle, this);
+    deleteGroup = ko.pureComputed({
+        read: this.selectedForDelete,
+        write: val => this.onSelectForDelete(val)
+    });
+
     constructor() {
         super();
 
-        this.columns = columns;
+        this.typeSub = this.selectedResourceType.subscribe(value => this.onSelectResourceType(value));
 
-        this.columns = columns;
-
-        const query = ko.pureComputed(
-            () => routeContext().query || {}
+        this.observe(
+            state$.getMany(
+                'cloudResources',
+                'location'
+            ),
+            this.onState
         );
+    }
 
-        this.filter = ko.pureComputed({
-            read: () => query().filter,
-            write: throttle(phrase => this.filterResources(phrase), inputThrottle)
+    onState([cloudResources, location]) {
+        if (!cloudResources) {
+            this.resourcesLoaded(false);
+            return;
+        }
+
+        const { params, query, pathname } = location;
+        const { tab = 'pools' } = params;
+        if (tab !== 'cloud') return;
+
+        const { filter = '', sortBy = 'name', order = 1, page = 0, selectedForDelete, typeFilter = '' } = query;
+        const { compareKey } = columns.find(column => column.name === sortBy);
+        const cloudResourceList = Object.values(cloudResources);
+        const pageStart = Number(page) * this.pageSize;
+        const rowParams = {
+            deleteGroup: this.deleteGroup,
+            onDelete: this.onDeleteCloudResource.bind(this)
+        };
+
+        const filteredRows = cloudResourceList
+            .filter(
+                ({ type, name }) => (!filter || name.toLowerCase().includes(filter)) &&
+                    (!typeFilter || type === typeFilter)
+            );
+
+        const emptyMessage = filteredRows.length > 0 ?
+            'The current filter does not match any cloud resource' :
+            'System does not contain any cloud resources';
+
+        const rows = filteredRows
+            .sort(createCompareFunc(compareKey, order))
+            .slice(pageStart, pageStart + this.pageSize)
+            .map((resource, i) => {
+                const row = this.rows.get(i) || new CloudResourceRowViewModel(rowParams);
+                row.onState(resource);
+                return row;
+            });
+
+        this.pathname = pathname;
+        this.filter(filter);
+        this.typeFilter(typeFilter);
+        this.sorting({ sortBy, order: Number(order) });
+        this.page(Number(page));
+        this.resourceCount(filteredRows.length);
+        this.selectedForDelete(selectedForDelete);
+        this.rows(rows);
+        this.emptyMessage(emptyMessage);
+        this.resourcesLoaded(true);
+    }
+
+    onSelectResourceType(type) {
+        this._query({
+            typeFilter: type,
+            page: 0,
+            selectedForDelete: null
         });
+    }
 
-        this.resourceTypeOptions = resourceTypeOptions;
-        this.selectedResourceType = ko.pureComputed({
-            read: () => query().resourceType || resourceTypeOptions[0].value,
-            write: value => this.selectResourceType(value)
+
+    onFilter(filter) {
+        this._query({
+            filter: filter,
+            page: 0,
+            selectedForDelete: null
         });
+    }
 
-        this.sorting = ko.pureComputed({
-            read: () => {
-                const { sortBy, order } = query();
-                const canSort = Object.keys(compareAccessors).includes(sortBy);
-                return {
-                    sortBy: (canSort && sortBy) || 'name',
-                    order: (canSort && Number(order)) || 1
-                };
-            },
-            write: value => this.orderBy(value)
+    onSort(sorting) {
+        this._query({
+            sorting,
+            page: 0,
+            selectedForDelete: null
         });
-
-        const allResources = ko.pureComputed(
-            () => (systemInfo() ? systemInfo().pools : []).filter(
-                ({ cloud_info }) => Boolean(cloud_info)
-            )
-        );
-
-        this.resources = ko.pureComputed(
-            () => {
-                const { sortBy, order } = this.sorting();
-                const compareOp = createCompareFunc(compareAccessors[sortBy], order);
-                const filter = (this.filter() || '').toLowerCase();
-                const resourceType = this.selectedResourceType();
-
-                return allResources()
-                    .filter(
-                        ({ cloud_info, name }) => name.toLowerCase().includes(filter) &&
-                            (!resourceType || cloud_info.endpoint_type === resourceType)
-                    )
-                    .sort(compareOp);
-            }
-        );
-
-        this.emptyMessage = ko.pureComputed(
-            () => allResources().length > 0 ?
-                'The current filter does not match any cloud resource' :
-                'System does not contain any cloud resources'
-        );
-
-        this.deleteGroup = ko.observable();
     }
 
-    newResourceRow(resource) {
-        return new CloudResourceRowViewModel(
-            resource,
-            resourcesToBuckets,
-            this.deleteGroup
-        );
+    onPage(page) {
+        this._query({
+            page,
+            selectedForDelete: null
+        });
     }
 
-    selectResourceType(type) {
-        this.deleteGroup(null);
+    _query(params) {
+        const {
+            typeFilter = this.typeFilter(),
+            filter = this.filter(),
+            sorting = this.sorting(),
+            page = this.page(),
+            selectedForDelete = this.selectedForDelete()
+        } = params;
 
-        const resourceType = type || undefined;
-        const filter = this.filter() || undefined;
-        const { sortBy, order } = this.sorting() || {};
-        navigateTo(undefined, undefined, { filter, resourceType, sortBy, order });
+        const { sortBy, order } = sorting;
+        const query = {
+            typeFilter: typeFilter || undefined,
+            filter: filter || undefined,
+            sortBy: sortBy,
+            order: order,
+            page: page || undefined,
+            selectedForDelete: selectedForDelete || undefined
+        };
+
+        action$.onNext(requestLocation(
+            realizeUri(this.pathname, {}, query)
+        ));
     }
 
-
-    orderBy({ sortBy, order }) {
-        this.deleteGroup(null);
-
-        const filter = this.filter() || undefined;
-        const resourceType = this.selectedResourceType() || undefined;
-        navigateTo(undefined, undefined, { filter, sortBy, order, resourceType });
-    }
-
-    filterResources(phrase) {
-        this.deleteGroup(null);
-
-        const filter = phrase || undefined;
-        const { sortBy, order } = this.sorting() || {};
-        const resourceType = this.selectedResourceType() || undefined;
-        navigateTo(undefined, undefined, { filter, sortBy, order, resourceType });
+    onSelectForDelete(selected) {
+        const selectedForDelete = this.selectedForDelete() === selected ? null : selected;
+        this._query({ selectedForDelete });
     }
 
     onAddCloudResource() {
         action$.onNext(openAddCloudResrouceModal());
+    }
+
+    onDeleteCloudResource(name) {
+        action$.onNext(deleteResource(name));
+    }
+
+    dispose() {
+        this.typeSub && this.typeSub.dispose();
     }
 }
 

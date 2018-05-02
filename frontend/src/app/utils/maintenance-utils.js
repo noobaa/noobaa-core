a1f63568ff24782e78dfbdc7ecd8de40ca0741d2
nimrod-becker <nimrod.becker@noobaa.com>
2018-05-02 13:56:22 +0300

    fix lint after aslant upgrade

--- frontend/src/app/utils/maintenance-utils.js
+++ frontend/src/app/utils/maintenance-utils.js
@@ -7,12 +7,12 @@ export function formatTimeLeftForMaintenanceMode(timeLeft) {
 
     const duration = moment.duration(timeLeft);
     return `${
-            String(duration.days()).padStart(2, 0)
-        }:${
-            String(duration.hours()).padStart(2, 0)
-        }:${
-            String(duration.minutes()).padStart(2, 0)
-        }:${
-            String(duration.seconds()).padStart(2, 0)
-        }`;
+        String(duration.days()).padStart(2, 0)
+    }:${
+        String(duration.hours()).padStart(2, 0)
+    }:${
+        String(duration.minutes()).padStart(2, 0)
+    }:${
+        String(duration.seconds()).padStart(2, 0)
+    }`;
 }

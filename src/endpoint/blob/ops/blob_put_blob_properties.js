72f455316c4b773eaaa0e1f0942b8d3513968887
Danny Zaken <danny.zaken@noobaa.com>
2018-06-20 13:32:16 +0300

    added empty implementation for put_blob_properties

---
+++ src/endpoint/blob/ops/blob_put_blob_properties.js
@@ -0,0 +1,31 @@
+/* Copyright (C) 2016 NooBaa */
+'use strict';
+
+const http_utils = require('../../../util/http_utils');
+
+
+/**
+ * https://docs.microsoft.com/en-us/rest/api/storageservices/set-blob-properties
+ */
+async function put_blob_properties(req, res) {
+    // TODO: implement PUT Blob Properties. for now just return success if blob exist 
+    return req.object_sdk.read_object_md({
+            bucket: req.params.bucket,
+            key: req.params.key,
+            md_conditions: http_utils.get_md_conditions(req),
+        })
+        .then(object_md => {
+            res.statusCode = 200;
+        });
+
+}
+
+module.exports = {
+    handler: put_blob_properties,
+    body: {
+        type: 'empty',
+    },
+    reply: {
+        type: 'empty',
+    },
+};

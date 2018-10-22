9a35076143263566d761f026779403a1d99050a8
Danny Zaken <danny.zaken@noobaa.com>
2018-10-22 15:42:53 +0300

    fixed test logs uploads to azure after vitaly failures

--- src/test/framework/send_logs.js
+++ src/test/framework/send_logs.js
@@ -12,13 +12,15 @@ const CONTAINER = 'vitaly-servers-logs';
 
 async function main() {
     const blob = azure_storage.createBlobService(process.env.AZURE_STORAGE_CONNECTION_STRING);
-    const noobaa_log_files = (await fs.readdirAsync('/var/log')).filter(f => f.startsWith('noobaa'));
+    const noobaa_log_files = (await fs.readdirAsync('/var/log'))
+        .filter(f => f.startsWith('noobaa'))
+        .map(filename => '/var/log/' + filename);
     noobaa_log_files.push('/tmp/res_1.tgz'); // runner log files
     noobaa_log_files.push('/tmp/ceph_deploy.log'); // ceph build and deploy log
     await P.map(noobaa_log_files, async log_file => {
         try {
             const blob_key = process.env.TEST_RUN_NAME + '/' + log_file;
-            await P.fromCallback(callback => blob.createBlockBlobFromLocalFile(CONTAINER, blob_key, '/var/log/' + log_file, callback));
+            await P.fromCallback(callback => blob.createBlockBlobFromLocalFile(CONTAINER, blob_key, log_file, callback));
         } catch (err) {
             console.error(`Failed uploading ${log_file} to azure`, err);
         }

33fe3e261095f296ffdea15f183ae971628348b4
Danny Zaken <danny.zaken@noobaa.com>
2018-11-11 20:57:21 +0200

    changed mongod to run with mongod user
    fix
    
    more fixes
    
    more fixes
    
    more fixes
    
    fix

--- src/deploy/NVA_build/fix_mongo_ssl.sh
+++ src/deploy/NVA_build/fix_mongo_ssl.sh
@@ -3,7 +3,8 @@ CORE_DIR="/root/node_modules/noobaa-core"
 if [ ! -d /etc/mongo_ssl/ ]; then
   mkdir /etc/mongo_ssl/
   . ${CORE_DIR}/src/deploy/NVA_build/setup_mongo_ssl.sh
-  chmod 400 -R /etc/mongo_ssl
+  chmod 400 -R /etc/mongo_ssl/*
+  chmod 500 /etc/mongo_ssl
   client_subject=`openssl x509 -in /etc/mongo_ssl/client.pem -inform PEM -subject -nameopt RFC2253 | grep subject | awk '{sub("subject= ",""); print}'`
   # add bash script to run mongo shell with authentications
   echo "mongo --ssl --sslPEMKeyFile /etc/mongo_ssl/client.pem --sslCAFile /etc/mongo_ssl/root-ca.pem --sslAllowInvalidHostnames -u \"${client_subject}\" --authenticationMechanism MONGODB-X509 --authenticationDatabase \"\\\$external\" \"\$@\"" > /usr/bin/mongors

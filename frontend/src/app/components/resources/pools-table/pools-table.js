78a4bc07d468aa7a79bae73b1bfcc4c9b779496e
nb-ohad <ohad.mitrani@noobaa.com>
2018-11-20 12:03:04 +0200

    - Update add tier, tier 1 tooltip. - Text changes and fixes - Mach resilient section warning icons to problem severity. - Add pools table empty message to match the new case where there are no pools in the system - Generate css push rules for row and column from minix - Change overview icon

--- frontend/src/app/components/resources/pools-table/pools-table.js
+++ frontend/src/app/components/resources/pools-table/pools-table.js
@@ -242,6 +242,7 @@ class PoolsTableViewModel extends ConnectableViewModel {
     sorting = ko.observable();
     page = ko.observable();
     poolCount = ko.observable();
+    emptyMessage = ko.observable();
     rows = ko.observableArray()
         .ofType(RowViewModel, { table: this })
 
@@ -287,6 +288,12 @@ class PoolsTableViewModel extends ConnectableViewModel {
                 bucketsByPool
             );
 
+            const emptyMessage =
+                (poolList.length === 0 && 'There are no existing nodes pool in the system') ||
+                (filteredPools.length === 0 && 'The current filter does not match any nodes pool') ||
+                '';
+
+
             ko.assignToProps(this, {
                 dataReady: true,
                 pathname: location.pathname,
@@ -294,6 +301,7 @@ class PoolsTableViewModel extends ConnectableViewModel {
                 sorting: { sortBy, order },
                 page: page,
                 poolCount: filteredPools.length,
+                emptyMessage: emptyMessage,
                 rows: filteredPools
                     .sort(compareOp)
                     .slice(pageStart, pageStart + paginationPageSize)

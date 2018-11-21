471a80fa4a8afbf973569c1d5dc5d331cedf3387
jackyalbo <jacky.albo@gmail.com>
2018-11-21 10:52:21 +0200

    fixing the upgrade

--- src/deploy/mongo_upgrade/mongo_upgrade_2_11_0.js
+++ src/deploy/mongo_upgrade/mongo_upgrade_2_11_0.js
@@ -17,7 +17,7 @@ function update_spillover_tiers() {
             print('update_spillover_tiers: no spillover tier');
             return;
         }
-        var second_tier = db.tiers.findOne({ _id: second_tier_in_tiering._id });
+        var second_tier = db.tiers.findOne({ _id: second_tier_in_tiering.tier });
         var second_tier_first_mirror = second_tier && second_tier.mirrors && second_tier.mirrors[0];
         var t2_m1_s1 = second_tier_first_mirror && second_tier_first_mirror.spread_pools &&
             second_tier_first_mirror.spread_pools[0];
@@ -25,12 +25,12 @@ function update_spillover_tiers() {
 
         if (!has_none_internals && (!second_tier_in_tiering.disabled || (second_tier_in_tiering.disabled && has_internal_data))) {
             print('update_spillover_tiers: first tier to become internal');
-            db.tiers.remove({ _id: first_tier_in_tiering._id });
+            db.tiers.remove({ _id: first_tier_in_tiering.tier });
             db.tieringpolicies.updateOne({ _id: tiering._id }, {
                 $set: {
                     tiers: [{
                         order: 0,
-                        tier: second_tier_in_tiering._id,
+                        tier: second_tier_in_tiering.tier,
                         spillover: false,
                         disabled: false
                     }]
@@ -46,12 +46,12 @@ function update_spillover_tiers() {
                 $set: {
                     tiers: [{
                         order: 0,
-                        tier: first_tier_in_tiering._id,
+                        tier: first_tier_in_tiering.tier,
                         spillover: false,
                         disabled: first_tier_in_tiering.disabled,
                     }, {
                         order: 1,
-                        tier: second_tier_in_tiering._id,
+                        tier: second_tier_in_tiering.tier,
                         spillover: false,
                         disabled: second_tier_in_tiering.disabled,
                     }]

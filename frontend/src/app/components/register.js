863ba1b97d365257bf267377ee6b7df39fbabc61
nb-ohad <ohad.mitrani@noobaa.com>
2018-12-27 16:33:45 +0200

    commit as backup

--- frontend/src/app/components/register.js
+++ frontend/src/app/components/register.js
@@ -108,6 +108,7 @@ import addFuncTriggerModal from './modals/add-func-trigger-modal/add-func-trigge
 import bucketPlacementSummaryModal from './modals/bucket-placement-summary-modal/bucket-placement-summary-modal';
 import keepUsingInternalStorageModal from './modals/keep-using-internal-storage-modal/keep-using-internal-storage-modal';
 import addResourcesModal from './modals/add-resources-modal/add-resources-modal';
+import createEmptyPoolModal from './modals/create-empty-pool-modal/create-empty-pool-modal';
 /** INJECT:modals.import **/
 
 // -------------------------------
@@ -464,6 +465,7 @@ export default function register(ko, services) {
         bucketPlacementSummaryModal,
         keepUsingInternalStorageModal,
         addResourcesModal,
+        createEmptyPoolModal,
         /** INJECT:modals.list **/
 
         overviewPanel,
--- frontend/src/app/epics/create-hosts-pool.js
+++ frontend/src/app/epics/create-hosts-pool.js
@@ -5,6 +5,7 @@ import { ofType } from 'rx-extensions';
 import { mapErrorObject } from 'utils/state-utils';
 import { CREATE_HOSTS_POOL } from 'action-types';
 import { completeCreateHostsPool, failCreateHostsPool } from 'action-creators';
+import { all, sleep } from 'utils/promise-utils';
 
 export default function(action$, { api }) {
     return action$.pipe(
@@ -13,7 +14,11 @@ export default function(action$, { api }) {
             const { name, hosts } = action.payload;
 
             try {
-                await api.pool.create_hosts_pool({ name, hosts });
+                await all(
+                    api.pool.create_hosts_pool({ name, hosts }),
+                    sleep(10000)
+                );
+
                 return completeCreateHostsPool(name);
 
             } catch (error) {

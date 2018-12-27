70b8f62e431f8fe9aa36e76e91705e122b61074d
nb-ohad <ohad.mitrani@noobaa.com>
2018-12-27 14:52:09 +0200

    Fix anomalies with func-summary
    - Add data loading indicator
    - Fix layout + tooltip for function description text

--- frontend/src/app/bindings/tooltip-templates/index.js
+++ frontend/src/app/bindings/tooltip-templates/index.js
@@ -8,3 +8,4 @@ export { default as listWithCaption } from './list-with-caption.html';
 export { default as linkListWithCaption } from './link-list-with-caption.html';
 export { default as checkList } from './check-list.html';
 export { default as propertySheet } from './property-sheet.html';
+export { default as preserveWhitespaces } from './preserve-whitespaces.html';

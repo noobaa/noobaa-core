9fa2266c5cb0cef6ef8bda9f5035aae681b18eeb
liranmauda <liran.mauda@gmail.com>
2019-02-10 15:41:48 +0200

    Creating podman_build_agent_image.sh
    Creating podman_build_agent_image.sh
    
    Agent.Dockerfile:
    -Changed the ARG agent_entrypoint path

--- src/deploy/NVA_build/podman_build_agent_image.sh
+++ src/deploy/NVA_build/podman_build_agent_image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-#podman build -f ./src/deploy/NVA_build/Server.Dockerfile -t nbimage --rm ./ || exit 1
 
 OPTIONS=$( getopt -o 'g:' --long "GIT_COMMIT:" -- "$@" )
 eval set -- "${OPTIONS}"
@@ -15,21 +14,27 @@ do
     esac
 done
 
-cp build/public/* ./
-if [ $(ls *.rpm | wc -l) -ne 1 ]
+cp build/linux/* ./
+
+if [ $(ls noobaa-setup* | wc -l) -ne 2 ]
 then
-    echo "There should be only one rpm file, exiting"
+    echo "There should be only two 'noobaa-setup' files, one is the setup and one md5, exiting"
     exit 1
 else
-    mv ./*rpm ./noobaa.rpm
+    rm -rf ./noobaa-setup*.md5 
+    mv ./noobaa-setup* ./noobaa-setup
 fi
-cp ./src/deploy/rpm/install_noobaa.sh ./
-cp ./src/deploy/NVA_build/Server.Dockerfile ./
 
-name="nbimage"
+script="run_agent_container.sh"
+dockerfile="Agent.Dockerfile"
+
+cp ./src/deploy/NVA_build/${script} ./
+cp ./src/deploy/NVA_build/${dockerfile} ./
+
+name="nbagent"
 name_and_tag="${name}:${GIT_COMMIT}"
-echo "Building: podman build --build-arg noobaa_rpm=./noobaa.rpm --build-arg install_script=./install_noobaa.sh -f ./Server.Dockerfile -t ${name_and_tag} --rm ./"
-podman build --no-cache --build-arg noobaa_rpm=./noobaa.rpm --build-arg install_script=./install_noobaa.sh -f ./Server.Dockerfile -t ${name_and_tag} --rm ./ || exit 1
+echo "Building: podman build -f ./${dockerfile} -t ${name_and_tag} --rm ./"
+podman build --no-cache -f ./${dockerfile} -t ${name_and_tag} --rm ./ || exit 1
 echo "Tagging: podman tag localhost/${name_and_tag} noobaaimages.azurecr.io/noobaa/${name_and_tag}"
 podman tag localhost/${name_and_tag} noobaaimages.azurecr.io/noobaa/${name_and_tag}
 echo "Pushing: podman push noobaaimages.azurecr.io/noobaa/${name_and_tag}"

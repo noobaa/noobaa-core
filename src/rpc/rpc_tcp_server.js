c76ff595f5d2215d4aa9ce4876dc853e985f71c9
Guy Margalit <guymguym@gmail.com>
2019-02-24 14:11:02 +0200

    issue #343 - handle more https servers that need honorCipherOrder:true

--- src/rpc/rpc_tcp_server.js
+++ src/rpc/rpc_tcp_server.js
@@ -22,7 +22,7 @@ class RpcTcpServer extends EventEmitter {
         super();
         this.protocol = (tls_options ? 'tls:' : 'tcp:');
         this.server = tls_options ?
-            tls.createServer({ honorCipherOrder: true, ...tls_options }, tcp_conn => this._on_tcp_conn(tcp_conn)) :
+            tls.createServer({ ...tls_options, honorCipherOrder: true }, tcp_conn => this._on_tcp_conn(tcp_conn)) :
             net.createServer(tcp_conn => this._on_tcp_conn(tcp_conn));
         this.server.on('close', err => {
             dbg.log0('on close:', err);

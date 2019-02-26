41ed496ca3096019664a952f9047f5fb3968afca
Danny Zaken <dannyzaken@gmail.com>
2019-02-26 13:50:28 +0200

    fixed statefulset serviceName to match the service in noobaa-operator
    added CONTAINER_PLATFORM=KUBERNETES env in noobaa-operator

--- src/deploy/build_operator.sh
+++ src/deploy/build_operator.sh
@@ -2,20 +2,23 @@
 
 export GOPATH=$PWD/gocode
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [ ! -d "$GOPATH/src/github.com/operator-framework" ]; then
-    echo "getting operator-sdk code"
+    echo -e "${GREEN}getting operator-sdk code${NC}"
     mkdir -p $GOPATH/src/github.com/operator-framework
     cd $GOPATH/src/github.com/operator-framework
     git clone https://github.com/operator-framework/operator-sdk
     cd operator-sdk
     #git checkout master
-    echo "building opeartor-sdk"
+    echo -e "${GREEN}building opeartor-sdk${NC}"
     make dep
     make install
 fi
-echo "getting dependencies. might take some time.."
+echo -e "${GREEN}getting dependencies. might take some time..${NC}"
 cd $GOPATH/src/noobaa-operator
 dep ensure -v
-echo "building noobaa-operator.."
+echo -e "${GREEN}building noobaa-operator..${NC}"
 operator-sdk build noobaa-operator
-echo "completed!"
+echo -e "${GREEN}completed!${NC}"

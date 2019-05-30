2443d7225199f5f6b918e062adba8db304458ca8
liranmauda <liran.mauda@gmail.com>
2019-05-30 15:33:07 +0300

    Renamed install_noobaa.sh to set_mongo_repo.sh and Cleaned the code
    Renamed install_noobaa.sh to set_mongo_repo.sh and Cleaned the code

--- src/deploy/NVA_build/docker_build_container.sh
+++ src/deploy/NVA_build/docker_build_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker build -f ./src/deploy/NVA_build/Server.Dockerfile -t nbimage --rm --squash ./ || exit 1 # squash is an experimental feature
-#docker build --build-arg noobaa_rpm=./noobaa.rpm --build-arg install_script=./src/deploy/rpm/install_noobaa.sh -f ./src/deploy/NVA_build/Server.Dockerfile -t nbimage --rm ./
+#docker build --build-arg noobaa_rpm=./noobaa.rpm --build-arg install_script=./src/deploy/rpm/install.sh -f ./src/deploy/NVA_build/Server.Dockerfile -t nbimage --rm ./
 docker build -f ./src/deploy/NVA_build/Server.Dockerfile -t nbimage --rm ./ || exit 1

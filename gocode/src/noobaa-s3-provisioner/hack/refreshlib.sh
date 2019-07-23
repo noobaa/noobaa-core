1b384ea2d136ace08b5b9ccd68ffd842a2f33e65
Danny Zaken <dannyzaken@gmail.com>
2019-07-23 09:39:32 +0300

    removed gocode folder and operator related files
    Update .travis.yml
    
    Update .travis.yml

--- gocode/src/noobaa-s3-provisioner/hack/refreshlib.sh
+++
@@ -1,8 +0,0 @@
-# refresh the bucket provisioning lib, from the supplied branch (default is 
-# master), and vendor it.
-
-branch="$1"
-[[ -z "$branch" ]] && branch="master"
-
-echo "refreshing bucket lib from \"$branch\" branch..."
-sed -ri "s,(.*lib-bucket-provisioner).*,\1 $branch," go.mod && vgo mod vendor

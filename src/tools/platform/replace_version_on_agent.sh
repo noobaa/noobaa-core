796bffc03042df15257d871742d148088f4dfb01
liranmauda <liran.mauda@gmail.com>
2019-07-23 18:02:43 +0300

    Cleanning unused files related to Azure.
    Removing unused files

--- src/tools/platform/replace_version_on_agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PS4='\e[36m+ ${FUNCNAME:-main}@${BASH_SOURCE}:${LINENO} \e[0m'
-
-fileLocation="/usr/local/noobaa/package.json"
-verArray=($(cat ${fileLocation} | grep version))
-sed -i "s/${verArray[@]}/${verArray[0]} \"test\",/g" ${fileLocation}
-#killing the agent_cli so it will reload the package.json when it get back up
-sudo kill -9 $(ps -ef | grep agent_cli | grep -v grep | awk '{print $2}')

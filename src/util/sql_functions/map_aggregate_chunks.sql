4af36b98e3fb5c9e5b3fa8d250d1119a61095e78
Jenia White <jeniawhite92@gmail.com>
2020-10-07 16:05:25 +0300

    Fixing map_aggregate_chunks
    Signed-off-by: Jenia White <jeniawhite92@gmail.com>
    (cherry picked from commit 8a4b94d22b398df998d1dbadd293fc2fbbea3851)

--- src/util/sql_functions/map_aggregate_chunks.sql
+++ src/util/sql_functions/map_aggregate_chunks.sql
@@ -2,7 +2,9 @@ CREATE OR REPLACE FUNCTION map_aggregate_chunks(TEXT) RETURNS TABLE(_id TEXT[], 
 DECLARE
     cur_query ALIAS FOR $1;
     rec_compress_size NUMERIC;
+    rec_size NUMERIC;
     rec_bucket_id TEXT;
+    response_size NUMERIC;
     rec RECORD;
     cur REFCURSOR;
 BEGIN
@@ -11,12 +13,14 @@ BEGIN
   WHILE FOUND 
   LOOP
     rec_compress_size := rec.data->'compress_size';
+    rec_size := rec.data->'size';
+    response_size := COALESCE(rec_compress_size, rec_size);
     rec_bucket_id := rec.data->>'bucket';
     _id := ARRAY ['', 'compress_size'];
-    value := rec_compress_size;
+    value := response_size;
     RETURN NEXT;
     _id := ARRAY [rec_bucket_id, 'compress_size'];
-    value := rec_compress_size;
+    value := response_size;
     RETURN NEXT;
     FETCH NEXT FROM cur INTO rec; 
   END LOOP;

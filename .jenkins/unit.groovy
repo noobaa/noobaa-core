532448246ec18d505988863312243f7be3d7c908
liranmauda <liran.mauda@gmail.com>
2020-10-12 15:44:43 +0300

    Killing the previous job from the same PR
    -  Killing the previous job from the same PR
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>
    (cherry picked from commit 162d39753e42f5269beddae6e1220bf4d900ded6)
    Signed-off-by: Nimrod Becker <bnimrod@gmail.com>

--- .jenkins/unit.groovy
+++ .jenkins/unit.groovy
@@ -35,27 +35,49 @@ node('cico-workspace') {
 			}
 			firstAttempt = false
 			cico = sh(
-				script: "cico node get -f value -c hostname -c comment --release=8 --retry-count=${cico_retries} --retry-interval=${cico_retry_interval}",
+				script: "cico node get -f value -c hostname -c comment --release=8 \
+							--retry-count=${cico_retries} --retry-interval=${cico_retry_interval}",
 				returnStdout: true
 			).trim().tokenize(' ')
 			env.CICO_NODE = "${cico[0]}.ci.centos.org"
 			env.CICO_SSID = "${cico[1]}"
+			env.CICO_NODE_SSH = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE}"
 		}
 	}
 
 	try {
 		stage('prepare bare-metal machine') {
 			sh 'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./.jenkins/prepare.sh root@${CICO_NODE}:'
-			sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} ./prepare.sh --workdir=/opt/build/noobaa-core --gitrepo=${ci_git_repo} --ref=${ci_git_ref}"
+			sh "${CICO_NODE_SSH} ./prepare.sh --workdir=/opt/build/noobaa-core --gitrepo=${ci_git_repo} --ref=${ci_git_ref}"
+		}
+
+		stage('stop jobs from the same PR') {
+			jobs = sh(
+				script: "${CICO_NODE_SSH} 'cd /opt/build/noobaa-core/.jenkins/ && ./get_job_numbers.sh --jobName ${JOB_NAME} \
+							--currentBuild ${currentBuild.number} --JENKINS_URL ${JENKINS_URL}'",
+				returnStdout: true
+			).trim().tokenize(' ')
+			if ( jobs.isEmpty() ) {
+				println "Nothing to abort"
+			} else {  
+				for (JobNumber in jobs) {
+					int JobNumberint = JobNumber as int
+					println "Aborting ${JOB_NAME} ${JobNumber}"
+					Jenkins.instance.getItemByFullName("${JOB_NAME}")
+									.getBuildByNumber(JobNumberint)
+									.finish(
+									hudson.model.Result.ABORTED,
+									new java.io.IOException("Aborting build")
+								);	
+				}
+			}
 		}
 
 		// real test start here
 		stage('Unit Tests') {
-			//sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} 'cd /opt/build/noobaa-core && make tester ${NO_CACHE} ${USE_HOSTNETWORK} ${CONTAINER_ENGINE}'"
-
 			// abort in case the test hangs
 			timeout(time:30, unit: 'MINUTES') {
-				sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${CICO_NODE} 'cd /opt/build/noobaa-core && make test ${USE_HOSTNETWORK} ${CONTAINER_ENGINE}'"
+				sh "${CICO_NODE_SSH} 'cd /opt/build/noobaa-core && make test ${USE_HOSTNETWORK} ${CONTAINER_ENGINE}'"
 			}
 		}
 	}

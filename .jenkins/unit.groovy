d8c8cc56f0e2948870fceea7a588e1f448f25b49
liranmauda <liran.mauda@gmail.com>
2020-11-01 08:58:09 +0200

    Jenkins: Define some build parameters
    Util.yaml:
    - Using the hidden parameter GIT_REPO
    - status-url points to blueocean
    
    Build-sanity.yaml:
    - Using the hidden parameter GIT_REPO
    
    jjb-deploy.yaml:
    - Adding the ability to load the jobs from a different branch and fork
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>
    (cherry picked from commit 0ec882516c6568edbd24ecb7869c596f45de9041)
    Signed-off-by: Nimrod Becker <bnimrod@gmail.com>

--- .jenkins/unit.groovy
+++ .jenkins/unit.groovy
@@ -1,6 +1,5 @@
 def cico_retries = 16
 def cico_retry_interval = 60
-def ci_git_repo = 'https://github.com/noobaa/noobaa-core'
 def ci_git_ref = 'master' // default, will be overwritten for PRs
 
 def HASH = '0123abcd' // default, will be overwritten
@@ -18,7 +17,7 @@ node('cico-workspace') {
 	stage('checkout ci repository') {
 		// TODO: only need to fetch the .jenkins directory, no tags, ..
 		checkout([$class: 'GitSCM', branches: [[name: 'FETCH_HEAD']],
-			userRemoteConfigs: [[url: "${ci_git_repo}", refspec: "${ci_git_ref}"]]])
+			userRemoteConfigs: [[url: "${GIT_REPO}", refspec: "${ci_git_ref}"]]])
 	}
 
 	stage('reserve bare-metal machine') {
@@ -41,10 +40,8 @@ node('cico-workspace') {
 
 	try {
 		stage('prepare bare-metal machine') {
-			//TODO: merge, then test the output, and the in another PR remove.
-			sh 'echo +++${GIT_REPO}+++'
 			sh 'scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./.jenkins/prepare.sh root@${CICO_NODE}:'
-			sh "${CICO_NODE_SSH} ./prepare.sh --workdir=${workdir} --gitrepo=${ci_git_repo} --ref=${ci_git_ref}"
+			sh "${CICO_NODE_SSH} ./prepare.sh --workdir=${workdir} --gitrepo=${GIT_REPO} --ref=${ci_git_ref}"
 		}
 
 		stage('stop jobs from the same PR') {

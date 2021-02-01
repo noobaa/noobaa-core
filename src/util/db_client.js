438bd5279ee42de172eb5203871b5c0d6da343ec
Evgeniy Belyi <jeniawhite92@gmail.com>
2021-02-01 14:20:25 +0200

    Fixing DB_TYPE assignment
    Signed-off-by: Evgeniy Belyi <jeniawhite92@gmail.com>

--- src/util/db_client.js
+++ src/util/db_client.js
@@ -5,20 +5,19 @@
 const config = require('../../config');
 const postgres_client = require('./postgres_client');
 const mongo_client = require('./mongo_client');
-const DB_TYPE = process.env.DB_TYPE || config.DB_TYPE;
 const dbg = require('./debug_module')(__filename);
 
 /**
  * @returns { nb.DBClient }
  */
 function instance() {
-    switch (DB_TYPE) {
+    switch (config.DB_TYPE) {
         case 'postgres':
             return postgres_client.instance();
         case 'mongodb':
             return mongo_client.instance();
         default: {
-            const str = `NON SUPPORTED DB_TYPE ${DB_TYPE}`;
+            const str = `NON SUPPORTED DB_TYPE ${config.DB_TYPE}`;
             dbg.error(str);
             throw new Error(str);
         }

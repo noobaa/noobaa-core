d65e6583d81e702e710d62e6ed8fee9d57ae0e16
liranmauda <liran.mauda@gmail.com>
2021-04-06 18:39:51 +0300

    Bumped version to 5.8 and updated node modules versions
    bumped sinon and move it to be dev
    
    googleapis 66.0.0 -> 70.0.0
    
    googleapis 66.0.0 -> 70.0.0 is updating:
    1. google-auth-library 6.1.6 -> 7.0.4
    2. googleapis-common 4.4.3 -> 5.0.2
    
    both have breaking changes that seems not to affect us
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- src/test/system_tests/ceph_s3_tests_deploy.sh
+++ src/test/system_tests/ceph_s3_tests_deploy.sh
@@ -29,8 +29,9 @@ commit_epoch=$(git show -s --format=%ci ${CEPH_TESTS_VERSION} | awk '{print $1}'
 commit_date=$(date -d ${commit_epoch} +%s)
 current_date=$(date +%s)
 
-if [ $((current_date-commit_date)) -gt $((3600*24*240)) ]
+max_days="270"
+if [ $((current_date-commit_date)) -gt $((3600*24*${max_days})) ]
 then
-    echo "ceph tests were not updated for 240 days, Exiting"
+    echo "ceph tests were not updated for ${max_days} days, Exiting"
     exit 1
 fi

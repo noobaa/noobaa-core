63094cc6035ef1701e88790e25f6719351b735cb
Romy <romy2232@gmail.com>
2022-02-28 22:55:38 +0200

    Fix endpoint hangs
    Signed-off-by: Romy <romy2232@gmail.com>
    (cherry picked from commit 7e9303d1ad6ef80042a647e9d382b7f74d72175a)
    Signed-off-by: Nimrod Becker <bnimrod@gmail.com>

--- src/util/postgres_client.js
+++ src/util/postgres_client.js
@@ -1001,6 +1001,7 @@ class PostgresTable {
     }
 
     async _upsert(query, update, options) {
+        await this.init_promise;
         const MAX_RETRIES = 5;
         let retries = 0;
         // eslint-disable-next-line no-constant-condition

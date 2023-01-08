5d19b5570ac5a485e75a8e0fee0ccb2c5e6dbf35
liranmauda <liran.mauda@gmail.com>
2023-01-08 15:06:42 +0200

    Consume secret variables from a file.
    - Add a util to get data from a file. will use that to fetch kubernetes secret mounted to a file.
    - Replaced env.NOOBAA_AUTH_TOKEN with config.NOOBAA_AUTH_TOKEN
    - Replaced env.JWT_SECRET wite config.JWT_SECRET
    - Replaced env.SECRET_SERVER with config.SECRET_SERVER
    - Keeping the default behavior of useing env variables for overriding if needed or testing.
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- src/util/jwt_utils.js
+++ src/util/jwt_utils.js
@@ -4,27 +4,23 @@
 /* eslint-disable global-require */
 
 const jwt = require('jsonwebtoken');
+const config = require('../../config');
 const dbg = require('./debug_module')(__filename);
 const JWT_SECRET = get_jwt_secret();
 
 function get_jwt_secret() {
-    if (process.env.JWT_SECRET) {
-        return process.env.JWT_SECRET;
-    } else {
-        // in kubernetes we must have JWT_SECRET loaded from a kubernetes secret
-        if (process.env.CONTAINER_PLATFORM === 'KUBERNETES') {
-            throw new Error('JWT_SECRET env variable not found. it must exist when running in kuberentes');
-        }
-        // for all non kubernetes platforms (docker, local, etc.) return a dummy secret
-        return 'abcdefgh';
+    if (config.JWT_SECRET) return config.JWT_SECRET;
+    // in kubernetes we must have JWT_SECRET loaded from a kubernetes secret
+    if (process.env.CONTAINER_PLATFORM === 'KUBERNETES') {
+        throw new Error('jwt_secret mounted to a file was not found. it must exist when running in kubernetes');
     }
+    // for all non kubernetes platforms (docker, local, etc.) return a dummy secret
+    return 'abcdefgh';
 }
 
 function make_auth_token(object = {}, jwt_options = {}) {
     // Remote services/endpoints should not sign tokens
-    if (process.env.NOOBAA_AUTH_TOKEN) {
-        return process.env.NOOBAA_AUTH_TOKEN;
-    }
+    if (config.NOOBAA_AUTH_TOKEN) return config.NOOBAA_AUTH_TOKEN;
     // create and return the signed token
     return jwt.sign(object, JWT_SECRET, jwt_options);
 }

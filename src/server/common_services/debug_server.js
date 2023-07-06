ec5afb81a806e02540d8e63f96f91b2ebad5ce6c
liranmauda <liran.mauda@gmail.com>
2023-07-06 12:53:49 +0300

    cleaned istanbul and coverage util
    - cleaned istanbul and coverage util
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- src/server/common_services/debug_server.js
+++ src/server/common_services/debug_server.js
@@ -3,7 +3,6 @@
 
 const dbg = require('../../util/debug_module')(__filename);
 const nb_native = require('../../util/nb_native');
-const coverage_utils = require('../../util/coverage_utils');
 
 function set_debug_level(req) {
     dbg.log0('Received set_debug_level req for level', req.rpc_params.level, 'mod', req.rpc_params.module);
@@ -11,16 +10,5 @@ function set_debug_level(req) {
     nb_native().fs.set_debug_level(req.rpc_params.level);
 }
 
-function get_coverage_data(req) {
-    const coverage_data = coverage_utils.get_coverage_data();
-    if (coverage_data) {
-        dbg.log0('get_coverage_data: Returning data to collector');
-    } else {
-        dbg.warn('get_coverage_data: No data');
-    }
-    return { coverage_data };
-}
-
 // EXPORTS
 exports.set_debug_level = set_debug_level;
-exports.get_coverage_data = get_coverage_data;

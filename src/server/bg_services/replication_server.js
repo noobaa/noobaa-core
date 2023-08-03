a7cc5b750852d6577258e9d5359abb834758f783
liranmauda <liran.mauda@gmail.com>
2023-08-03 10:49:55 +0300

    Object Version Replication - add the ability to replicate object versions.
    - Create config.BUCKET_DIFF_FOR_REPLICATION to determine if BucketDiff is in replication mode.
    
    - copy now get an object instead of keys and return an object instead of an array
    
     - copy_objects_mixed_types:
        - Will get objects instead of keys
        - Will return an object of num_of_objects and size_of_objects
        - if there is only one metadata per key, copy it as none version (even if there is a version)
         - If there is more than one metadata, copy all from the older to the newest, considering the versionID
    
     ### Replication Utils:
     - copy_objects will get objects instead of keys
     - Cleaned unused `get_object_md` and `check_data_or_md_changed` functions
    
    - Use BucketDiff for replication instead of the functions in the scanner, and remove the unused functions
    - Support Object versions replication
    - Fix the metrics to consume the new replay format.
    
    1.  will not copy the last version if the latest is a delete marker, in order to do so we will need to be were if there is versioning enabled (maybe add to the replication_api sync_version flag)
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- src/server/bg_services/replication_server.js
+++ src/server/bg_services/replication_server.js
@@ -11,7 +11,7 @@ async function copy_objects(req) {
     switch (copy_type) {
         case 'MIX': {
             const res = await copy_objects_mixed_types(req);
-            console.log('move_objects_by_type: res', res);
+            dbg.log0('move_objects_by_type: res', res);
             return res;
         }
         default:
@@ -65,27 +65,50 @@ async function delete_objects(req) {
 }
 
 async function copy_objects_mixed_types(req) {
-    const { src_bucket_name, dst_bucket_name, keys } = req.rpc_params;
-    dbg.log1('replication_server copy_objects_mixed_types: params:', src_bucket_name, dst_bucket_name, keys);
-    const copy_done_list = [];
+    const { src_bucket_name, dst_bucket_name, keys_diff_map } = req.rpc_params;
+    dbg.log1('replication_server copy_objects_mixed_types: params:', src_bucket_name, dst_bucket_name, keys_diff_map);
+    const copy_res = {
+        num_of_objects: 0,
+        size_of_objects: 0
+    };
+    const keys = Object.keys(keys_diff_map);
 
     const noobaa_con = cloud_utils.set_noobaa_s3_connection(system_store.data.systems[0]);
     if (!noobaa_con) throw new Error('noobaa endpoint connection is not started yet...');
-    await P.map_with_concurrency(100, keys, async key => {
-        const params = {
-            Bucket: dst_bucket_name.unwrap(),
-            CopySource: `/${src_bucket_name.unwrap()}/${key}`, // encodeURI for special chars is needed
-            Key: key
-        };
-        try {
-            await noobaa_con.copyObject(params).promise();
-            copy_done_list.push(key);
-        } catch (err) {
-            dbg.error('replication_server copy_objects_mixed_types: got error:', err);
+    await P.map_with_concurrency(100, keys, async key => { //The concurrency can only be on the keys as the order of the versions matters
+        if (keys_diff_map[key].length === 1) {
+            const params = {
+                Bucket: dst_bucket_name.unwrap(),
+                CopySource: `/${src_bucket_name.unwrap()}/${key}`, // encodeURI for special chars is needed
+                Key: key
+            };
+            try {
+                await noobaa_con.copyObject(params).promise();
+                copy_res.num_of_objects += 1;
+                copy_res.size_of_objects += keys_diff_map[key][0].Size;
+            } catch (err) {
+                dbg.error('replication_server copy_objects_mixed_types: got error:', err);
+            }
+        } else {
+            for (let i = keys_diff_map[key].length - 1; i >= 0; i--) { // We need to replicate the oldest first
+                const version = keys_diff_map[key][i].VersionId;
+                const params = {
+                    Bucket: dst_bucket_name.unwrap(),
+                    CopySource: `/${src_bucket_name.unwrap()}/${key}?versionId=${version}`, // encodeURI for special chars is needed
+                    Key: key
+                };
+                try {
+                    await noobaa_con.copyObject(params).promise();
+                    copy_res.num_of_objects += 1;
+                    copy_res.size_of_objects += keys_diff_map[key][i].Size;
+                } catch (err) {
+                    dbg.error('replication_server copy_objects_mixed_types: got error:', err);
+                }
+            }
         }
     });
     dbg.log1('replication_server copy_objects_mixed_types: finished successfully');
-    return copy_done_list;
+    return copy_res;
 }
 
 ////////////////////////////////////////////

45269c7ac9186cd9168352ed0f12a418c57ac9b9
Amit Prinz Setter <alphaprinz@gmail.com>
2023-11-12 09:27:10 +0200

    bump node to 20.9.0
    Signed-off-by: Amit Prinz Setter <alphaprinz@gmail.com>

--- src/util/google_storage_wrap.js
+++ src/util/google_storage_wrap.js
@@ -1,16 +1,18 @@
 /* Copyright (C) 2016 NooBaa */
 'use strict';
 
-const { Util } = require('@google-cloud/storage/build/src/nodejs-common/util');
+/*const { Util } = require('@google-cloud/storage/build/src/nodejs-common/util');
 const pkg = require('../../package.json');
 const DEV_MODE = (process.env.DEV_MODE === 'true');
+*/
 const crypto_utils = require('./crypto_utils');
 
-let stage_or_prod = 'production';
+/*let stage_or_prod = 'production';
 if (DEV_MODE) {
     stage_or_prod = 'staging';
 }
 Util.prototype.getUserAgentFromPackageJson = () => `NooBaa/${pkg.version} (GPN:noobaa.com; ${stage_or_prod}) NooBaa/${pkg.version}`;
+*/
 
 const { Storage } = require('@google-cloud/storage');
 

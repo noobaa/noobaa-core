27988e65a773b751598477925e1baa6604ff8e7e
naveenpaul1 <napaul@redhat.com>
2024-01-19 16:32:45 +0530

    NSFS | NC | add account uid gid can access new_buckets_path check
    Signed-off-by: naveenpaul1 <napaul@redhat.com>
    (cherry picked from commit bf13a4596efd8c81d361b5e409a24dfb9828ef28)

--- src/test/unit_tests/test_nc_nsfs_health.js
+++ src/test/unit_tests/test_nc_nsfs_health.js
@@ -25,6 +25,7 @@ const DEFAULT_FS_CONFIG = {
     warn_threshold_ms: 100,
 };
 
+const bucket_storage_path = path.join(tmp_fs_path, 'account_inaccessible');
 
 mocha.describe('nsfs nc health', function() {
 
@@ -40,10 +41,13 @@ mocha.describe('nsfs nc health', function() {
             fs_utils.create_fresh_path(`${config_root}/${dir}`)));
         await fs_utils.create_fresh_path(root_path);
         await fs_utils.create_fresh_path(config_root_invalid);
+        await nb_native().fs.mkdir(DEFAULT_FS_CONFIG, bucket_storage_path, 0o770);
     });
     mocha.after(async () => {
         fs_utils.folder_delete(`${config_root}`);
         fs_utils.folder_delete(`${root_path}`);
+        fs_utils.folder_delete(`${config_root_invalid}`);
+        await nb_native().fs.rmdir(DEFAULT_FS_CONFIG, bucket_storage_path);
     });
 
     mocha.describe('health check', async function() {
@@ -52,6 +56,7 @@ mocha.describe('nsfs nc health', function() {
         const new_buckets_path = `${root_path}new_buckets_path_user1/`;
         const account1 = { name: acount_name, nsfs_account_config: { new_buckets_path: new_buckets_path } };
         const bucket1 = { name: bucket_name, path: new_buckets_path + '/bucket1' };
+        const account_inaccessible = { name: 'account_inaccessible', nsfs_account_config: { uid: 999, gid: 999, new_buckets_path: bucket_storage_path } };
         mocha.before(async () => {
             const https_port = 6443;
             Health = new NSFSHealth({ config_root, https_port });
@@ -249,6 +254,27 @@ mocha.describe('nsfs nc health', function() {
             assert.strictEqual(health_status.checks.buckets_status.invalid_buckets.length, 0);
             assert.strictEqual(health_status.checks.accounts_status.valid_accounts.length, 0);
             assert.strictEqual(health_status.checks.accounts_status.invalid_accounts.length, 0);
+            //revert to config root
+            Health.config_root =  config_root;
+        });
+
+        mocha.it('Account with inaccessible path', async function() {
+            await write_config_file(config_root, accounts_schema_dir, account_inaccessible.name, account_inaccessible);
+            Health.get_service_state.restore();
+            Health.get_endpoint_response.restore();
+            Health.all_account_details = true;
+            Health.all_bucket_details = true;
+            const get_service_state = sinon.stub(Health, "get_service_state");
+            get_service_state.onFirstCall().returns(Promise.resolve({ service_status: 'active', pid: 1000 }))
+                .onSecondCall().returns(Promise.resolve({ service_status: 'active', pid: 2000 }));
+            const get_endpoint_response = sinon.stub(Health, "get_endpoint_response");
+            get_endpoint_response.onFirstCall().returns(Promise.resolve({response: {response_code: 'RUNNING', total_fork_count: 0}}));
+            const health_status = await Health.nc_nsfs_health();
+            assert.strictEqual(health_status.checks.buckets_status.valid_buckets.length, 1);
+            assert.strictEqual(health_status.checks.accounts_status.valid_accounts.length, 1);
+            assert.strictEqual(health_status.checks.accounts_status.invalid_accounts.length, 1);
+            assert.strictEqual(health_status.checks.accounts_status.invalid_accounts[0].code, "ACCESS_DENIED");
+            assert.strictEqual(health_status.checks.accounts_status.invalid_accounts[0].name, account_inaccessible.name);
         });
     });
 });

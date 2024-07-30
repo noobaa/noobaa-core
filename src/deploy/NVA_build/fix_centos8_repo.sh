2bb78651feb93a5cfffe044a0ef16c69e62f09a1
Romy <35330373+romayalon@users.noreply.github.com>
2024-07-30 16:30:00 +0300

    Centos8 repository link
    Signed-off-by: Romy <35330373+romayalon@users.noreply.github.com>

--- src/deploy/NVA_build/fix_centos8_repo.sh
+++ src/deploy/NVA_build/fix_centos8_repo.sh
@@ -2,5 +2,5 @@
 
 if [ "$CENTOS_VER" == "8" ]; then
 	sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-	sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+	sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=https://mirror.nsc.liu.se/centos-store|g" /etc/yum.repos.d/CentOS-*
 fi

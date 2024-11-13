7fad1567ca7d457f7bc5a24fc483afcb66b12e4a
Romy <35330373+romayalon@users.noreply.github.com>
2024-11-13 09:12:22 +0200

    remove pre-requisites from centos machine
    Signed-off-by: Romy <35330373+romayalon@users.noreply.github.com>

--- src/test/unit_tests/test_rpm_install.js
+++ src/test/unit_tests/test_rpm_install.js
@@ -6,26 +6,26 @@ const os_utils = require('../../util/os_utils');
 
 mocha.describe('rpm install tests', function() {
     mocha.it('rpm build & install', async () => {
-        console.log('installing pre-requisites');
+        // console.log('installing pre-requisites');
 
-        const install_wget = 'dnf install wget';
-        await exec(install_wget);
-        console.log('installed wget');
+        // const install_wget = 'dnf install wget';
+        // await exec(install_wget);
+        // console.log('installed wget');
 
-        const downlod_boost_system = 'wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/boost-system-1.75.0-8.el9.x86_64.rpm';
-        await exec(downlod_boost_system);
-        console.log('downloaded boost system RPM');
-        const downlod_boost_thread = 'wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/boost-thread-1.75.0-8.el9.x86_64.rpm';
-        await exec(downlod_boost_thread);
-        console.log('downloaded boost thread RPM');
+        // const downlod_boost_system = 'wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/boost-system-1.75.0-8.el9.x86_64.rpm';
+        // await exec(downlod_boost_system);
+        // console.log('downloaded boost system RPM');
+        // const downlod_boost_thread = 'wget https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/boost-thread-1.75.0-8.el9.x86_64.rpm';
+        // await exec(downlod_boost_thread);
+        // console.log('downloaded boost thread RPM');
 
-        const install_boost_system_rpm = 'rpm -i boost-system-1.75.0-8.el9.x86_64.rpm';
-        await exec(install_boost_system_rpm);
-        console.log('installed boost system RPM');
+        // const install_boost_system_rpm = 'rpm -i boost-system-1.75.0-8.el9.x86_64.rpm';
+        // await exec(install_boost_system_rpm);
+        // console.log('installed boost system RPM');
 
-        const install_boost_thread_rpm = 'rpm -i boost-thread-1.75.0-8.el9.x86_64.rpm';
-        await exec(install_boost_thread_rpm);
-        console.log('installed boost thread RPM');
+        // const install_boost_thread_rpm = 'rpm -i boost-thread-1.75.0-8.el9.x86_64.rpm';
+        // await exec(install_boost_thread_rpm);
+        // console.log('installed boost thread RPM');
 
         const cd_cwd_command = 'cd ' + process.cwd();
         await exec(cd_cwd_command);

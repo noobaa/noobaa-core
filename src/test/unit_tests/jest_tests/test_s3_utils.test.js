cad1cb20f8340d9f680d0b3964e535f83184f9d0
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2024-11-18 16:29:42 +0530

    Fix AMZ restore status header
    Co-authored-by: Guy Margalit <guymguym@gmail.com>
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
    (cherry picked from commit 888259bd9115c6d8f3676b59c78efe04a9281ccb)

--- src/test/unit_tests/jest_tests/test_s3_utils.test.js
+++ src/test/unit_tests/jest_tests/test_s3_utils.test.js
@@ -5,6 +5,19 @@ const s3_utils = require('../../../endpoint/s3/s3_utils');
 const { S3Error } = require('../../../endpoint/s3/s3_errors');
 const config = require('../../../../config');
 
+function create_dummy_nb_response() {
+    return {
+        headers: {},
+        setHeader: function(k, v) {
+            if (Array.isArray(v)) {
+                v = v.join(',');
+            }
+
+            this.headers[k] = v;
+        }
+    };
+}
+
 describe('s3_utils', () => {
     describe('parse_restrore_request_days', () => {
         it('should parse correctly when 0 < days < max days', () => {
@@ -55,6 +68,51 @@ describe('s3_utils', () => {
             expect(days).toBe(config.S3_RESTORE_REQUEST_MAX_DAYS);
 
             config.S3_RESTORE_REQUEST_MAX_DAYS_BEHAVIOUR = initial;
+        });
+    });
+
+    describe('set_response_object_md', () => {
+        it('should return no restore status when restore_status is absent', () => {
+            const object_md = {
+                xattr: {}
+            };
+            const res = create_dummy_nb_response();
+
+            // @ts-ignore
+            s3_utils.set_response_object_md(res, object_md);
+
+            expect(res.headers['x-amz-restore']).toBeUndefined();
+        });
+
+        it('should return restore status when restore is requested and ongoing', () => {
+            const object_md = {
+                xattr: {},
+                restore_status: {
+                    ongoing: true,
+                },
+            };
+            const res = create_dummy_nb_response();
+
+            // @ts-ignore
+            s3_utils.set_response_object_md(res, object_md);
+
+            expect(res.headers['x-amz-restore']).toBeDefined();
+        });
+
+        it('should return restore status when restore is completed', () => {
+            const object_md = {
+                xattr: {},
+                restore_status: {
+                    ongoing: false,
+                    expiry_time: new Date(),
+                },
+            };
+            const res = create_dummy_nb_response();
+
+            // @ts-ignore
+            s3_utils.set_response_object_md(res, object_md);
+
+            expect(res.headers['x-amz-restore']).toBeDefined();
         });
     });
 });

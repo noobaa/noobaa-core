3974dba5443316b78ce2aecf9929302fc727dea0
Amit Prinz Setter <alphaprinz@gmail.com>
2024-12-19 18:07:18 +0200

    Bucket Notifications - PR notes, take 2 (#8593)
    1. Switch connect file format kv -> json
    2. Use only filename to specify connect file.
    3. Use AWS s3api schema to store notifications conf.
    
    This 5.18 commit is a backport of 04fe4edf1362e5bafc1f5d43e0ec629697d4f484.
    
    Signed-off-by: Amit Prinz Setter <alphaprinz@gmail.com>

--- src/endpoint/s3/ops/s3_get_bucket_notification.js
+++ src/endpoint/s3/ops/s3_get_bucket_notification.js
@@ -10,6 +10,17 @@ async function get_bucket_notification(req) {
         bucket_name: req.params.bucket,
     });
 
+    //adapt to aws cli structure
+    if (result && result.length > 0) {
+        for (const conf of result) {
+            conf.Event = conf.event;
+            conf.Topic = conf.topic;
+            conf.Id = conf.id;
+            delete conf.vent;
+            delete conf.topic;
+            delete conf.id;
+        }
+    }
 
     const reply = result && result.length > 0 ?
         {
@@ -18,8 +29,9 @@ async function get_bucket_notification(req) {
                 TopicConfiguration: result
             }
         } :
-        //if there's no notification, reuturn empty NotificationConfiguration tag
+        //if there's no notification, return empty NotificationConfiguration tag
         { NotificationConfiguration: {} };
+
 
     return reply;
 }

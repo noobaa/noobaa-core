db60c033e63ba1351f06dfe576504c7d5c7997be
liranmauda <liran.mauda@gmail.com>
2025-02-06 09:26:42 +0200

    Code maintenance | Clean dead code from net_utils | nope-ip removal - Phase 2
    - Code maintenance: Clean dead code from net_utils
    
    Signed-off-by: liranmauda <liran.mauda@gmail.com>

--- src/util/net_utils.js
+++ src/util/net_utils.js
@@ -1,66 +1,11 @@
 /* Copyright (C) 2016 NooBaa */
 'use strict';
 
-const _ = require('lodash');
 const os = require('os');
 const net = require('net');
-const dns = require('dns');
 const ip_module = require('ip');
-const pinger = require('ping');
 
-const P = require('./promise');
-const dbg = require('./debug_module')(__filename);
-const os_utils = require('./os_utils');
-const hostname_regexp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 const fqdn_regexp = /^(?=^.{1,253}$)(^(((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9])|((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63})$)/;
-
-const DEFAULT_PING_OPTIONS = {
-    timeout: 5000,
-    retries: 0,
-    packetSize: 64
-};
-
-async function ping(target, options) {
-    dbg.log1('pinging', target);
-
-    options = options || DEFAULT_PING_OPTIONS;
-    _.defaults(options, DEFAULT_PING_OPTIONS);
-    const candidate_ip = new URL(target).hostname || target;
-
-    if (net.isIP(candidate_ip)) {
-        await _ping_ip(candidate_ip);
-    } else {
-        const ip_table = await dns_resolve(target);
-        await P.map_any(ip_table, ip => _ping_ip(ip));
-    }
-}
-
-function _ping_ip(session, ip) {
-    return new Promise((resolve, reject) => {
-        pinger.sys.probe(ip, (is_alive, error) => {
-            if (is_alive) {
-                resolve();
-            } else {
-                reject(error);
-            }
-        });
-    });
-}
-
-async function dns_resolve(target, options) {
-    const modified_target = new URL(target).hostname || target;
-    await os_utils.get_dns_config(); // unused? needed?
-    const res = await dns.promises.resolve(modified_target, (options && options.rrtype) || 'A');
-    return res;
-}
-
-function is_hostname(target) {
-    if (hostname_regexp.test(target)) {
-        return true;
-    }
-
-    return false;
-}
 
 function is_fqdn(target) {
     if (target && fqdn_regexp.test(target)) {
@@ -118,9 +63,6 @@ function find_ifc_containing_address(address) {
     }
 }
 
-exports.ping = ping;
-exports.dns_resolve = dns_resolve;
-exports.is_hostname = is_hostname;
 exports.is_ip = is_ip;
 exports.is_fqdn = is_fqdn;
 exports.is_localhost = is_localhost;

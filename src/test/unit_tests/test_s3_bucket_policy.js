25191b94c3fe74d18f9a937386c1fee55b93b59e
Vinayakswami Hariharmath <vharihar@redhat.com>
2025-02-10 19:40:07 +0530

    [MCG NC] Bucket policy with deny statement using NotPrincipal also denies the principal
    This is the put-bucket-policy case for combination of
    
    Effect : DENY,
    Action: $OPERATION,
    NotPrincipal: $ACCOUNT
    
    The $ACCOUNT mentioned in the "NotPrincipal" should be
    excluded from DENy operation and should be allowed on the
    $OPERATION
    
    Example: For operation get_object, if we have DENY effect
    for * (all accounts) and we want to give access to any one
    or few accounts, then that account can be part of "NotPrincipal"
    
    Fixes: https://issues.redhat.com/browse/DFBUGS-1519
    Signed-off-by: Vinayakswami Hariharmath <vharihar@redhat.com>

--- src/test/unit_tests/test_s3_bucket_policy.js
+++ src/test/unit_tests/test_s3_bucket_policy.js
@@ -1647,6 +1647,47 @@ mocha.describe('s3_bucket_policy', function() {
         }));
     });
 
+    mocha.it('should be able to use notPrincipal with Effect Deny', async function() {
+        const self = this; // eslint-disable-line no-invalid-this
+        self.timeout(15000);
+        const auth_put_policy = {
+        Version: '2012-10-17',
+        Statement: [
+            {
+                Effect: 'Allow',
+                Principal: { AWS: [user_a, user_b] },
+                Action: ['s3:*'],
+                Resource: [`arn:aws:s3:::${BKT}/*`]
+            },
+            {
+                Effect: 'Deny',
+                NotPrincipal: { AWS: user_a },
+                Action: ['s3:GetObject'],
+                Resource: [`arn:aws:s3:::${BKT}/*`]
+            }
+        ]};
+        await s3_owner.putBucketPolicy({
+            Bucket: BKT,
+            Policy: JSON.stringify(auth_put_policy)
+        });
+
+        await s3_a.putObject({
+            Body: BODY,
+            Bucket: BKT,
+            Key: KEY,
+        });
+
+        await s3_a.getObject({
+            Bucket: BKT,
+            Key: KEY,
+        });
+
+        await assert_throws_async(s3_b.getObject({
+            Bucket: BKT,
+            Key: KEY,
+        }));
+    });
+
     mocha.it('should be able to use notResource', async function() {
         const self = this; // eslint-disable-line no-invalid-this
         self.timeout(15000);

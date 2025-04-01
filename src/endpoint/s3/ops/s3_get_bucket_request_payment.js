3630cf428935d4911fafc3e72f4279ae2fb2b867
shirady <57721533+shirady@users.noreply.github.com>
2025-04-01 16:15:57 +0300

    return Mock on GetBucketRequestPayment
    1. Rename the file and actions related to GetBucketRequestPayment and add a log printing so we will not fail the request, also add a test for the mock in containerized and NC.
    2. Rename the file and actions related to PutBucketRequestPayment in the same way.
    
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>
    (cherry picked from commit e0f09225153c184450506413674746bb757e7b94)

--- src/endpoint/s3/ops/s3_get_bucket_request_payment.js
+++ src/endpoint/s3/ops/s3_get_bucket_request_payment.js
@@ -1,20 +1,24 @@
 /* Copyright (C) 2016 NooBaa */
 'use strict';
 
+const dbg = require('../../../util/debug_module')(__filename);
+
 /**
  * http://docs.aws.amazon.com/AmazonS3/latest/API/RESTrequestPaymentGET.html
  */
-async function get_bucket_requestPayment(req) {
+async function get_bucket_request_payment(req) {
     await req.object_sdk.read_bucket({ name: req.params.bucket });
+    const payer = 'BucketOwner';
+    dbg.log1(`s3_get_bucket_request_payment (returns ${payer} on every request)`);
     return {
         RequestPaymentConfiguration: {
-            Payer: 'BucketOwner'
+            Payer: payer
         }
     };
 }
 
 module.exports = {
-    handler: get_bucket_requestPayment,
+    handler: get_bucket_request_payment,
     body: {
         type: 'empty',
     },

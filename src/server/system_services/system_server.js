a984b8b388b257e0429a6c968e25c285f8a334ad
Naveen Paul <napaul@redhat.com>
2025-08-14 11:08:54 +0530

    Mongo | Upgrade flow
    Signed-off-by: Naveen Paul <napaul@redhat.com>

--- src/server/system_services/system_server.js
+++ src/server/system_services/system_server.js
@@ -191,6 +191,7 @@ function new_system_changes(req, name, owner_account_id) {
     if (config.DEFAULT_POOL_TYPE === 'HOSTS') {
         const pool_name = config.DEFAULT_POOL_NAME;
         const fs_pool = pool_server.new_pool_defaults(pool_name, system._id, 'HOSTS', 'BLOCK_STORE_FS', owner_account_id);
+        fs_pool.is_default_pool = true;
         fs_pool.hosts_pool_info = { is_managed: false, host_count: 0 };
         default_pool = fs_pool;
     } else {

7b02317ba0a4220c374298593c823189278e9d23
Ben <belimele@redhat.com>
2025-09-04 16:48:57 +0200

    Write results to a bucket
    Signed-off-by: Ben <belimele@redhat.com>

--- tools/ibm_runner_helpers/run_warp.sh
+++ tools/ibm_runner_helpers/run_warp.sh
@@ -3,21 +3,40 @@ set -euxo pipefail
 PS4='+ [run_warp] '
 export HOME=/home/ubuntu
 
+handle_failure() {
+local msg="$1"
+send_failure_notification "$msg"
+}
+
+handle_success() {
+# Upload logs to S3 bucket with timestamp-based directory
+local timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+local log_dir="warp-test-logs-${timestamp}"
+local logs_path="/home/ubuntu/tests/noobaa-core/logs/warp-test-logs"
+
+echo "Uploading logs to IBM COS s3://${S3_LOGS_BUCKET}/${log_dir}/"
+aws s3 cp "$logs_path" "s3://${S3_LOGS_BUCKET}/${log_dir}/" --recursive --no-progress --endpoint-url "${IBM_COS_ENDPOINT}"
+echo "Successfully uploaded logs to IBM COS s3://${S3_LOGS_BUCKET}/${log_dir}/"
+send_success_notification "s3://${S3_LOGS_BUCKET}/${log_dir}/"
+}
+
 send_failure_notification() {
 local msg="$1"
 node /usr/local/bin/slack_notifier.js "${SLACK_NIGHTLY_RESULTS_URL}" failure "Warp failed: $msg" || true
 }
 
 send_success_notification() {
-node /usr/local/bin/slack_notifier.js "${SLACK_NIGHTLY_RESULTS_URL}" success "Warp success" || true
+local log_location="$1"
+node /usr/local/bin/slack_notifier.js "${SLACK_NIGHTLY_RESULTS_URL}" success "Warp success - logs available at: $log_location" || true
 }
 
 shutdown_vm() {
 echo "Shutting down VM..."
-sudo shutdown -P now
+# TODO: Uncomment once done testing
+# sudo shutdown -P now
 }
 
-trap 'send_failure_notification "line $LINENO exit $?"' ERR
+trap 'handle_failure "line $LINENO exit $?"' ERR
 trap 'shutdown_vm' EXIT
 
 cd /home/ubuntu/tests/noobaa-core
@@ -27,9 +46,9 @@ make tester
 
 echo "Creating Warp logs directory..."
 mkdir -p logs/warp-test-logs
-chmod 777 logs/warp-test-logs
+chmod 755 logs/warp-test-logs
 
 echo "Running Warp tests..."
 make test-warp -o tester
 
-send_success_notification
+handle_success

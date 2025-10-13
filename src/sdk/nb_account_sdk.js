3d165ab325d1331d2f75749cb24b028e6737e460
Naveen Paul <napaul@redhat.com>
2025-10-13 14:33:41 +0530

    IAM_NOOBAA | IAM Create account
    Signed-off-by: Naveen Paul <napaul@redhat.com>

---
+++ src/sdk/nb_account_sdk.js
@@ -0,0 +1,29 @@
+/* Copyright (C) 2016 NooBaa */
+'use strict';
+
+const AccountSDK = require('../sdk/account_sdk');
+const BucketSpaceNB = require('./bucketspace_nb');
+const AccountSpaceNB = require('../sdk/accountspace_nb');
+
+// NBAccountSDK was based on AccountSDK
+class NBAccountSDK extends AccountSDK {
+     /**
+     * @param {{
+     *      rpc_client: nb.APIClient;
+     *      internal_rpc_client: nb.APIClient;
+     * }} args
+     */
+    constructor({rpc_client, internal_rpc_client}) {
+        const bucketspace = new BucketSpaceNB({ rpc_client, internal_rpc_client });
+        const accountspace = new AccountSpaceNB({ rpc_client, internal_rpc_client });
+
+        super({
+            rpc_client: rpc_client,
+            internal_rpc_client: internal_rpc_client,
+            bucketspace: bucketspace,
+            accountspace: accountspace,
+        });
+    }
+}
+
+module.exports = NBAccountSDK;

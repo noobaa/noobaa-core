39465ee87b99a96336496f9abc6c9de12829c505
Karthik P S <karthikperla2000@gmail.com>
2025-11-20 15:08:45 +0530

    Updating log level, add comment.
    Signed-off-by: Karthik P S <karthikperla2000@gmail.com>

--- src/upgrade/upgrade_scripts/5.21.0/remove_datachunks_index.js
+++ src/upgrade/upgrade_scripts/5.21.0/remove_datachunks_index.js
@@ -2,13 +2,15 @@
 "use strict";
 
 async function run({ dbg, db_client }) {
+  /* After the index is dropped, a new composite index (system, bucket, dedup_key) 
+    will be created during bootstrap */
   const indexName = 'idx_btree_datachunks_dedup_key';
 
   try {
     const pool = db_client.instance().get_pool();
     await pool.query(`DROP INDEX IF EXISTS ${indexName};`);
 
-    dbg.log2("Executed upgrade script for dropping index ", indexName);
+    dbg.log0("Executed upgrade script for dropping index ", indexName);
   } catch (err) {
     dbg.error('An error ocurred in the upgrade process:', err);
     throw err;

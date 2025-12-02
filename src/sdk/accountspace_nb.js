f73d114b59e7cc53c8523a5ca1031664c88b9497
shirady <57721533+shirady@users.noreply.github.com>
2025-12-02 16:06:52 +0200

    remove requesting account in accountspace_nb
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>
    (cherry picked from commit e58b242f97d0e4dc6a598cb2bf2305b71a3c20a9)

--- src/sdk/accountspace_nb.js
+++ src/sdk/accountspace_nb.js
@@ -36,20 +36,20 @@ class AccountSpaceNB {
     async create_user(params, account_sdk) {
 
         const requesting_account = account_sdk.requesting_account;
-        const account_email_wrapped = account_util.get_account_email_from_username(params.username, requesting_account._id.toString());
+        const account_email_wrapped = account_util.get_account_email_from_username(params.username, requesting_account._id);
         const req = {
                 name: params.username, // actual username saved
                 email: account_email_wrapped, // unique email generated from username lowercase and root account id
                 has_login: false,
                 s3_access: true,
                 allow_bucket_creation: true,
-                owner: requesting_account._id.toString(),
+                owner: requesting_account._id,
                 iam_path: params.iam_path,
                 roles: ['admin'],
                 // TODO: default_resource remove
-                default_resource: requesting_account.default_resource.name,
+                default_resource: requesting_account.default_resource,
             };
-        const iam_account = await account_sdk.rpc_client.account.create_user(req, requesting_account);
+        const iam_account = await account_sdk.rpc_client.account.create_user(req);
         // TODO : Clean account cache
         // TODO : Send Event
         return {
@@ -62,27 +62,23 @@ class AccountSpaceNB {
     }
 
     async get_user(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.get_user(params, requesting_account);
+        return await account_sdk.rpc_client.account.get_user(params);
     }
 
     async update_user(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.update_user(params, requesting_account);
+        return await account_sdk.rpc_client.account.update_user(params);
         // TODO : Clean account cache
         // TODO : Send Event
     }
 
     async delete_user(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.delete_user(params, requesting_account);
+        return await account_sdk.rpc_client.account.delete_user(params);
         // TODO : clean account cache
 
     }
 
     async list_users(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.list_users(params, requesting_account);
+        return await account_sdk.rpc_client.account.list_users(params);
 
     }
 
@@ -91,30 +87,25 @@ class AccountSpaceNB {
     /////////////////////////////////
 
     async create_access_key(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.create_access_key(params, requesting_account);
+        return await account_sdk.rpc_client.account.create_access_key(params);
     }
 
     async get_access_key_last_used(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.get_access_key_last_used(params, requesting_account);
+        return await account_sdk.rpc_client.account.get_access_key_last_used(params);
     }
 
     async update_access_key(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.update_access_key(params, requesting_account);
+        return await account_sdk.rpc_client.account.update_access_key(params);
         // TODO : clean account cache
     }
 
     async delete_access_key(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.delete_access_key(params, requesting_account);
+        return await account_sdk.rpc_client.account.delete_access_key(params);
         // TODO : clean account cache
     }
 
     async list_access_keys(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.list_access_keys(params, requesting_account);
+        return await account_sdk.rpc_client.account.list_access_keys(params);
     }
 
     ///////////////////////////
@@ -122,18 +113,15 @@ class AccountSpaceNB {
     ///////////////////////////
 
     async tag_user(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.tag_user(params, requesting_account);
+        return await account_sdk.rpc_client.account.tag_user(params);
     }
 
     async untag_user(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.untag_user(params, requesting_account);
+        return await account_sdk.rpc_client.account.untag_user(params);
     }
 
     async list_user_tags(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.list_user_tags(params, requesting_account);
+        return await account_sdk.rpc_client.account.list_user_tags(params);
     }
 
     ////////////////////
@@ -141,23 +129,19 @@ class AccountSpaceNB {
     ////////////////////
 
     async put_user_policy(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.put_user_policy(params, requesting_account);
+        return await account_sdk.rpc_client.account.put_user_policy(params);
     }
 
     async get_user_policy(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.get_user_policy(params, requesting_account);
+        return await account_sdk.rpc_client.account.get_user_policy(params);
     }
 
     async delete_user_policy(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.delete_user_policy(params, requesting_account);
+        return await account_sdk.rpc_client.account.delete_user_policy(params);
     }
 
     async list_user_policies(params, account_sdk) {
-        const requesting_account = account_sdk.requesting_account;
-        return await account_sdk.rpc_client.account.list_user_policies(params, requesting_account);
+        return await account_sdk.rpc_client.account.list_user_policies(params);
     }
 }
 

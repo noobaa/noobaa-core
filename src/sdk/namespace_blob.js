0388d3afdb18ad0b5eaa258c4586fae76a855702
Naveen Paul <napaul@redhat.com>
2026-04-22 13:14:06 +0530

    Add error handler to the Azure stream to catch AbortError
    Signed-off-by: Naveen Paul <napaul@redhat.com>
    (cherry picked from commit d93390a92cfd5e1f00ac0d794c0734eace1b41e4)

--- src/sdk/namespace_blob.js
+++ src/sdk/namespace_blob.js
@@ -248,6 +248,12 @@ class NamespaceBlob {
                     this.container,
                     inspect(_.omit(params, 'object_md.ns')),
                     'callback res');
+                // Add error handler to the Azure stream to catch AbortError
+                res.readableStreamBody?.on('error', stream_err => {
+                    dbg.error('NamespaceBlob.read_object_stream: Azure stream error:', stream_err.name, stream_err.message);
+                    // Destroy count_stream with error - properly cleans up and propagates error
+                    count_stream.destroy(stream_err);
+                });
                 if (!res.readableStreamBody) throw new Error('NamespaceBlob.read_object_stream: download response is invalid');
                 return resolve(res.readableStreamBody.pipe(count_stream));
             }).catch(err => {

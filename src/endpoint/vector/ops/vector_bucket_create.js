c6799ed93e51730329f7ff0f75329fb5598c7e53
Amit Prinz Setter <alphaprinz@gmail.com>
2026-04-29 08:01:29 -0700

    vectors - create vector bucket - use account's default resource if it's nsfs (#9563)
    Signed-off-by: Amit Prinz Setter <alphaprinz@gmail.com>
    (cherry picked from commit eb3867ee81775ee28b1b611b10150d55aa7e7cce)

--- src/endpoint/vector/ops/vector_bucket_create.js
+++ src/endpoint/vector/ops/vector_bucket_create.js
@@ -3,6 +3,7 @@
 //const config = require('../../../../config');
 const dbg = require('../../../util/debug_module')(__filename);
 const config = require('../../../../config');
+const system_store = require('../../../server/system_services/system_store').get_instance();
 
 const { VectorError } = require('../vector_errors');
 
@@ -13,9 +14,19 @@ async function post_vector_bucket(req, res) {
 
     dbg.log0("post_vector_bucket body =", req.body, ", headers =", req.headers);
 
-    const ns_name = req.headers[config.VECTORS_NSR_HEADER];
+    let ns_name = req.headers[config.VECTORS_NSR_HEADER];
     const subpath = req.headers[config.NSFS_CUSTOM_BUCKET_PATH_HTTP_HEADER];
     const vector_db_type = req.headers[config.VECTORS_DB_TYPE_HEADER] || 'lance';
+
+    //if ns was not explicitly given in header, try account's default resource
+    if (!ns_name) {
+        const default_resource_name = req.object_sdk?.requesting_account.default_resource;
+        const default_resource = system_store?.data?.systems[0]?.namespace_resources_by_name[default_resource_name];
+        //only nsfs resources are applicable
+        if (default_resource && default_resource.nsfs_config) {
+            ns_name = default_resource_name;
+        }
+    }
 
     // NS header is mandatory for containerized, optional for NC NSFS
     if (!ns_name && !req.object_sdk.nsfs_config_root) {

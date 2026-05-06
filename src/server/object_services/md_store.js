17640b2feffa02403861914f4ab85af50e0b03a9
Danny Zaken <dannyzaken@gmail.com>
2026-05-06 15:29:01 -0400

    exclude deleted when listing multipart parts from the DB
    in object_server.list_multiparts, the query did not filter by "deleted".
    This is both wrong semantically and caused the query to miss the index.
    
    Signed-off-by: Danny Zaken <dannyzaken@gmail.com>
    (cherry picked from commit c86af59d472aab02cf081391309d43bbb214c2a0)

--- src/server/object_services/md_store.js
+++ src/server/object_services/md_store.js
@@ -1203,6 +1203,7 @@ class MDStore {
             size: { $exists: true },
             md5_b64: { $exists: true },
             create_time: { $exists: true },
+            deleted: null,
         }, {
             sort: {
                 num: 1,

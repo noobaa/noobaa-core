9beb5de638a00922f0e8ccc07e6ddcf2061cb1b6
kajalpareek-lab <pareekkajal97@gmail.com>
2026-07-02 14:18:57 +0530

    refactor: remove WORM_ENABLED object-lock feature gate
    Remove the global WORM feature gate and rely on object-lock bucket/object state, while adding guards for legacy buckets without object_lock_configuration and allowing internal Disabled lock state in schema validation.
    
    Signed-off-by: kajalpareek-lab <pareekkajal97@gmail.com>

--- src/endpoint/s3/ops/s3_get_object_retention.js
+++ src/endpoint/s3/ops/s3_get_object_retention.js
@@ -2,16 +2,11 @@
 'use strict';
 
 const s3_utils = require('../s3_utils');
-const S3Error = require('../s3_errors').S3Error;
-const config = require('../../../../config');
 
 /**
  * https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetObjectRetention.html
  */
 async function get_object_retention(req) {
-    if (!config.WORM_ENABLED) {
-        throw new S3Error(S3Error.NotImplemented);
-    }
     const object_retention = await req.object_sdk.get_object_retention({
         bucket: req.params.bucket,
         key: req.params.key,
